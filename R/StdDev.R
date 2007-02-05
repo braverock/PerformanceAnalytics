624b4c90bbea8dc20a384e98ee3add8725fb9f3c
Brian G. Peterson <brian@braverock.com>
2007-02-05 19:03:04 +0000

    - add std wrapper function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/StdDev.R
+++ R/StdDev.R
@@ -13,6 +13,14 @@ function(R)
 }
 
 ###############################################################################
+
+`std` <-
+function(r) {
+    # NOTE: std function is listed in the doc for fBasics, but not implemented
+    return(StdDev(R))
+}
+
+###############################################################################
 # R (http://r-project.org/) Econometrics for Performance and Risk Analysis
 #
 # Copyright (c) 2004-2007 Peter Carl and Brian G. Peterson
@@ -20,8 +28,12 @@ function(R)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: StdDev.R,v 1.1 2007-02-02 19:06:15 brian Exp $
+# $Id: StdDev.R,v 1.2 2007-02-05 19:03:04 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2007/02/02 19:06:15  brian
+# - Initial Revision of packaged foles to version control
+# Bug 890
+#
 ###############################################################################
