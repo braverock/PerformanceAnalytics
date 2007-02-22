24ad386d205cf64e61b27590b56fb6fc556fad70
Brian G. Peterson <brian@braverock.com>
2007-02-22 18:26:26 +0000

    - update function name for consistency
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/cummax.column.R
+++ R/cummax.column.R
@@ -1,4 +1,4 @@
-`cumMax.column` <-
+`cummax.column` <-
 function (R, na.rm = TRUE, ...)
 { # @author Peter Carl
 
@@ -23,10 +23,13 @@ function (R, na.rm = TRUE, ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: cummax.column.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: cummax.column.R,v 1.3 2007-02-22 18:26:26 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
