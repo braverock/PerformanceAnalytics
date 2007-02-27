1b29cca29b74765b2c4cf5b3272bad088567480d
Peter Carl <pcarl@gsb.uchicago.edu>
2007-02-27 21:19:41 +0000

    - added function wrapper for downloading and calculating S&P500 price returns
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/download.SP500PriceReturns.R
@@ -0,0 +1,28 @@
+function(start = "1998-01-01", end = NULL)
+{ # @ author Peter Carl
+
+    #  Download returns of the "market" asset.
+
+    # Required inputs are just start and stop dates.
+    # Outputs a table of ***Monthly*** log returns
+
+    # FUNCTION:
+
+    # Requires the tseries library
+    require(tseries)
+
+    # Download the "market" returns, in this case, the S&P 500 INDEX (^GSPC)
+    # from finance.yahoo.com
+
+    # @todo: Offer a menu of "market" assets
+    # @todo: Compression attribute can be made configurable for different granularity
+    x = get.hist.quote("^gspc", start = start, end = end, quote = "Close", compression = "m")
+    # @todo: make the data type returned selectable (e.g., df, ts, zoo, matrix)
+    # @todo: offer simple or log returns as method
+
+    # Yahoo returns yield data as price series, so we need to calculate returns 
+    market.returns = CalculateReturns(x, method="simple")
+
+    colnames(market.returns)="SP500"
+    market.returns
+}
