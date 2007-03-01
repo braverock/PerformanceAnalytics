f90f9c5f83a65b352db6b4d08c7829ea54b65589
Brian G. Peterson <brian@braverock.com>
2007-03-01 03:26:59 +0000

    - add function and documentation for DownsideDeviation wrapper SemiVariance
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SemiDeviation.R
+++ R/SemiDeviation.R
@@ -3,7 +3,7 @@ function (R)
 { # @author Peter Carl
 
     # DESCRIPTION:
-    # This function should is just a wrapper of DownsideDeviation with
+    # This function is just a wrapper of DownsideDeviation with
     # MAR = mean(x)
     # see below
 
@@ -12,7 +12,14 @@ function (R)
     x = checkDataVector(R)
 
     return(DownsideDeviation(R, MAR=mean(R)))
+}
 
+`SemiVariance` <-
+function (R)
+{
+    x = checkDataVector(R)
+
+    return(DownsideDeviation(R, MAR=mean(R)))
 }
 
 ###############################################################################
@@ -23,10 +30,13 @@ function (R)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: SemiDeviation.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: SemiDeviation.R,v 1.3 2007-03-01 03:26:59 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
