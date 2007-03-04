a841ad94bb3e548bd91fa303db11718fa2013cf1
Brian G. Peterson <brian@braverock.com>
2007-03-04 01:05:24 +0000

    - simplify code because NA's are taken care of in checkDataVector
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@168 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SharpeRatio.R
+++ R/SharpeRatio.R
@@ -21,9 +21,9 @@ function (R, rf = 0)
 
     # FUNCTION:
 
-    x = checkDataVector(R)
-    return((mean(x, na.rm = TRUE) - rf)/sd(x, na.rm = TRUE))
-    #return((mean(x) - rf)/sd(x))
+    R = checkDataVector(R)
+    return((mean(R) - rf)/sd(R))
+
 }
 
 ###############################################################################
@@ -34,10 +34,13 @@ function (R, rf = 0)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: SharpeRatio.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: SharpeRatio.R,v 1.3 2007-03-04 01:05:24 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
