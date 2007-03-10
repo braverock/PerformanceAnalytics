014f8c95aba9d7aec0e8a6c034067dd5529d8fe3
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-10 19:46:35 +0000

    - handles unequal periods - correctly calculates when first period is negative
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Drawdown.R
+++ R/chart.Drawdown.R
@@ -25,12 +25,28 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
     # FUNCTION:
 
     # Transform input data to a matrix
-    x = checkDataMatrix(R)
+    x = checkDataZoo(R)
 
     # Calculate drawdown level
-    Return.cumulative = cumprod.column(1+x)
-    maxCumulativeReturn = cummax.column(Return.cumulative)
-    drawdown = Return.cumulative/maxCumulativeReturn - 1
+#     Return.cumulative = cumprod.column(1+x)
+#     maxCumulativeReturn = cummax.column(Return.cumulative)
+#     drawdown = Return.cumulative/maxCumulativeReturn - 1
+    # Get dimensions and labels
+    columns = ncol(x)
+    columnnames = colnames(x)
+#
+    for(column in 1:columns) {
+        Return.cumulative = cumprod(1+na.omit(x[,column])) 
+        maxCumulativeReturn = cummax(c(1,Return.cumulative))[-1]
+        column.drawdown = Return.cumulative/maxCumulativeReturn - 1
+
+        if(column == 1)
+            drawdown = column.drawdown
+        else
+            drawdown = merge(drawdown,column.drawdown)
+    }
+
+    colnames(drawdown) = columnnames
 
     # Chart the drawdown level
     chart.TimeSeries(drawdown, col = colorset, legend.loc = legend.loc, ...)
@@ -45,10 +61,13 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.Drawdown.R,v 1.3 2007-02-26 13:50:57 brian Exp $
+# $Id: chart.Drawdown.R,v 1.4 2007-03-10 19:46:35 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2007/02/26 13:50:57  brian
+# - change cumMax.column to cummax.column to reflect change in function name to pass "R CMD check"
+#
 # Revision 1.2  2007/02/07 13:24:49  brian
 # - fix pervasive comment typo
 #
