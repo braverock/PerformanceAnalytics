632b8b29e65da7f9c2f33e82aebfc871e69061cf
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-10 05:08:04 +0000

    - revised function to take zoo objects and plot unequal time periods
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.CumReturns.R
+++ R/chart.CumReturns.R
@@ -3,8 +3,8 @@ function (R, wealth.index = FALSE, legend.loc = NULL, colorset = (1:12), ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
-    # A wrapper function that cumulates the returns given and draws a line graph
-    # of the results as a "wealth index".
+    # Cumulates the returns given and draws a line graph of the results as
+    # a cumulative return or a "wealth index".
 
     # Inputs:
     # R = a matrix, data frame, or timeSeries of returns
@@ -19,19 +19,26 @@ function (R, wealth.index = FALSE, legend.loc = NULL, colorset = (1:12), ...)
     # FUNCTION:
 
     # Transform input data to a matrix
-    x = checkDataMatrix(R)
-
-#     # Get dimensions and labels
-#     columns = ncol(x)
-#     rows = nrow(x)
-#     columnnames = colnames(x)
-#     rownames = rownames(x)
+    x = checkDataZoo(R)
+    
+    # Get dimensions and labels
+    columns = ncol(x)
+    columnnames = colnames(x)
 
     # Calculate the cumulative return
+    one = 0
     if(!wealth.index)
-        Return.cumulative = cumprod.column(1+x) - 1
-    else
-        Return.cumulative = cumprod.column(1+x)
+        one = 1
+
+    for(column in 1:columns) {
+        column.Return.cumulative = cumprod(1+na.omit(x[,column])) - one
+        if(column == 1)
+            Return.cumulative = column.Return.cumulative
+        else
+            Return.cumulative = merge(Return.cumulative,column.Return.cumulative)
+    }
+
+    colnames(Return.cumulative) = columnnames
 
     # Chart the cumulative returns series
     chart.TimeSeries(Return.cumulative, col = colorset, legend.loc = legend.loc, ...)
@@ -46,10 +53,13 @@ function (R, wealth.index = FALSE, legend.loc = NULL, colorset = (1:12), ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.CumReturns.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: chart.CumReturns.R,v 1.3 2007-03-10 05:08:04 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
