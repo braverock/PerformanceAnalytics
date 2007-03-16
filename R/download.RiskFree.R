e2d0419dee2de8a94a10d5f550e136a3bd140486
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-16 14:22:47 +0000

    - now delivers a zoo object instead of a data.frame
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@262 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/download.RiskFree.R
+++ R/download.RiskFree.R
@@ -16,7 +16,7 @@ function(start = "1998-01-01", end = NULL, compression = c("m","d"))
     x = get.hist.quote("^irx", start = start, end = end, quote = "Close", compression = compression)
 
     # Yahoo returns yield data as an annualized percentage;
-    rf = as.data.frame(x/100/12)
+    rf = x/100/12
     colnames(rf) = "13-week US Treasury Bill (^IRX)"
     rf
 }
