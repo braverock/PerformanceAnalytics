7417ae993a3239823ce5b551cab1f895dc047a9f
Brian G. Peterson <brian@braverock.com>
2007-03-20 11:27:32 +0000

    - add copyright, license, and CVS Log
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@278 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/download.RiskFree.R
+++ R/download.RiskFree.R
@@ -20,3 +20,31 @@ function(start = "1998-01-01", end = NULL, compression = c("m","d"))
     colnames(rf) = "13-week US Treasury Bill (^IRX)"
     rf
 }
+
+###############################################################################
+# R (http://r-project.org/) Econometrics for Performance and Risk Analysis
+#
+# Copyright (c) 2004-2007 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: download.RiskFree.R,v 1.6 2007-03-20 11:27:32 brian Exp $
+#
+###############################################################################
+# $Log: not supported by cvs2svn $
+# Revision 1.5 2007-03-16 peter
+# - now delivers a zoo object instead of a data.frame
+#
+# Revision 1.4 2007-03-04 14:59:27 brian
+# - minor changes to pass R CMD check
+#
+# Revision 1.3 2007-02-27 21:38:02 brian
+# - change compression param to an enumerated default
+#
+# Revision 1.2 2007-02-27 16:32:58 peter
+# - added compression as attribute
+#
+# Revision 1.1 2007-02-27 16:29:04 peter
+# - added function
+###############################################################################
