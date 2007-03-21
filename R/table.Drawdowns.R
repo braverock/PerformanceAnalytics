5364bf119781adbaee4548b1a114deee5ff8c94b
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-21 04:20:11 +0000

    - added error trap where top exceeds # drawdowns
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@283 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/table.Drawdowns.R
+++ R/table.Drawdowns.R
@@ -7,6 +7,7 @@ function (R, top = 5)
 
     # Inputs:
     # R: Assumes returns rather than prices
+    # top: the number of drawdowns to include
 
     # Output:
     # Creates a data.frame of the worst "n" drawdowns
@@ -16,6 +17,12 @@ function (R, top = 5)
     R = checkData(R, method = "zoo")
 
     x = sortDrawdowns(findDrawdowns(R))
+
+    ndrawdowns = length(x$from)
+    if (ndrawdowns < top){
+        warning(paste("Only ",ndrawdowns," available in the data.",sep="")
+        top = ndrawdowns
+    }
 
     result = data.frame(time(R)[x$from[1:top]], time(R)[x$to[1:top]], x$return[1:top], x$length[1:top])
 
@@ -32,8 +39,11 @@ function (R, top = 5)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: table.Drawdowns.R,v 1.1 2007-03-21 04:13:43 peter Exp $
+# $Id: table.Drawdowns.R,v 1.2 2007-03-21 04:20:11 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2007/03/21 04:13:43  peter
+# - initial addition of function to cvs
+#
 ###############################################################################
