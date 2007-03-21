59d05adab48e8b81196b74342d73d8341e74420b
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-21 14:07:38 +0000

    - added trough date, periods to trough, periods to recovery - changed to use dataCheck
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@285 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/findDrawdowns.R
+++ R/findDrawdowns.R
@@ -12,7 +12,7 @@ function (R)
 
     # FUNCTION:
 
-    x = checkDataVector(R)
+    x = checkData(R, method="vector") # matrix?
 
     cumulativeReturn = cumprod(1+x)
     maxCumulativeReturn = cummax(cumulativeReturn)
@@ -23,6 +23,7 @@ function (R)
     begin = c()
     end = c()
     length = c()
+    trough = c()
     index = 1
     if (drawdowns[1] >= 0)
         priorSign = 1
@@ -31,29 +32,36 @@ function (R)
     from = 1
     sofar = drawdowns[1]
     to = 1
+    dmin = 1
     for (i in 2:length(drawdowns)) {
         thisSign <- ifelse(drawdowns[i] < 0, 0, 1)
         if (thisSign == priorSign) {
-            if(drawdowns[i]< sofar)
+            if(drawdowns[i]< sofar) {
                 sofar = drawdowns[i]
+                dmin = i
+            }
             to = i
         }
         else {
-            draw[index] <- sofar
-            begin[index] <- from
-            end[index] <- to
+    # @todo: add trough date, time to trough, recovery time
+            draw[index] = sofar
+            begin[index] = from
+            trough[index] = dmin
+            end[index] = to +1
             #cat(sofar, from, to)
-            from <- i
-            sofar <- drawdowns[i]
-            to <- i
-            index <- index + 1
-            priorSign <- thisSign
+            from = i 
+            sofar = drawdowns[i]
+            to = i
+            dmin = i +1
+            index = index + 1
+            priorSign = thisSign
         }
     }
     draw[index] = sofar
     begin[index] = from
-    end[index] = to
-    list(return=draw, from=begin, to=end, length=end-begin)
+    trough[index] = dmin +1
+    end[index] = to + 1
+    list(return=draw, from=begin, trough = trough, to=end, length=end-begin, peaktotrough = trough-begin, recovery = end-trough)
 
 }
 
@@ -65,10 +73,13 @@ function (R)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: findDrawdowns.R,v 1.3 2007-02-07 13:24:49 brian Exp $
+# $Id: findDrawdowns.R,v 1.4 2007-03-21 14:07:38 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.2  2007/02/06 11:58:42  brian
 # - standardize attribution for Drawdown runs
 #
