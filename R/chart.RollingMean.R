6962422afd6b91d655a1b9f4dfae78f3f34fc5c7
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-22 13:46:50 +0000

    - uses checkData
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@305 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.RollingMean.R
+++ R/chart.RollingMean.R
@@ -1,5 +1,5 @@
 `chart.RollingMean` <-
-function (R, n=12, xaxis = TRUE, ylim = NULL, main = paste("Rolling ",n,"-Month Mean Return",sep=""), trim = FALSE, ...)
+function (R, width = 12, xaxis = TRUE, ylim = NULL, na.pad = FALSE, ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -7,36 +7,35 @@ function (R, n=12, xaxis = TRUE, ylim = NULL, main = paste("Rolling ",n,"-Month 
 
     # Inputs:
     # R: a matrix, data frame, or timeSeries, usually a set of monthly returns.
-    #   The first column is assumed to be the returns of interest.
 
     # Outputs:
     # A timeseries line charts of the rolling mean, with error bars
 
     # FUNCTION:
-    x = checkDataMatrix(R)
-    x = as.data.frame(x[,1]) # @todo: handle multiple columns?
-    ncols = ncol(x)
-    rows = nrow(x)
+    x = checkData(R, method = "zoo")
 
-    x.mean = rollingFunction(x, n = n, trim = trim, na.rm = FALSE, firstcolumn = 1, digits = 8, FUN = "mean")
-    x.stdev = rollingFunction(x, n = n, trim = trim, na.rm = FALSE, firstcolumn = 1, digits = 8, FUN = "stdev")
+    # Get dimensions and labels
+    columnnames = colnames(x)
+
+    # Calculate
+
+    x.mean = rollapply(na.omit(x[,1,drop=FALSE]), width = width, FUN = "mean", na.pad = na.pad, align = "right")
+    x.stdev = rollapply(na.omit(x[,1,drop=FALSE]), width = width, FUN = "stdev", na.pad = na.pad, align = "right")
 
     # @todo: allow user to set confidence interval
     # @todo: add chart for StdDev w confidence bands: x.stdev +- 2* x.stdev/sqrt(2*n)
-    lower.band = x.mean - 2 * x.stdev/sqrt(n)
-    upper.band = x.mean + 2 * x.stdev/sqrt(n)
+    lower.band = x.mean - 2 * x.stdev/sqrt(width)
+    upper.band = x.mean + 2 * x.stdev/sqrt(width)
 
-    result = as.matrix(cbind(x.mean,lower.band,upper.band))
+    result = merge(x.mean,lower.band,upper.band)
 
     # Set ylim correctly to allow for confidence bands
     if(is.null(ylim[1]))
-        ylim = range(result[!is.na(result)])
+        ylim = range(result)
 
     # The first row is the annualized returns
-    chart.TimeSeries(result, ylim = ylim, main = main, xaxis = xaxis, ylab = "Mean Return", lty = c(1,2,2), colorset = c("black","darkgray","darkgray"), ...)
+    chart.TimeSeries(result, ylim = ylim, xaxis = xaxis, ylab = "Return", lty = c(1,2,2), colorset = c("black","darkgray","darkgray"), main = paste(columnnames[1], " Rolling ",width,"-Month Mean Return",sep=""), ...)
 
-    #lines(1:rows, as.matrix(lower.band), col = "darkgray", lwd = 1, lty = "dashed", type = "l")
-    #lines(1:rows, as.matrix(upper.band), col = "darkgray", lwd = 1, lty = "dashed", type = "l")
 }
 
 ###############################################################################
@@ -47,10 +46,13 @@ function (R, n=12, xaxis = TRUE, ylim = NULL, main = paste("Rolling ",n,"-Month 
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.RollingMean.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: chart.RollingMean.R,v 1.3 2007-03-22 13:46:50 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
