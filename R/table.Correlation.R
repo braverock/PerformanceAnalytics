5b71887a11b298aee0ac7b3473e7116825960d57
Peter Carl <pcarl@gsb.uchicago.edu>
2007-03-22 12:38:50 +0000

    - handles multiple assets and benchmarks - uses checkData - manages unequal time periods
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@303 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/table.Correlation.R
+++ R/table.Correlation.R
@@ -15,45 +15,38 @@ function (Ra, Rb, n, trim = TRUE, na.rm = FALSE, ...)
 
     # FUNCTION:
 
-    # Prepare the data
+    Ra = checkData(Ra, method = "zoo")
+    Rb = checkData(Rb, method = "zoo")
 
-    # target.vec is the vector of data we want correlations for; we'll get it
-    # from Ra
-    target.vec = checkDataVector(Ra)
-    # data.matrix is a vector or matrix of data we want correlations against;
-    # we'll take it from Rb
-    data.matrix = checkDataMatrix(Rb)
-    columns=ncol(Rb)
-    columnnames = colnames(Rb)
+    # Get dimensions and labels
+    columns.a = ncol(Ra)
+    columns.b = ncol(Rb)
+    columnnames.a = colnames(Ra)
+    columnnames.b = colnames(Rb)
 
-    #  For each period (or row):
-    for (column in 1:columns) {
-        values = vector('numeric', 0)
+    # Calculate
+    for(column.a in 1:columns.a) { # for each asset passed in as R
+        for(column.b in 1:columns.b) { # against each asset passed in as Rb
+            merged.assets = merge(Ra[,column.a,drop=FALSE], Rb[,column.b,drop=FALSE])
+            merged.assets = na.omit(merged.assets) # leaves the overlapping period
 
-        # Calculate correlation and significance
-#         htest = cor.test(Ra, Rb[,column], ...)
-        htest = cor.test(target.vec, data.matrix[,column], ...)
-        values = cbind(htest$estimate, htest$p.value,htest$conf.int[1],htest$conf.int[2])
+            htest = cor.test(merged.assets[,1], merged.assets[,2], ...)
+            values = cbind(htest$estimate, htest$p.value,htest$conf.int[1],htest$conf.int[2])
 
-        if(column == 1) {
-            result.df = data.frame(Value = values)#, row.names = columnnames[column])
-        }
-
-        else {
-            nextrow = data.frame(Value = values)#, row.names = columnnames[column])
-            result.df = rbind(result.df, nextrow)
+            if(column.a == 1 & column.b == 1) {
+                result.df = data.frame(Value = values)
+                rownames(result.df) = paste(columnnames.a[column.a], columnnames.b[column.b], sep = " to ")
+            }
+            else {
+                nextrow = data.frame(Value = values)
+                rownames(nextrow) = paste(columnnames.a[column.a], columnnames.b[column.b], sep = " to ")
+                result.df = rbind(result.df, nextrow)
+            }
         }
     }
 
     colnames(result.df) = c("Correlation","p-value","Lower CI","Upper CI")
-    rownames(result.df) = columnnames
 
-#     if (trim & TS)
-#         positions = positions[-(1:(n - 1))]
-#     if (TS) {
-#         resultingtable = timeSeries(as.matrix(ans), positions, units = x.orig@units,
-#             FinCenter = x.orig@FinCenter)
-#     }
     result.df
 
 }
@@ -66,10 +59,13 @@ function (Ra, Rb, n, trim = TRUE, na.rm = FALSE, ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: table.Correlation.R,v 1.4 2007-02-28 03:59:50 peter Exp $
+# $Id: table.Correlation.R,v 1.5 2007-03-22 12:38:50 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2007/02/28 03:59:50  peter
+# - repaired broken data check
+#
 # Revision 1.3  2007/02/26 22:04:36  brian
 # - changes in functions to pass "R CMD check" for package
 #
