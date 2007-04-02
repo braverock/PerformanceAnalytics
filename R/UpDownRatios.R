b6edb834fc572a1300ad6b0bd8ae51756ab128b3
Peter Carl <pcarl@gsb.uchicago.edu>
2007-04-02 21:57:26 +0000

    - modified to use checkData functions - uses zoo to align dates - changed calculations to sum returns
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@329 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/UpDownRatios.R
+++ R/UpDownRatios.R
@@ -27,48 +27,101 @@ function(R, Rb, method = "capture", side = "up", ...)
     # in x.
 
     # FUNCTION:
-    assetReturns.vec = checkDataVector(R)
-    benchmarkReturns.vec = checkDataVector(Rb)
 
-    UpRi = subset(assetReturns.vec, assetReturns.vec > 0)
-    DownRi = subset(assetReturns.vec, assetReturns.vec < 0)
-    UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
-    DownRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
+    # Transform input data to a data frame
 
-    if(method == "capture" & side == "up") {
-        UpRi = subset(assetReturns.vec, benchmarkReturns.vec > 0)
-        UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
-        cumRi = prod(1+UpRi) - 1
-        cumRb = prod(1+UpRb) - 1
-        result = cumRi/cumRb
+    Ra = checkData(R, method = "zoo")
+    Rb = checkData(Rb, method = "zoo")
+    #rf = checkDataMatrix(rf)
+
+    # Get dimensions and labels
+    columns.a = ncol(Ra)
+    columns.b = ncol(Rb)
+    columnnames.a = colnames(Ra)
+    columnnames.b = colnames(Rb)
+
+    # Calculate
+    for(column.a in 1:columns.a) { # for each asset passed in as R
+        for(column.b in 1:columns.b) { # against each asset passed in as Rb
+
+            merged.assets = merge(Ra[,column.a,drop=FALSE], Rb[,column.b,drop=FALSE])
+
+            if(method == "capture" & side == "up") {
+                UpRa = subset(merged.assets[,1], merged.assets[,2] > 0)
+                UpRb = subset(merged.assets[,2], merged.assets[,2] > 0)
+                cumRa = sum(UpRa)
+                cumRb = sum(UpRb)
+                result = cumRa/cumRb
+            }
+            if(method == "capture" & side == "down") {
+                DnRa = subset(merged.assets[,1], merged.assets[,2] < 0)
+                DnRb = subset(merged.assets[,2], merged.assets[,2] < 0)
+                cumRa = sum(DnRa)
+                cumRb = sum(DnRb)
+                result = cumRa/cumRb
+            }
+            ### This isn't right - find a reference
+#             if(method == "number" & side == "up") {
+#                 UpRi = length(subset(merged.assets[,1], merged.assets[,2] > 0))
+#                 UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
+#                 result = UpRi/UpRb
+#             }
+#             if(method == "number" & side == "down") {
+#                 DnRi = length(subset(merged.assets[,1], merged.assets[,2] < 0))
+#                 DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
+#                 result = DnRi/DnRb
+#             }
+
+        }
     }
-    if(method == "capture" & side == "down") {
-        DnRi = subset(assetReturns.vec, benchmarkReturns.vec < 0)
-        DnRb = subset(benchmarkReturns.vec, benchmarkReturns.vec < 0)
-        cumRi = prod(1+DnRi) - 1
-        cumRb = prod(1+DnRb) - 1
-        result = cumRi/cumRb
-    }
-    if(method == "number" & side == "up") {
-        UpRi = length(subset(assetReturns.vec, benchmarkReturns.vec > 0))
-        UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
-        result = UpRi/UpRb
-    }
-    if(method == "number" & side == "down") {
-        DnRi = length(subset(assetReturns.vec, benchmarkReturns.vec < 0))
-        DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
-        result = DnRi/DnRb
-    }
-    if(method == "percentage" & side == "up") {
-        UpRi = length(subset(assetReturns.vec, (benchmarkReturns.vec > 0) && (benchmarkReturns.vec < assetReturns.vec)))
-        UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
-        result = UpRi/UpRb
-    }
-    if(method == "percentage" & side == "down") {
-        DnRi = length(subset(assetReturns.vec, (benchmarkReturns.vec < 0) && (benchmarkReturns.vec < assetReturns.vec)))
-        DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
-        result = DnRi/DnRb
-    }
+
+
+
+# 
+# 
+# 
+#     assetReturns.vec = checkDataVector(R)
+#     benchmarkReturns.vec = checkDataVector(Rb)
+# 
+#     UpRi = subset(assetReturns.vec, assetReturns.vec > 0)
+#     DownRi = subset(assetReturns.vec, assetReturns.vec < 0)
+#     UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
+#     DownRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
+# 
+#     if(method == "capture" & side == "up") {
+#         UpRi = subset(assetReturns.vec, benchmarkReturns.vec > 0)
+#         UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
+#         cumRi = prod(1+UpRi) - 1
+#         cumRb = prod(1+UpRb) - 1
+#         result = cumRi/cumRb
+#     }
+#     if(method == "capture" & side == "down") {
+#         DnRi = subset(assetReturns.vec, benchmarkReturns.vec < 0)
+#         DnRb = subset(benchmarkReturns.vec, benchmarkReturns.vec < 0)
+#         cumRi = prod(1+DnRi) - 1
+#         cumRb = prod(1+DnRb) - 1
+#         result = cumRi/cumRb
+#     }
+#     if(method == "number" & side == "up") {
+#         UpRi = length(subset(assetReturns.vec, benchmarkReturns.vec > 0))
+#         UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
+#         result = UpRi/UpRb
+#     }
+#     if(method == "number" & side == "down") {
+#         DnRi = length(subset(assetReturns.vec, benchmarkReturns.vec < 0))
+#         DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
+#         result = DnRi/DnRb
+#     }
+#     if(method == "percentage" & side == "up") {
+#         UpRi = length(subset(assetReturns.vec, (benchmarkReturns.vec > 0) && (benchmarkReturns.vec < assetReturns.vec)))
+#         UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
+#         result = UpRi/UpRb
+#     }
+#     if(method == "percentage" & side == "down") {
+#         DnRi = length(subset(assetReturns.vec, (benchmarkReturns.vec < 0) && (benchmarkReturns.vec < assetReturns.vec)))
+#         DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
+#         result = DnRi/DnRb
+#     }
     result
 }
 
@@ -80,10 +133,13 @@ function(R, Rb, method = "capture", side = "up", ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: UpDownRatios.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: UpDownRatios.R,v 1.3 2007-04-02 21:57:26 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
