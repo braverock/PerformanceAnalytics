2166017344438547fdea4335e2b748c952f2c8d8
Peter Carl <pcarl@gsb.uchicago.edu>
2007-04-25 20:07:12 +0000

    - added x-y axes
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@410 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Scatter.R
+++ R/chart.Scatter.R
@@ -39,8 +39,11 @@ function (x, y, reference.grid = TRUE, main = "Title", ylab=NULL, xlab=NULL, xli
 
     plot(x~y, main = main, pch = symbolset, ylog = ylog, col=colorset, ...)
 
-    if(reference.grid)
+    if(reference.grid) {
         grid(col = elementcolor)
+        abline(h = 0, col = elementcolor)
+        abline(v = 0, col = elementcolor)
+    }
 }
 
 ###############################################################################
@@ -51,10 +54,14 @@ function (x, y, reference.grid = TRUE, main = "Title", ylab=NULL, xlab=NULL, xli
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.Scatter.R,v 1.3 2007-04-02 21:58:42 peter Exp $
+# $Id: chart.Scatter.R,v 1.4 2007-04-25 20:07:12 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2007/04/02 21:58:42  peter
+# - changed colorset default
+# - changed to use checkData
+#
 # Revision 1.2  2007/02/07 13:24:49  brian
 # - fix pervasive comment typo
 #
