9674dff2a4263b38ba791eede64dee71e3a14b61
Brian G. Peterson <brian@braverock.com>
2007-06-21 21:36:08 +0000

    - fixed to use length of entire series, per Platinga, van der Meer, Sortino 2001
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@443 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/DownsideDeviation.R
+++ R/DownsideDeviation.R
@@ -14,10 +14,10 @@ function (Ra, MAR = 0)
 
     # FUNCTION:
 
-    x = checkDataVector(Ra)
+    Ra = checkDataVector(Ra)
 
-    y = subset(x,x < MAR)
-    return(sqrt(sum((y - MAR)^2)/(length(y))))
+    r = subset(Ra,Ra < MAR)
+    return(sqrt(sum((r - MAR)^2)/(length(Ra))))
 }
 
 ###############################################################################
@@ -28,10 +28,13 @@ function (Ra, MAR = 0)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: DownsideDeviation.R,v 1.4 2007-06-21 21:24:39 brian Exp $
+# $Id: DownsideDeviation.R,v 1.5 2007-06-21 21:36:08 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2007/06/21 21:24:39  brian
+# - update to use length rather than length-1 after reviewing several original Sortino papers
+#
 # Revision 1.3  2007/03/14 00:54:06  brian
 # - updates to parameters for standardization
 #
