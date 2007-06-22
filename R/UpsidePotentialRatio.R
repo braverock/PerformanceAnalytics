be8bfbb3eb4b99fb8aa4c51c059b91748fd36aec
Brian G. Peterson <brian@braverock.com>
2007-06-22 02:16:52 +0000

    - initial commit of functions and documentation for Sortino's Upside Potential Ratio
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@446 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/UpsidePotentialRatio.R
@@ -0,0 +1,38 @@
+`UpsidePotentialRatio` <-
+function (Ra, MAR = 0)
+{ # @author Brian G. Peterson
+
+    # Description:
+    # Sortino proposed to better account for skill and excess performance
+    # by using only downside semivariance as the measure of risk.
+    # UpsidePotentialRatio is an attempted improvement over the SortinoRatio
+
+    # Ra    return vector
+    # MAR   minimum acceptable return
+    # Function:
+
+    Ra = checkData(Ra, method = "vector")
+
+    r = subset(Ra,Ra > MAR)
+    return( ( sum(r - MAR)/(length(Ra)) )/ DownsideDeviation(Ra, MAR) )
+}
+
+`UPR`<-
+function (Ra, MAR = 0)
+{ # @author Brian G. Peterson
+    UpsidePotentialRatio(Ra=Ra, MAR=MAR)
+}
+
+###############################################################################
+# R (http://r-project.org/) Econometrics for Performance and Risk Analysis
+#
+# Copyright (c) 2004-2007 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: UpsidePotentialRatio.R,v 1.1 2007-06-22 02:16:52 brian Exp $
+#
+###############################################################################
+# $Log: not supported by cvs2svn $
+###############################################################################
