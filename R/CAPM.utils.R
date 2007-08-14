e95b57d70c6ba80cba8e9acd868e46b2a8834def
Peter Carl <pcarl@gsb.uchicago.edu>
2007-08-14 20:53:27 +0000

    - changed rf to mean(rf) in CAPM.CML
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@513 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/CAPM.utils.R
+++ R/CAPM.utils.R
@@ -15,13 +15,14 @@ function (Rb, rf = 0 )
 function (Ra, Rb, rf = 0)
 { #@author Brian G. Peterson
 
-    Ra = checkDataVector(Ra)
-    Rb = checkDataVector(Rb)
+    Ra = checkData(Ra, method="vector")
+    Rb = checkData(Rb, method="vector")
+    rf = checkData(rf, method="vector")
 
     if (length(Ra) != length(Rb))
         stop("Returns to be assessed have unequal time periods. Are there NA's in the data?")
 
-    CML = rf + CAPM.CML.slope(Rb, rf)*mean(Ra)
+    CML = mean(rf) + CAPM.CML.slope(Rb, rf)*mean(Ra)
 
     return(CML)
 }
@@ -57,10 +58,15 @@ function (Rb, rf = 0)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: CAPM.utils.R,v 1.2 2007-03-11 16:53:19 brian Exp $
+# $Id: CAPM.utils.R,v 1.3 2007-08-14 20:53:27 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/03/11 16:53:19  brian
+# - add equations and text to documentation
+# - standardize on Ra as the Return of the Asset
+# - standardize on Ra as first argument where that wasn't previously true
+#
 # Revision 1.1  2007/03/03 18:10:46  brian
 # - Initial Revision of functions and documentation for CAPM utils on CML, SML, and RiskPremium
 #
