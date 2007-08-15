49d3405e1ecf00fb980c84d56ba3a4f6da6b1d30
Peter Carl <pcarl@gsb.uchicago.edu>
2007-08-15 03:37:43 +0000

    - first entry in CVS - wrapper for read.zoo with a few case conditions for setting defaults
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@522 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/Return.read.R
@@ -0,0 +1,66 @@
+`Return.read` <-
+function (filename=stop("Please specify a filename"), frequency=c("m","d","q","i","o"), format.in=c("ISO8601","excel","oo","gnumeric"), sep=",", header=TRUE, ...)
+{ # @author Peter Carl
+
+# A simple wrapper with some defaults for read.zoo
+
+# format is a parameter that says how the data being read is formatted.  Although
+# the default is set to the ISO 8601 standard (which can also be set as "%F"),
+# most spreadsheets have less sensible date formats as defaults.
+
+    format.in = format.in[1]
+    frequency = frequency[1]
+
+    switch(format.in,
+        ISO8601 = { # the least confusing default to use, %Y-%m-%d or %F
+            format = "%F"
+        },
+        excel = { # default date format for MS Excel spreadsheet csv
+            format = "%m/%d/%Y"
+        },
+        oo = { # default date format for OpenOffice spreadsheet csv
+            format = "%m/%d/%y" # this is a operating system dependent format
+        },
+        gnumeric = { # default date format for Gnumeric spreadsheet
+            format = "%d-%b-%Y"
+        },
+        { format = format.in }
+    )
+
+# Sets the date format conditioned on the frequency passed in
+
+    switch(frequency,
+        d = { # set to daily timeseries
+            FUN = as.Date
+        },
+        m = { # set to monthly timeseries
+            FUN = as.yearmon
+        },
+        q = { # set to quarterly timeseries
+            FUN = as.yearqtr
+        },
+        i = { # set to irregular timeseries
+            FUN = as.POSIXct
+        },
+        { # set to other timeseries
+            FUN = NULL
+        }
+    )
+    result = read.zoo(filename, sep=sep, format=format, FUN=FUN, header=header, ...)
+
+    result
+
+}
+###############################################################################
+# R (http://r-project.org/) Econometrics for Performance and Risk Analysis
+#
+# Copyright (c) 2004-2007 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: Return.read.R,v 1.1 2007-08-15 03:37:43 peter Exp $
+#
+###############################################################################
+# $Log: not supported by cvs2svn $
+###############################################################################
