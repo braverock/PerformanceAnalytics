072f594c1122ac5ccbb65b57478a20ab9119de27
Peter Carl <pcarl@gsb.uchicago.edu>
2007-09-11 02:52:02 +0000

    - removed the clever bits and calculate the hard way - row and column names work
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@572 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Return.Geltner.R
+++ R/Return.Geltner.R
@@ -1,6 +1,6 @@
 `Return.Geltner` <-
 function (Ra, ...)
-{ # @author Brian G. Peterson
+{ # @author Brian G. Peterson, Peter Carl
 
     # Description:
     # Geltner Returns came from real estate where they are used to uncover a
@@ -9,23 +9,34 @@ function (Ra, ...)
     # Ra    return vector
 
     # Function:
+    Ra = checkData(Ra, method="zoo")
+    # Get dimensions and labels
+    columns.a = ncol(Ra)
+    columnnames.a = colnames(Ra)
+    geltner = zoo(NULL)
 
-    if (is.vector(Ra)) {
+    for(column.a in 1:columns.a) { # for each asset passed in as R
+        column.Ra = zoo(NULL)
         # clean the data and get rid of NAs
-        Ra = checkData (Ra,na.rm=TRUE, method="zoo", ...=...)
+        column.Ra = checkData (Ra[, column.a, drop = FALSE], na.rm = TRUE, method = "zoo", ...=...)
         # compute the lagged return series
-        lagRa=lag(Ra, k=-1)
+        lagRa = lag(column.Ra, k=-1)
         # compute the first order autocorrelation
-        f_acf=as.numeric(acf(Ra,plot=FALSE)[1][[1]])
+        f_acf = as.numeric(acf(column.Ra, plot = FALSE)[1][[1]])
         # now calculate and return the Geltner series
-        geltner=(Ra-(lagRa*f_acf))/(1-f_acf)
-        # put back in rownames, since those have been lost by now
-        # rownames(geltner)=rownames(Ra)
-        # return
-        return(geltner)
-    } else {
-        apply(Ra, 2, Return.Geltner, ...=...)
+        column.geltner = (column.Ra-(lagRa*f_acf))/(1-f_acf)
+
+        if(column.a == 1) 
+            geltner = column.geltner
+        else 
+	        geltner = merge (geltner, column.geltner)
+
     }
+
+    colnames(geltner) = columnnames.a
+
+    # RESULTS:
+    return(geltner)
 
 }
 
@@ -37,8 +48,13 @@ function (Ra, ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: Return.Geltner.R,v 1.1 2007-09-01 16:29:55 brian Exp $
+# $Id: Return.Geltner.R,v 1.2 2007-09-11 02:52:02 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2007/09/01 16:29:55  brian
+# - initial revision of Geltner Returns function
+# - seems to only work on rectangular multicolumn inputs
+# - may lose rownames
+#
 ###############################################################################
