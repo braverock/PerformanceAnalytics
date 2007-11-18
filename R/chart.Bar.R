4fa8fde4814e50ab0af8e6facb8a25fc158fdbe7
Peter Carl <pcarl@gsb.uchicago.edu>
2007-11-18 14:28:02 +0000

    - squared off the bar ends
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@612 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Bar.R
+++ R/chart.Bar.R
@@ -18,7 +18,7 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
     # Transform input data to a matrix
     x = checkDataMatrix(R)
 
-    chart.TimeSeries(x, type = "h", colorset = colorset, legend.loc = legend.loc, ...)
+    chart.TimeSeries(x, type = "h", colorset = colorset, legend.loc = legend.loc, lend="butt",...)
 
 }
 
@@ -30,10 +30,13 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.Bar.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: chart.Bar.R,v 1.3 2007-11-18 14:28:02 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
