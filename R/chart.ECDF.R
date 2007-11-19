01002d9e85c919663a35e5da4304b7df884c2676
Peter Carl <pcarl@gsb.uchicago.edu>
2007-11-19 21:06:09 +0000

    - plots a fitted ECDF line to the sorted data
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@617 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/chart.ECDF.R
@@ -0,0 +1,38 @@
+`chart.ECDF` <-
+function(R, main = "Empirical CDF", xlab="x", ylab="F(x)", colorset = c("black", "#005AFF"), lwd = 1, xlim = NULL, ylim = NULL, darken = FALSE, lty = c(1,1), ...)
+{ # @author Peter Carl
+
+    # DESCRIPTION:
+
+    # Create an ECDF overlaid with a CDF
+
+    # Inputs:
+    # R = usually a set of monthly return
+
+    # Code inspired by a chart on David Rupert's web page
+    # http://www.stat.tamu.edu/~ljin/Finance/chapter2/Fig2_5.txt
+
+    R = checkData(R, method="vector", na.rm = TRUE)
+
+
+    if(darken)
+        elementcolor = "darkgray" #better for the printer
+    else
+        elementcolor = "lightgray" #better for the screen
+
+    t = seq(-2.5,2.5,length.out=1000)
+    p1 = pnorm(t,mean(R), sd(R))
+
+    rx = sort(R)
+
+    stepy = ((0:(length(R)-1))+0.5)/length(R)
+ 
+    plot(rx, stepy, type="s", col = colorset[1], xlab = xlab, ylab = ylab, main = main, axes = FALSE, lty = lty[1], lwd = lwd, ...)
+    lines(t, p1, lty = lty[2], col = colorset[2], lwd = lwd, ...)
+
+    axis(1, cex.axis = 0.8, col = elementcolor)
+    axis(2, cex.axis = 0.8, col = elementcolor)
+
+    box(col=elementcolor)
+
+}
