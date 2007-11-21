935de0dc68a2483b322c54647b89a4985f2d2f98
Peter Carl <pcarl@gsb.uchicago.edu>
2007-11-21 05:31:28 +0000

    - added fitting for norm, lnorm, cauchy, and sst distributions
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@620 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.QQPlot.R
+++ R/chart.QQPlot.R
@@ -1,5 +1,5 @@
 `chart.QQPlot` <-
-function (R, colorset = c("#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00", "#800000"), symbolset = 1, xlab = NULL, ylab = NULL, main = NULL, darken = FALSE, ...)
+function (R, colorset = c("#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00", "#800000"), symbolset = 1, xlab = NULL, ylab = NULL, main = NULL, darken = FALSE, distribution = "normal", line = TRUE, ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -13,26 +13,61 @@ function (R, colorset = c("#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00",
 
     # FUNCTION:
 
-    x = checkData(R, method = "vector")
-
+    x = checkData(R, method = "vector", na.rm = TRUE)
+    n = length(x)
     if(darken)
         elementcolor = "darkgray" #better for the printer
     else
         elementcolor = "lightgray" #better for the screen
 
 
-    if(is.null(xlab))
-        xlab = "Normal Quantiles"
+    if(distribution == "normal") {
+        if(is.null(xlab)) xlab = "Normal Quantiles"
+        if(is.null(ylab)) ylab = "Empirical Quantiles"
+        if(is.null(main)) main = "Normal QQ-Plot"
 
-    if(is.null(ylab))
-        ylab = "Empirical Quantiles"
+        # Normal Quantile-Quantile Plot:
+        qqnorm(x, xlab = xlab, ylab = ylab, main = main, pch = symbolset, axes = FALSE, ...)
+#         qqline(x, col = colorset[2], lwd = 2)
+        q.theo = qnorm(c(0.25,0.75))
+    }
+    if(distribution == "sst") {
+        library("sn")
+        if(is.null(xlab)) xlab = "Skew-T Quantiles"
+        if(is.null(ylab)) ylab = "Empirical Quantiles"
+        if(is.null(main)) main = "Skew-T QQ-Plot"
 
-    if(is.null(main))
-        main = "Normal QQ-Plot"
+        # Skew Student-T Quantile-Quantile Plot:
+        y = qst(c(1:n)/(n+1))
+        qqplot(y, x, xlab = xlab, ylab = ylab, axes=FALSE, ...)
+        q.theo = qst(c(0.25,0.75))
+    }
+    if(distribution == "cauchy") {
+        if(is.null(xlab)) xlab = "Cauchy Quantiles"
+        if(is.null(ylab)) ylab = "Empirical Quantiles"
+        if(is.null(main)) main = "Cauchy QQ-Plot"
 
-    # Normal Quantile-Quantile Plot:
-    qqnorm(x, xlab = xlab, ylab = ylab, main = main, pch = symbolset, axes = FALSE, ...)
-    qqline(x, col = colorset[2], lwd = 2)
+        # Skew Student-T Quantile-Quantile Plot:
+        y = qcauchy(c(1:n)/(n+1))
+        qqplot(y, x, xlab = xlab, ylab = ylab, axes=FALSE, ...)
+        q.theo = qcauchy(c(0.25,0.75))
+    }
+    if(distribution == "lnorm") {
+        if(is.null(xlab)) xlab = "Log Normal Quantiles"
+        if(is.null(ylab)) ylab = "Empirical Quantiles"
+        if(is.null(main)) main = "Log Normal QQ-Plot"
+
+        # Skew Student-T Quantile-Quantile Plot:
+        y = qlnorm(c(1:n)/(n+1))
+        qqplot(y, x, xlab = xlab, ylab = ylab, axes=FALSE, ...)
+        q.theo = qlnorm(c(0.25,0.75))
+    }
+
+    q.data=quantile(x,c(0.25,0.75))
+    slope = diff(q.data)/diff(q.theo)
+    int = q.data[1] - slope* q.theo[1]
+
+    if(line) abline(int, slope, col = colorset[2], lwd = 2)
 
     axis(1, cex.axis = 0.8, col = elementcolor)
     axis(2, cex.axis = 0.8, col = elementcolor)
@@ -49,10 +84,13 @@ function (R, colorset = c("#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00",
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.QQPlot.R,v 1.3 2007-09-26 02:59:23 peter Exp $
+# $Id: chart.QQPlot.R,v 1.4 2007-11-21 05:31:28 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2007/09/26 02:59:23  peter
+# - changed chart elements to be consistent with other chart
+#
 # Revision 1.2  2007/02/07 13:24:49  brian
 # - fix pervasive comment typo
 #
