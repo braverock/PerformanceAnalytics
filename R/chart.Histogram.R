537f0c507472d0dfc0306e759c77167aa58f2b15
Peter Carl <pcarl@gsb.uchicago.edu>
2007-11-22 04:52:20 +0000

    - added fit for skew t - fixed ylim for cauchy - unified fit across x axis
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@622 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Histogram.R
+++ R/chart.Histogram.R
@@ -1,5 +1,5 @@
 `chart.Histogram` <-
-function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", methods = c("none","add.density", "add.normal", "add.centered", "add.rug", "add.risk", "add.qqplot"), show.outliers = TRUE, colorset = c("lightgray", "#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00", "#800000"), border.col = "white", lwd = 2, xlim = NULL, ylim = NULL, darken = FALSE, note.lines = NULL, note.labels = NULL, note.color = "darkgray", probability = FALSE, p=0.99, ...)
+function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", methods = c("none","add.density", "add.normal", "add.centered", "add.cauchy", "add.sst", "add.rug", "add.risk", "add.qqplot"), show.outliers = TRUE, colorset = c("lightgray", "#00008F", "#005AFF", "#23FFDC", "#ECFF13", "#FF4A00", "#800000"), border.col = "white", lwd = 2, xlim = NULL, ylim = NULL, darken = FALSE, note.lines = NULL, note.labels = NULL, note.color = "darkgray", probability = FALSE, p=0.99, ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -25,7 +25,7 @@ function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", meth
     columns = ncol(y)
     rows = nrow(y)
     columnnames = colnames(y)
-
+    n=length(x)
     rangedata = 0
 
     if(is.null(main)){
@@ -48,19 +48,49 @@ function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", meth
         rangedata = c(min(x),max(x))
     else
         rangedata =  c(qnorm(0.001, mean(x), stdev(x)), qnorm(0.999, mean(x), stdev(x)))
+    if(!is.null(note.lines)) {
+        rangedata = c(rangedata,note.lines)
+    }
+    yrange = 0
+    if(is.null(xlim))
+        xlim = range(rangedata)
 # Need to add ylim calc up here to capture full cauchy density function, otherwise it won't plot
 # What else needs to be done up here before plotting the histogram?
+    s = seq(xlim[1], xlim[2], length = 500)
+
     for (method in methods) {
         switch(method,
+            add.density = {
+                # Show density estimate
+                den = density(x, n=length(x))
+            },
             add.cauchy = {
 #               requires library(MASS)
-                fitted=fitdistr(x, 'cauchy')
-                xlab = paste("Cauchy(location=",round(fitted$estimate[[1]],2),", scale=",round(fitted$estimate[[2]],2),")")
-                s = quantile(x, probs=seq(0, 1, 0.005))
-                ylim=range(dcauchy(s,location = fitted$estimate[[1]], scale = fitted$estimate[[2]], log = FALSE))
+                # This uses a Maximum Likelihood method as shown on:
+                # Wessa P., (2006), Maximum-likelihood Cauchy Distribution Fitting (v1.0.0) in 
+                # Free Statistics Software (v1.1.21-r4), Office for Research Development and 
+                # Education, URL http://www.wessa.net/rwasp_fitdistrcauchy.wasp/
+                fit=fitdistr(x, 'cauchy')
+                xlab = paste("Cauchy(location=",round(fit$estimate[[1]],2),", scale=",round(fit$estimate[[2]],2),")")
+                fitted.cauchy = dcauchy(s,location = fit$estimate[[1]], scale = fit$estimate[[2]], log = FALSE)
+                yrange=c(yrange,max(fitted.cauchy))
+                probability = TRUE
+            },
+            add.sst = {
+#               requires library(sn)
+                fit = st.mle(y=x)
+                fitted.sst = dst(s, location = fit$dp[[1]], scale = fit$dp[[2]], shape = fit$dp[[3]], df=fit$dp[[4]], log=F)
+                yrange=c(yrange,max(fitted.sst))
                 probability = TRUE
             },
             add.normal = {
+                fitted.normal = dnorm(s, mean(x), stdev(x))
+                yrange=c(yrange,max(fitted.normal))
+                probability = TRUE
+            },
+            add.centered = {
+                fitted.centered = dnorm(s, 0, stdev(x))
+                yrange=c(yrange,max(fitted.centered))
                 probability = TRUE
             },
             add.risk = {
@@ -68,12 +98,8 @@ function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", meth
             }
         )
     }
-    if(!is.null(note.lines)) {
-        rangedata = c(rangedata,note.lines)
-    }
+    ylim = c(0,max(yrange))
 
-    if(is.null(xlim))
-        xlim = range(rangedata)
     hist(x = x, probability = probability, xlim = xlim, ylim = ylim, col = colorset[1], border = border.col, xlab = xlab, main = main, breaks = breaks, cex.axis = 0.8, axes = FALSE, ...)
     axis(1, cex.axis = 0.8, col = elementcolor)
     axis(2, cex.axis = 0.8, col = elementcolor)
@@ -84,27 +110,21 @@ function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", meth
         switch(method,
             add.density = {
                 # Show density estimate
-                den = density(x, n=length(x))
                 lines(den, col = colorset[2], lwd = lwd)
             },
             add.normal = {
                 # Show normal distribution around the mean
-                s = seq(xlim[1], xlim[2], length = 500)
-                lines(s, dnorm(s, mean(x), stdev(x)), col = colorset[3], lwd = lwd)
+                lines(s, fitted.normal, col = colorset[3], lwd = lwd)
             },
             add.centered = {
                 # Show normal distribution around 0
-                s = seq(xlim[1], xlim[2], length = 500)
-                lines(s, dnorm(s, 0, stdev(x)), col = colorset[3], lwd = lwd)
+                lines(s, fitted.centered, col = colorset[3], lwd = lwd)
             },
             add.cauchy = {
-                # First we have to fit the data to calculate the location and scale parameters (see ylim calc above)
-                # This uses a Maximum Likelihood method as shown on:
-                # Wessa P., (2006), Maximum-likelihood Cauchy Distribution Fitting (v1.0.0) in 
-                # Free Statistics Software (v1.1.21-r4), Office for Research Development and 
-                # Education, URL http://www.wessa.net/rwasp_fitdistrcauchy.wasp/
-
-                lines(s, dcauchy(s,location = fitted$estimate[[1]], scale = fitted$estimate[[2]], log = FALSE), col = colorset[4], lwd=lwd)
+                lines(s, fitted.cauchy, col = colorset[4], lwd=lwd)
+            },
+            add.sst = { #requires package sn
+                lines(s, fitted.sst, col = colorset[4], lwd=lwd)
             },
             add.rug = {
                 rug(x, col = elementcolor)
@@ -149,7 +169,7 @@ function(R, breaks="FD", main = NULL, xlab = "Returns", ylab = "Frequency", meth
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.Histogram.R,v 1.18 2007-11-20 21:19:22 peter Exp $
+# $Id: chart.Histogram.R,v 1.19 2007-11-22 04:52:20 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
