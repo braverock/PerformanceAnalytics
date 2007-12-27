a75c94102e26ef006bd597cd5930c411f690925b
Peter Carl <pcarl@gsb.uchicago.edu>
2007-12-27 18:40:03 +0000

    - fixed theta calculations
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@631 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SmoothingIndex.R
+++ R/SmoothingIndex.R
@@ -1,26 +1,29 @@
 `SmoothingIndex` <-
-function (Ra, ...)
+function (ra, ...)
 { # @author Peter Carl
 
     # Description:
     # SmoothingIndex
 
-    # Ra    return vector
+    # ra    log return vector
 
     # Function:
 
-    Ra = checkData(Ra, method="vector")
+    ra = checkData(ra, method="vector", na.rm=TRUE)
 
-    # First, create a a maximum liklihood estimation fit for an MA process.
+    MA2 = NULL
+    thetas = 0
+    smoothing.index = 0
 
-    # include.mean: Getmansky, et al. 2004 p 555 "By applying the above procedure 
+    # First, create a a maximum likelihood estimation fit for an MA process.
+
+    # include.mean: Getmansky, et al. JFE 2004 p 555 "By applying the above procedure 
     # to observed de-meaned returns...", so set parameter to FALSE
     # transform.pars: ibid, "we impose the additional restriction that the estimated MA(k)
     # process be invertible." so set the parameter to TRUE
-    MA2 = arima(Ra, order=c(0,0,2), method="ML", transform.pars=TRUE, 
-include.mean=FALSE)
+    MA2 = arima(ra, order=c(0,0,2), method="ML", transform.pars=TRUE, include.mean=FALSE)
 
-    # Page 555 of Getmansky et al. JFE (2004):
+    # Page 555:
     # 
     # "Because of the scaling property Eq. (52) of the MA(k) likelihood function, a 
     # simple procedure for obtaining estimates of our smoothing model with the 
@@ -31,10 +34,13 @@ include.mean=FALSE)
     # Eq. (49)."
 
     # From the arima function above, MA2$coef contains two coefficients, and no intercept value.
-    # The text above indicates that theta_0 would be equal to 1 and returned as a coefficient.
-    # The calculation below adjusts for that.  as.numeric strips off the labels
+    # The calculation below adjusts for that.
 
-    thetas = as.numeric((MA2$coef)/sum(MA2$coef))
+    # Dr. Stefan Albrecht, CFA points out, "I assume that you have to take:"
+thetas = c(ma0 = 1, coef(MA2)) / (1 + sum(coef(MA2)))
+# 
+# 
+#     thetas = as.numeric((MA2$coef)/sum(MA2$coef))
 
     # This measure is well known in the industrial organization literature as the Herfindahl 
     # index, a measure of the concentration of firms in a given industry where yj represents the 
@@ -44,8 +50,20 @@ include.mean=FALSE)
     # In the context of smoothed returns, a lower value of x implies more smoothing, and the upper bound
     # of 1 implies no smoothing, hence we shall refer to x as a ‘‘smoothing index’’.
 
-    SmoothingIndex = sum(thetas^2) # Calc'd as Herfindahl index would be
+    smoothing.index = sum(thetas^2) # Calc'd as Herfindahl index would be, referred to as ξ, below
 
-    return(SmoothingIndex)
+    # The interpretation of this is tricky:
+
+    # "Because θj ∈ [0, 1], ξ is also confined to the unit interval, and is minimized when all 
+    # the θj ’s are identical, which implies a value of 1/(k + 1) for ξ, and is maximized when 
+    # one coefficient is 1 and the rest are 0, in which case ξ = 1. In the context of smoothed 
+    # returns, a lower value of ξ implies more smoothing, and the upper bound of 1 implies no 
+    # smoothing, hence we shall refer to ξ as a “smoothing index”."
+
+    # That's fine, except that this method (as described in the paper), does not enforce
+    # θj ∈ [0, 1], so ξ is not limited to that range either.  All we can say is that lower values
+    # are "less liquid" and higher values are "more liquid" or mis-specified.
+
+    return(smoothing.index)
 
 }
