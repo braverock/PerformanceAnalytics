7416ea4ba016665364bb840136f53a59dcfc0cfa
Brian G. Peterson <brian@braverock.com>
2007-12-27 21:58:33 +0000

    - chnage ra to Ra to match usage in the rest of the package
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@656 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/SmoothingIndex.R
+++ R/SmoothingIndex.R
@@ -1,5 +1,5 @@
 `SmoothingIndex` <-
-function (ra, ...)
+function (Ra, ...)
 { # @author Peter Carl
 
     # Description:
@@ -9,7 +9,7 @@ function (ra, ...)
 
     # Function:
 
-    ra = checkData(ra, method="vector", na.rm=TRUE)
+    ra = checkData(Ra, method="vector", na.rm=TRUE)
 
     MA2 = NULL
     thetas = 0
@@ -17,20 +17,20 @@ function (ra, ...)
 
     # First, create a a maximum likelihood estimation fit for an MA process.
 
-    # include.mean: Getmansky, et al. JFE 2004 p 555 "By applying the above procedure 
+    # include.mean: Getmansky, et al. JFE 2004 p 555 "By applying the above procedure
     # to observed de-meaned returns...", so set parameter to FALSE
     # transform.pars: ibid, "we impose the additional restriction that the estimated MA(k)
     # process be invertible." so set the parameter to TRUE
     MA2 = arima(ra, order=c(0,0,2), method="ML", transform.pars=TRUE, include.mean=FALSE)
 
     # Page 555:
-    # 
-    # "Because of the scaling property Eq. (52) of the MA(k) likelihood function, a 
-    # simple procedure for obtaining estimates of our smoothing model with the 
-    # normalization Eq. (49) is to transform estimates (θ; sigma) from standard 
-    # MA(k) estimation packages such as SAS or RATS by dividing each θi by 1 + θ1 + 
-    # θ2 ... θk and multiplying sigma by the same factor. The likelihood function 
-    # remains unchanged but the transformed smoothing coefficients will now satisfy 
+    #
+    # "Because of the scaling property Eq. (52) of the MA(k) likelihood function, a
+    # simple procedure for obtaining estimates of our smoothing model with the
+    # normalization Eq. (49) is to transform estimates (θ; sigma) from standard
+    # MA(k) estimation packages such as SAS or RATS by dividing each θi by 1 + θ1 +
+    # θ2 ... θk and multiplying sigma by the same factor. The likelihood function
+    # remains unchanged but the transformed smoothing coefficients will now satisfy
     # Eq. (49)."
 
     # From the arima function above, MA2$coef contains two coefficients, and no intercept value.
@@ -38,15 +38,15 @@ function (ra, ...)
 
     # Dr. Stefan Albrecht, CFA points out, "I assume that you have to take:"
 thetas = c(ma0 = 1, coef(MA2)) / (1 + sum(coef(MA2)))
-# 
-# 
+#
+#
 #     thetas = as.numeric((MA2$coef)/sum(MA2$coef))
 
-    # This measure is well known in the industrial organization literature as the Herfindahl 
-    # index, a measure of the concentration of firms in a given industry where yj represents the 
-    # market share of firm j: Because theta_j A1⁄20; 1; x is also confined to the unit interval, 
-    # and is minimized when all the theta_j ’s are identical, which implies a value of 1=ðk þ 1Þ 
-    # for x; and is maximized when one coefficient is 1 and the rest are 0, in which case x 1⁄4 1: 
+    # This measure is well known in the industrial organization literature as the Herfindahl
+    # index, a measure of the concentration of firms in a given industry where yj represents the
+    # market share of firm j: Because theta_j A1⁄20; 1; x is also confined to the unit interval,
+    # and is minimized when all the theta_j ’s are identical, which implies a value of 1=ðk þ 1Þ
+    # for x; and is maximized when one coefficient is 1 and the rest are 0, in which case x 1⁄4 1:
     # In the context of smoothed returns, a lower value of x implies more smoothing, and the upper bound
     # of 1 implies no smoothing, hence we shall refer to x as a ‘‘smoothing index’’.
 
@@ -54,10 +54,10 @@ thetas = c(ma0 = 1, coef(MA2)) / (1 + sum(coef(MA2)))
 
     # The interpretation of this is tricky:
 
-    # "Because θj ∈ [0, 1], ξ is also confined to the unit interval, and is minimized when all 
-    # the θj ’s are identical, which implies a value of 1/(k + 1) for ξ, and is maximized when 
-    # one coefficient is 1 and the rest are 0, in which case ξ = 1. In the context of smoothed 
-    # returns, a lower value of ξ implies more smoothing, and the upper bound of 1 implies no 
+    # "Because θj ∈ [0, 1], ξ is also confined to the unit interval, and is minimized when all
+    # the θj ’s are identical, which implies a value of 1/(k + 1) for ξ, and is maximized when
+    # one coefficient is 1 and the rest are 0, in which case ξ = 1. In the context of smoothed
+    # returns, a lower value of ξ implies more smoothing, and the upper bound of 1 implies no
     # smoothing, hence we shall refer to ξ as a “smoothing index”."
 
     # That's fine, except that this method (as described in the paper), does not enforce
