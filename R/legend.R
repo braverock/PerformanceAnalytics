3b85d9abbd74803e0228b59d42ff3d11d461d2d2
Peter Carl <pcarl@gsb.uchicago.edu>
2008-02-15 04:20:17 +0000

    - parameterized box color separately from background elements
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@696 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/legend.R
+++ R/legend.R
@@ -5,7 +5,7 @@ function (x, y = NULL, legend, fill = NULL, col = par("col"),
     yjust = 1, x.intersp = 1, y.intersp = 1, adj = c(0, 0.5),
     text.width = NULL, text.col = par("col"), merge = do.lines &&
         has.pch, trace = FALSE, plot = TRUE, ncol = 1, horiz = FALSE,
-    title = NULL, inset = 0, border.col = NULL, border.lwd = 1, border.lty = "solid")
+    title = NULL, inset = 0, border.col = NULL, border.lwd = 1, border.lty = "solid", box.col = NULL, box.lwd = 1, box.lty = "solid")
 {
     # Modifications to core graphics legend() function
     # @author R Core Dev Team
@@ -207,7 +207,7 @@ function (x, y = NULL, legend, fill = NULL, col = par("col"),
             fill <- rep(fill, length.out = n.leg)
             rect2(left = xt, top = yt + ybox/2, dx = xbox, dy = ybox,
                 col = fill, density = density, angle = angle,
-                border = bg) #removed internal border
+                border = box.col) #removed internal border
         }
         xt <- xt + dx.fill
     }
@@ -425,10 +425,13 @@ function (x, y = NULL, legend, fill = NULL, col = par("col"),
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: legend.R,v 1.2 2007-02-07 13:24:49 brian Exp $
+# $Id: legend.R,v 1.3 2008-02-15 04:20:17 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2007/02/07 13:24:49  brian
+# - fix pervasive comment typo
+#
 # Revision 1.1  2007/02/02 19:06:15  brian
 # - Initial Revision of packaged files to version control
 # Bug 890
