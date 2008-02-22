f38c45aa688b060220bb85653eec85a323518fe2
Peter Carl <pcarl@gsb.uchicago.edu>
2008-02-22 03:47:16 +0000

    - added example to comments
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@700 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/style.QPfit.R
+++ R/style.QPfit.R
@@ -108,4 +108,40 @@ function(R.fund, R.style, all=F, ...)
 
     return(result)
 
+    # EXAMPLE:
+    # > head(R.fund)
+    #          SP500.TR
+    # Jan 1996   0.0340
+    # Feb 1996   0.0093
+    # Mar 1996   0.0096
+    # Apr 1996   0.0147
+    # May 1996   0.0258
+    # Jun 1996   0.0038
+    # > head(R.style)
+    #          Russell.1000.Growth Russell.1000.Value
+    # Jan 1996              0.0335             0.0312
+    # Feb 1996              0.0183             0.0076
+    # Mar 1996              0.0013             0.0170
+    # Apr 1996              0.0263             0.0038
+    # May 1996              0.0349             0.0125
+    # Jun 1996              0.0014             0.0008
+    # > style.QPfit(R.fund, R.style)
+    # [1] 0.5047724 0.4952276
+    # > style.QPfit(R.fund, R.style, all=T)
+    # $solution
+    # [1] 0.5047724 0.4952276
+    # 
+    # $value
+    # [1] -0.0008888153
+    # 
+    # $unconstrainted.solution
+    # [1] 0.5040111 0.4815228
+    # 
+    # $iterations
+    # [1] 2 0
+    # 
+    # $iact
+    # [1] 1
+    # 
+
 }
