d1c906072bbd83502903c117c7d0a5e2c649b3f4
Peter Carl <pcarl@gsb.uchicago.edu>
2008-02-22 03:41:56 +0000

    - first draft of a function for style fitting with constrained weights
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@699 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/style.QPfit.R
@@ -0,0 +1,111 @@
+`style.QPfit` <-
+function(R.fund, R.style, all=F, ...) 
+{
+# INPUTS
+# R.fund   Vector of a fund return time series
+# R.style   Matrix of a style return time series
+# 
+# 
+# OUTPUTS
+# weights   Vector of optimal style index weights
+# @ todo: TE  Tracking error between the calc'd and actual fund
+# @ todo: Fp  Vector of calculated fund time series
+# @ todo: R^2  Coefficient of determination
+#
+# 
+# w_i   Style weights
+# V   Variance-covariance matrix of style index matrix
+# C   Vector of covariances between the style index and the fund
+# e   Vector of 1's
+# n   Number of style indexes
+# 
+# To implement style analysis as described in:
+# http://www.stanford.edu/~wfsharpe/art/sa/sa.htm
+# here's what we're trying to do:
+# min VAR(R.f - SUM[w_i * R.s_i]) = min VAR(F - w*S)
+#   s.t. SUM[w_i] = 1; w_i > 0
+# 
+# Remembering that VAR(aX + bY) = a^2 VAR(X) + b^2 VAR(Y) + 2ab COV(X,Y), 
+# we can refactor our problem as:
+# 
+# = VAR(R.f) + w'*V*w - 2*w'*COV(R.f,R.s)
+# 
+# drop VAR[R.f] as it isn't a function of weights, multiply by 1/2:
+# 
+# = min (1/2) w'*V*w - C'w
+#   s.t. w'*e = 1, w_i > 0
+# 
+# Now, map that to the inputs of solve.QP, which is specified as:
+# min(-d' b + 1/2 b' D b) with the constraints A' b >= b_0
+# 
+# so:
+# b is the weight vector,
+# D is the variance-covariance matrix of the styles
+# d is the covariance vector between the fund and the styles
+#
+
+    # Check to see if the required libraries are loaded
+    if(!require("quadprog", quietly=TRUE))
+        stop("package", sQuote("quadprog"), "is needed.  Stopping")
+
+    R.fund = checkData(R.fund, method="matrix")
+    R.style = checkData(R.style, method="matrix")
+
+    # @todo: Missing data is not allowed, use = "pairwise.complete.obs" ?
+
+    style.cols <- dim(R.style)[2]
+
+    # Calculate D and d
+    Dmat = cov(R.style)
+    dvec = cov(R.fund, R.style)
+
+    # To specify A' b >= b_0, we create an nxn matrix Amat and the constraints 
+    # vector b0.  First we tackle Amat.  The first constraint listed above is 
+    # SUM[w_i] = 1.  The left side of the constraint is expressed as a vector 
+    # of 1's:
+    a1 <- rep(1, style.cols)
+
+    # In b0, which represents the right side of the equation, this vector is 
+    # paired with the value '1'.
+
+    # The second constraint sets the lower bound of the weights to zero.  First
+    # we set up an identity matrix.  
+    a2 <- matrix(0, style.cols, style.cols)
+    diag(a2) <- 1
+
+    # It's paired in b0 with a vector of lower bounds set to zeros:
+    w.min <- rep(0, style.cols)
+
+    # Construct A from the two components a1 and a2
+    Amat <- t(rbind(a1, a2))
+
+    # Construct b_0
+    b0 <- c(1, w.min)
+
+
+    # This is where 'meq' comes in.  The ?solve.QP page says:
+    #     meq: the first 'meq' constraints are treated as equality
+    #     constraints, all further as inequality constraints (defaults
+    #     to 0).
+    # I think that the way to interpret this is: if it is set to a value 'q' <= n,
+    # the ordered constraints numbered less than or equal to 'q' are treated as an 
+    # equality constraint.  In this case, we only want to first constraint to be
+    # treated as an equality, so that the weights would sum to exactly 1.  So we
+    # set meq = 1.
+
+    # With 'meq' set to 1, the second constraint (a2) is treated as an inequality,
+    # so each weight is constrainted to be greater than or equal to zero.
+    optimal <- solve.QP(Dmat, dvec, Amat, bvec=b0, meq=1)
+
+    if(all)
+        result = optimal
+    else
+        result = optimal$solution
+
+    # @todo: retain the labels on the weights
+    # @todo: add other values to output, e.g., 
+    #    result <- list(weights = optimal$solution, R.squared = , tracking.error = )
+
+    return(result)
+
+}
