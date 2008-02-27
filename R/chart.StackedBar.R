49ae0ec0c7b9f9a9bad10f3514048c6407cfb6e3
Peter Carl <pcarl@gsb.uchicago.edu>
2008-02-27 04:01:10 +0000

    - added cex.names for sizing xaxis tags
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@712 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.StackedBar.R
+++ R/chart.StackedBar.R
@@ -12,18 +12,21 @@
 # legend("center",legend=colnames(x),fill=gray.colors(11,start=0,end=1),cex=.7,ncol=3, box.col="black")
 # gray.colors(w.columns,start=0,end=1)
 `chart.StackedBar` <- 
-function (w, colorset=1:ncol(w), main = NULL, space = 0, legend.cex = 0.7, cex = 1, las=3, legend.loc="under", ... ) 
+function (w, colorset = NULL, main = NULL, space = 0, legend.cex = 0.7, cex = 1, las=3, legend.loc="under", cex.names = 1, ... ) 
 {
 
     w = checkData(w,method="matrix")
     w.columns = ncol(w)
     w.rows = nrow(w)
 
+    if(is.null(colorset))
+        colorset=1:nrow(w)
+
     if(!is.null(legend.loc)){
         if(legend.loc == "under"){
             layout(rbind(1,2), height=c(5,1), width=1)
             if(las > 1)
-                par(mar=c(max(stringDims(colnames(w))$width)/2, 4, 4, 2)+.1, cex = cex)
+                par(mar=c(max(stringDims(colnames(w))$width)/2, 4, 4, 2)*cex.names +.1, cex = cex)
             else
                 par(mar=c(1,4,4,2)+.1)
             legend.tmp = NULL
@@ -36,7 +39,7 @@ function (w, colorset=1:ncol(w), main = NULL, space = 0, legend.cex = 0.7, cex =
 
 
 
-    barplot(w,col=colorset,space=space, main=main, legend.loc = legend.tmp, las = las, ...)
+    barplot(w,col=colorset,space=space, main=main, legend.loc = legend.tmp, las = las, cex.names = cex.names, ...)
 
     if(!is.null(legend.loc) & legend.loc =="under"){
         par(mar=c(2,2,1,1)+.1)
@@ -57,10 +60,13 @@ function (w, colorset=1:ncol(w), main = NULL, space = 0, legend.cex = 0.7, cex =
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.StackedBar.R,v 1.3 2008-02-26 04:56:59 peter Exp $
+# $Id: chart.StackedBar.R,v 1.4 2008-02-27 04:01:10 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2008/02/26 04:56:59  peter
+# - fixed label calculation to handle correct dimension
+#
 # Revision 1.2  2008/02/26 04:38:53  peter
 # - now handles multiple columns for fund
 # - legend "under" draws correctly
