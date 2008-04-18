090e9fa39dfeff677f1f13183111e831f8cb807d
Peter Carl <pcarl@gsb.uchicago.edu>
2008-04-18 03:59:52 +0000

    - added na.omit to avoid problems with missing data
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@726 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.RollingStyle.R
+++ R/chart.RollingStyle.R
@@ -1,5 +1,5 @@
 `chart.RollingStyle` <-
-function (R.fund, R.style, method = c("constrained","unconstrained","normalized"), width = 12, main = paste("Rolling ", width ,"-Month Style Weights", sep=""), xaxis = TRUE, colorset = (1:12), legend.loc = NULL, na.pad = FALSE, ...)
+function (R.fund, R.style, method = c("constrained","unconstrained","normalized"), width = 12, main = paste(colnames(R.fund)[1]," Rolling ", width ,"-Month Style Weights", sep=""), xaxis = TRUE, colorset = (1:12), legend.loc = "under", na.pad = FALSE, space = 0, ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -10,7 +10,7 @@ function (R.fund, R.style, method = c("constrained","unconstrained","normalized"
     # FUNCTION:
 
     # Transform input data to a data frame
-    R.fund = checkData(R.fund[,1,drop=F], method = "zoo")
+    R.fund = checkData(R.fund[,1,drop=FALSE], method = "zoo")
     R.style = checkData(R.style, method = "zoo")
 
     method = method[1]
@@ -23,14 +23,12 @@ function (R.fund, R.style, method = c("constrained","unconstrained","normalized"
 
 
     # Calculate
-    merged.assets = merge(R.fund, R.style)
-    result = rollapply(data = merged.assets, FUN= function(x) t(style.fit(R.fund= x[,1,drop=FALSE], R.style = x[,-1,drop=FALSE], method = method)$weights), width = width, by = 1, by.column = FALSE, na.pad = na.pad, align = "right")
+    merged.assets = na.omit(merge(R.fund, R.style))
+    result = rollapply(data = merged.assets, FUN= function(x) {t(style.fit(R.fund= x[,1,drop=FALSE], R.style = x[,-1,drop=FALSE], method = method)$weights)}, width = width, by = 1, by.column = FALSE, na.pad = FALSE, align = "right")
 
     colnames(result) = columnnames.style
-#     column.result = zoo(column.result.tmp, order.by = time(column.result))
 
-
-    chart.StackedBar(result, xaxis = xaxis, main = main, legend.loc = legend.loc, col = colorset, ...)
+    chart.StackedBar(t(result), main = main, legend.loc = legend.loc, col = colorset, space = space, ...)
 
 }
 
@@ -42,9 +40,12 @@ function (R.fund, R.style, method = c("constrained","unconstrained","normalized"
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.RollingStyle.R,v 1.1 2008-02-23 05:55:21 peter Exp $
+# $Id: chart.RollingStyle.R,v 1.2 2008-04-18 03:59:52 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2008/02/23 05:55:21  peter
+# - chart demonstrating fund exposures through time
+#
 #
 ###############################################################################
