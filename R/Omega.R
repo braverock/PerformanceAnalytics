8fb1247baf2c4a399e112639de00ad636528b9fd
Peter Carl <pcarl@gsb.uchicago.edu>
2008-06-25 03:50:37 +0000

    - added package test for Hmisc
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@778 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Omega.R
+++ R/Omega.R
@@ -67,6 +67,7 @@ function(R, L = 0, method = c("simple", "interp", "binomial", "blackscholes"), o
         interp = {
 
             # require("Hmisc")
+            stopifnot("package:Hmisc" %in% search() || require("Hmisc",quietly=TRUE))
             a = min(x)
             b = max(x)
 
@@ -98,10 +99,13 @@ function(R, L = 0, method = c("simple", "interp", "binomial", "blackscholes"), o
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: Omega.R,v 1.9 2008-06-02 16:05:19 brian Exp $
+# $Id: Omega.R,v 1.10 2008-06-25 03:50:37 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.9  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.8  2007/03/04 18:37:09  brian
 # - update to use switch on method, more efficient
 #
