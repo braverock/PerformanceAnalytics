74768160edee36232bc9f695c21741bb5429e98c
Peter Carl <pcarl@gsb.uchicago.edu>
2008-06-30 04:22:10 +0000

    - altered calculation to be consistent with Grinold formulation. - thanks to Arthur P.Steinmetz (OppenheimerFunds Inc.)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@797 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/TrackingError.R
+++ R/TrackingError.R
@@ -13,8 +13,8 @@ function (Ra, Rb, scale = 12)
     assetReturns.vec = checkDataVector(Ra)
     benchmarkReturns.vec = checkDataVector(Rb)
 
-    TrackingError = sqrt(sum(assetReturns.vec - benchmarkReturns.vec)^2 / (length(assetReturns.vec) - 1)) * sqrt(scale)
-
+    #TrackingError = sqrt(sum(assetReturns.vec - benchmarkReturns.vec)^2 / (length(assetReturns.vec) - 1)) * sqrt(scale)
+    TrackingError = sd(assetReturns.vec - benchmarkReturns.vec) *  sqrt(scale)
     TrackingError
 
 }
@@ -27,10 +27,13 @@ function (Ra, Rb, scale = 12)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: TrackingError.R,v 1.6 2008-06-02 16:05:19 brian Exp $
+# $Id: TrackingError.R,v 1.7 2008-06-30 04:22:10 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.6  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.5  2007/04/04 00:23:01  brian
 # - typos and minor comment updates
 #
