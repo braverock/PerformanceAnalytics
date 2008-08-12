ad58c2d999a497d5a46dd877053f5cbdfaf7223a
Brian G. Peterson <brian@braverock.com>
2008-08-12 22:52:22 +0000

    - change mean.MM to multivariate_mean fn call to eliminate contention with R core mean fn
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@853 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/MultivariateMoments.R
+++ R/MultivariateMoments.R
@@ -11,7 +11,7 @@
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 ###############################################################################
-# $Id: MultivariateMoments.R,v 1.3 2008-05-07 21:38:24 brian Exp $
+# $Id: MultivariateMoments.R,v 1.4 2008-08-12 22:52:22 brian Exp $
 ###############################################################################
 
 
@@ -39,7 +39,7 @@ M4.MM = function(R){
    return( 1/T*M4 );
 }
 
-mean.MM = function(w,mu){
+multivariate_mean = function(w,mu){
    return( t(w)%*%mu )
 }
 
@@ -56,36 +56,36 @@ kurtosis.MM=function(w,sigma,M4){
 }
 
 SR.StdDev.MM = function(w, mu , sigma ){
-         return( mean.MM(w,mu) / StdDev.MM(w,sigma)   )
+         return( multivariate_mean(w,mu) / StdDev.MM(w,sigma)   )
 }
 
 GVaR.MM = function(w, mu, sigma, p ){
-         return ( -mean.MM(w,mu) - qnorm(1-p)*StdDev.MM(w,sigma)  )
+         return ( -multivariate_mean(w,mu) - qnorm(1-p)*StdDev.MM(w,sigma)  )
 }
 
 SR.GVaR.MM = function(w, mu, sigma, p ){
-          return(  mean.MM(w,mu) / GVaR.MM(w,mu,sigma,p)    )
+          return(  multivariate_mean(w,mu) / GVaR.MM(w,mu,sigma,p)    )
 }
 
 mVaR.MM = function(w, mu, sigma, M3, M4, p ){
           skew = skewness.MM(w,sigma,M3);
           exkurt = kurtosis.MM(w,sigma,M4) - 3;
           z = qnorm(1-p);
-          h = z + (1/6)*(z^2 -1)*skew
-          h = h + (1/24)*(z^3 - 3*z)*exkurt - (1/36)*(2*z^3 - 5*z)*skew^2;
-          return ( -mean.MM(w,mu) - h*StdDev.MM(w,sigma)  )
+          zc = z + (1/6)*(z^2 -1)*skew
+          Zcf = zc + (1/24)*(z^3 - 3*z)*exkurt - (1/36)*(2*z^3 - 5*z)*skew^2;
+          return ( -multivariate_mean(w,mu) - Zcf*StdDev.MM(w,sigma)  )
 }
 
 SR.mVaR.MM = function(w, mu, sigma, M3, M4, p) {
-          return( mean.MM(w,mu) / mVaR.MM(w, mu, sigma, M3, M4, p )     )
+          return( multivariate_mean(w,mu) / mVaR.MM(w, mu, sigma, M3, M4, p )     )
 }
 
 GES.MM = function(w, mu, sigma , p){
-         return ( -mean.MM(w,mu) + dnorm(qnorm(1-p))*StdDev.MM(w,sigma)/(1-p) )
+         return ( -multivariate_mean(w,mu) + dnorm(qnorm(1-p))*StdDev.MM(w,sigma)/(1-p) )
 }
 
 SR.GES.MM = function(w, mu , sigma , p){
-                       return( mean.MM(w,mu) / GES.MM(w, mu, sigma , p)  )
+                       return( multivariate_mean(w,mu) / GES.MM(w, mu, sigma , p)  )
 }
 
 Ipower = function(power,h){
@@ -131,11 +131,11 @@ mES.MM = function(w, mu, sigma, M3 , M4 , p){
           E = E + (1/72)*(  Ipower(6,h) -15*Ipower(4,h)+ 45*Ipower(2,h) - 15*dnorm(h) )*(skew^2)
           E = E/(1-p)
 
-          return (- mean.MM(w,mu) - StdDev.MM(w,sigma)*min(-E,h) )
+          return (- multivariate_mean(w,mu) - StdDev.MM(w,sigma)*min(-E,h) )
 }
 
 SR.mES.MM = function(w, mu, sigma, M3 , M4 , p){
-   return( mean.MM(w,mu) / mES.MM(w, mu, sigma, M3 , M4 , p)  )
+   return( multivariate_mean(w,mu) / mES.MM(w, mu, sigma, M3 , M4 , p)  )
 }
 
 ###############################################################################
@@ -146,10 +146,13 @@ SR.mES.MM = function(w, mu, sigma, M3 , M4 , p){
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: MultivariateMoments.R,v 1.3 2008-05-07 21:38:24 brian Exp $
+# $Id: MultivariateMoments.R,v 1.4 2008-08-12 22:52:22 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2008-05-07 21:38:24  brian
+# - update header, footer, and licencing to reflect moving into PerformanceAnalytics
+#
 # Revision 1.2  2008-01-20 12:07:24  kris
 # Changed function definitions in optim_functions.R and updated the function calls in optimizer.R to these functions
 #
