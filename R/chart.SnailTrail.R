05070ddfc5e064e34007891d2bd8a8cb8c3abaaa
Peter Carl <pcarl@gsb.uchicago.edu>
2008-08-16 03:40:32 +0000

    - added default colorset
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@871 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.SnailTrail.R
+++ R/chart.SnailTrail.R
@@ -1,5 +1,5 @@
 `chart.SnailTrail` <-
-function (R, rf = 0, main = "Annualized Return and Risk", add.names = c("all", "lastonly", "firstandlast", "none"), xlab = "Annualized Risk", ylab = "Annualized Return", add.sharpe = c(1,2,3), colorset = NULL, symbolset = 16, darken = FALSE , legend.loc = NULL, xlim = NULL, ylim = NULL, width = 12, stepsize = 12, lty=1, lwd=2, cex.axis=0.8, cex.main = 1, cex.lab = .9, cex.text = 0.8,...)
+function (R, rf = 0, main = "Annualized Return and Risk", add.names = c("all", "lastonly", "firstandlast", "none"), xlab = "Annualized Risk", ylab = "Annualized Return", add.sharpe = c(1,2,3), colorset = 1:12, symbolset = 16, darken = FALSE , legend.loc = NULL, xlim = NULL, ylim = NULL, width = 12, stepsize = 12, lty=1, lwd=2, cex.axis=0.8, cex.main = 1, cex.lab = .9, cex.text = 0.8,...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -175,10 +175,13 @@ function (R, rf = 0, main = "Annualized Return and Risk", add.names = c("all", "
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.SnailTrail.R,v 1.3 2008-06-26 01:46:26 peter Exp $
+# $Id: chart.SnailTrail.R,v 1.4 2008-08-16 03:40:32 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2008-06-26 01:46:26  peter
+# - removed 'first' attribute
+#
 # Revision 1.2  2008-06-24 02:26:59  peter
 # - changed 'add.labels' to 'add.names' to be consistent with chart.RiskReturnScatter
 #
