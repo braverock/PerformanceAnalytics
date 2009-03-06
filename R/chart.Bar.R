79e7a623f2fc163899c212d3a34b763cdea926bc
Peter Carl <pcarl@gsb.uchicago.edu>
2009-03-06 03:06:44 +0000

    - changed checkData to use default xts
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@929 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.Bar.R
+++ R/chart.Bar.R
@@ -16,7 +16,7 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
     # FUNCTION:
 
     # Transform input data to a matrix
-    x = checkData(R, method = "zoo")
+    x = checkData(R)
 
     chart.TimeSeries(x, type = "h", colorset = colorset, legend.loc = legend.loc, lend="butt",...)
 
@@ -30,10 +30,13 @@ function (R, legend.loc = NULL, colorset = (1:12), ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.Bar.R,v 1.5 2008-10-16 18:45:37 brian Exp $
+# $Id: chart.Bar.R,v 1.6 2009-03-06 03:06:44 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2008-10-16 18:45:37  brian
+# - use checkData with method="zoo" instead of checkDataMatrix
+#
 # Revision 1.4  2008-06-02 16:05:19  brian
 # - update copyright to 2004-2008
 #
