ef2e6356d51bfbb44d575e66d79c82f38853d7cc
Peter Carl <pcarl@gsb.uchicago.edu>
2009-04-01 14:02:49 +0000

    - fixed number ratio and added percentage ratio
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@938 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/UpDownRatios.R
+++ R/UpDownRatios.R
@@ -1,5 +1,5 @@
 `UpDownRatios` <-
-function(R, Rb, method = "capture", side = "up", ...)
+function(Ra, Rb, method = c("capture","number","percent"), side = c("up","down"), ...)
 {# @author Peter Carl
 
     # DESCRIPTION:
@@ -8,10 +8,10 @@ function(R, Rb, method = "capture", side = "up", ...)
     #     return when the benchmark was up (down) divided by the benchmark's
     #     compound return when the benchmark was up (down). The greater (lower)
     #     the value, the better.
-    #   Up (Down) Number Ratio: similarly, this is a measure of the number of
+    #   Up (Down) Numbers Ratio: similarly, this is a measure of the number of
     #     periods that the investment was up (down) when the benchmark was up (down),
     #     divided by the number of periods that the Benchmark was up (down).
-    #   Up (Down) Percentage Ratio: this is a measure of the number of periods
+    #   Up (Down) Percent Ratio: this is a measure of the number of periods
     #     that the investment outperformed the benchmark when the benchmark was
     #     up (down), divided by the number of periods that the benchmark was up (down).
     #     Unlike the prior two metrics, in both cases a higher value is better.
@@ -23,16 +23,15 @@ function(R, Rb, method = "capture", side = "up", ...)
     # side: "Up" or "Down" statistic.
 
     # Outputs:
-    # A data.table of n-period trailing calculations for each column
-    # in x.
+    # A data.table 
 
     # FUNCTION:
 
-    # Transform input data to a data frame
+    Ra = checkData(Ra)
+    Rb = checkData(Rb)
 
-    Ra = checkData(R, method = "zoo")
-    Rb = checkData(Rb, method = "zoo")
-    # rf = checkData(rf, method = "zoo")
+    method = method[1]
+    side = side[1]
 
     # Get dimensions and labels
     columns.a = ncol(Ra)
@@ -41,10 +40,10 @@ function(R, Rb, method = "capture", side = "up", ...)
     columnnames.b = colnames(Rb)
 
     # Calculate
-    for(column.a in 1:columns.a) { # for each asset passed in as R
-        for(column.b in 1:columns.b) { # against each asset passed in as Rb
+#     for(column.a in 1:columns.a) { # for each asset passed in as R
+#         for(column.b in 1:columns.b) { # against each asset passed in as Rb
 
-            merged.assets = merge(Ra[,column.a,drop=FALSE], Rb[,column.b,drop=FALSE])
+            merged.assets = na.omit(merge(Ra[,1,drop=FALSE], Rb[,1,drop=FALSE]))
 
             if(method == "capture" & side == "up") {
                 UpRa = subset(merged.assets[,1], merged.assets[,2] > 0)
@@ -54,73 +53,36 @@ function(R, Rb, method = "capture", side = "up", ...)
                 result = cumRa/cumRb
             }
             if(method == "capture" & side == "down") {
-                DnRa = subset(merged.assets[,1], merged.assets[,2] < 0)
-                DnRb = subset(merged.assets[,2], merged.assets[,2] < 0)
+                DnRa = subset(merged.assets[,1], merged.assets[,2] <= 0)
+                DnRb = subset(merged.assets[,2], merged.assets[,2] <= 0)
                 cumRa = sum(DnRa)
                 cumRb = sum(DnRb)
                 result = cumRa/cumRb
             }
-            ### This isn't right - find a reference
-#             if(method == "number" & side == "up") {
-#                 UpRi = length(subset(merged.assets[,1], merged.assets[,2] > 0))
-#                 UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
-#                 result = UpRi/UpRb
-#             }
-#             if(method == "number" & side == "down") {
-#                 DnRi = length(subset(merged.assets[,1], merged.assets[,2] < 0))
-#                 DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
-#                 result = DnRi/DnRb
-#             }
 
-        }
-    }
+            if(method == "number" & side == "up") {
+                UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > 0) & (merged.assets[,2] > 0)))
+                UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
+                result = UpRi/UpRb
+            }
+            if(method == "number" & side == "down") {
+                DnRi = length(subset(merged.assets[,1], (merged.assets[,1] < 0) & (merged.assets[,2] < 0)))
+                DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
+                result = DnRi/DnRb
+            }
 
+            if(method == "percent" & side == "up") {
+                UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] > 0)))
+                UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
+                result = UpRi/UpRb
+            }
+            if(method == "percent" & side == "down") {
+                DnRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] < 0)))
+                DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
+                result = DnRi/DnRb
+            }
 
-
-# 
-# 
-# 
-#     assetReturns.vec = checkDataVector(R)
-#     benchmarkReturns.vec = checkDataVector(Rb)
-# 
-#     UpRi = subset(assetReturns.vec, assetReturns.vec > 0)
-#     DownRi = subset(assetReturns.vec, assetReturns.vec < 0)
-#     UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
-#     DownRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
-# 
-#     if(method == "capture" & side == "up") {
-#         UpRi = subset(assetReturns.vec, benchmarkReturns.vec > 0)
-#         UpRb = subset(benchmarkReturns.vec, benchmarkReturns.vec > 0)
-#         cumRi = prod(1+UpRi) - 1
-#         cumRb = prod(1+UpRb) - 1
-#         result = cumRi/cumRb
-#     }
-#     if(method == "capture" & side == "down") {
-#         DnRi = subset(assetReturns.vec, benchmarkReturns.vec < 0)
-#         DnRb = subset(benchmarkReturns.vec, benchmarkReturns.vec < 0)
-#         cumRi = prod(1+DnRi) - 1
-#         cumRb = prod(1+DnRb) - 1
-#         result = cumRi/cumRb
-#     }
-#     if(method == "number" & side == "up") {
-#         UpRi = length(subset(assetReturns.vec, benchmarkReturns.vec > 0))
-#         UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
-#         result = UpRi/UpRb
-#     }
-#     if(method == "number" & side == "down") {
-#         DnRi = length(subset(assetReturns.vec, benchmarkReturns.vec < 0))
-#         DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
-#         result = DnRi/DnRb
-#     }
-#     if(method == "percentage" & side == "up") {
-#         UpRi = length(subset(assetReturns.vec, (benchmarkReturns.vec > 0) && (benchmarkReturns.vec < assetReturns.vec)))
-#         UpRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec > 0))
-#         result = UpRi/UpRb
-#     }
-#     if(method == "percentage" & side == "down") {
-#         DnRi = length(subset(assetReturns.vec, (benchmarkReturns.vec < 0) && (benchmarkReturns.vec < assetReturns.vec)))
-#         DnRb = length(subset(benchmarkReturns.vec, benchmarkReturns.vec < 0))
-#         result = DnRi/DnRb
+#         }
 #     }
     result
 }
@@ -133,10 +95,13 @@ function(R, Rb, method = "capture", side = "up", ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: UpDownRatios.R,v 1.5 2008-10-16 18:45:37 brian Exp $
+# $Id: UpDownRatios.R,v 1.6 2009-04-01 14:02:49 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2008-10-16 18:45:37  brian
+# - use checkData with method="zoo" instead of checkDataMatrix
+#
 # Revision 1.4  2008-06-02 16:05:19  brian
 # - update copyright to 2004-2008
 #
