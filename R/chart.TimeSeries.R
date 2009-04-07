5e865ccfd79b25bfcfbe73b1da18676ac0aaa223
Peter Carl <pcarl@gsb.uchicago.edu>
2009-04-07 22:30:52 +0000

    - added cex.* attributes for sizing text elements
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@951 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.TimeSeries.R
+++ R/chart.TimeSeries.R
@@ -1,5 +1,5 @@
 `chart.TimeSeries` <-
-function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, type = "l", lty = 1, lwd = 2, main = NULL, ylab=NULL, xlab="Date", date.format.in="%Y-%m-%d", date.format = "%m/%y", xlim = NULL, ylim = NULL, element.color="darkgray", event.lines = NULL, event.labels = NULL, period.areas = NULL, event.color = "darkgray", period.color = "aliceblue", colorset = (1:12), pch = (1:12), legend.loc = NULL, ylog = FALSE, cex.axis=0.8, cex.legend = 0.8, cex.labels = 0.8, major.ticks='auto', minor.ticks=TRUE, grid.color="lightgray", grid.lty="dotted", ...)
+function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, type = "l", lty = 1, lwd = 2, main = NULL, ylab=NULL, xlab="Date", date.format.in="%Y-%m-%d", date.format = "%m/%y", xlim = NULL, ylim = NULL, element.color="darkgray", event.lines = NULL, event.labels = NULL, period.areas = NULL, event.color = "darkgray", period.color = "aliceblue", colorset = (1:12), pch = (1:12), legend.loc = NULL, ylog = FALSE, cex.axis=0.8, cex.legend = 0.8, cex.lab = 1, cex.eventlabels = 0.8, major.ticks='auto', minor.ticks=TRUE, grid.color="lightgray", grid.lty="dotted", ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -137,7 +137,7 @@ function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, ty
 
         abline(v = event.ind, col = event.color, lty = 2)
         if(!is.null(event.labels)) {
-            text(x=event.ind,y=ylim[2], label = event.labels[number.event.labels], offset = .2, pos = 2, cex = cex.labels, srt=90, col = event.color)
+            text(x=event.ind,y=ylim[2], label = event.labels[number.event.labels], offset = .2, pos = 2, cex = cex.eventlabels, srt=90, col = event.color)
         }
     }
 
@@ -155,12 +155,6 @@ function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, ty
         lines(1:rows, y[,column], col = colorset[column], lwd = lwd[column], pch = pch[column], lty = lty[column], type = type, ...)
     }
 
-#     if (xaxis) {
-#         axis(1, at = lab.ind, lab=rownames[lab.ind], cex.axis = cex.axis, col = elementcolor)
-#         title(xlab = xlab)
-#         # use axis(..., las=3) for vertical labels.
-#     }
-
     if (xaxis) {
         if(minor.ticks)
             axis(1, at=1:NROW(y), labels=FALSE, col='#BBBBBB')
@@ -168,7 +162,7 @@ function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, ty
 
         axis(1, at=ep, labels=names(ep), las=1, lwd=1, mgp=c(3,label.height,0), cex.axis = cex.axis) 
         #axis(1, at = lab.ind, lab=rownames[lab.ind], cex.axis = cex.axis, col = elementcolor)
-        title(xlab = xlab)
+        title(xlab = xlab, cex = cex.lab)
         # use axis(..., las=3) for vertical labels.
     }
 
@@ -189,7 +183,7 @@ function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, ty
     # Add the other titles
     if(is.null(main))
         main=columnnames[1]
-    title(ylab = ylab)
+    title(ylab = ylab, cex = cex.lab)
     title(main = main)
 
 }
@@ -202,10 +196,13 @@ function (R, auto.grid=TRUE, xaxis = TRUE, yaxis = TRUE, yaxis.right = FALSE, ty
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.TimeSeries.R,v 1.15 2009-03-20 03:22:53 peter Exp $
+# $Id: chart.TimeSeries.R,v 1.16 2009-04-07 22:30:52 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.15  2009-03-20 03:22:53  peter
+# - added xts
+#
 # Revision 1.14  2009-03-04 05:14:05  peter
 # - added axTicksByTime from xts for nice xaxis
 #
