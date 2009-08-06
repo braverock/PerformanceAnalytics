4a92647b04d92f60be3c0ac037eed0e0a2ecc1b4
Peter Carl <pcarl@gsb.uchicago.edu>
2009-08-06 02:51:34 +0000

    - fixed max.cex to cap size
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1013 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/textplot.R
+++ R/textplot.R
@@ -1,5 +1,5 @@
 # # Original function from gplots package written by warnes
-# $Id: textplot.R,v 1.2 2009-04-17 04:08:33 peter Exp $
+# $Id: textplot.R,v 1.3 2009-08-06 02:51:34 peter Exp $
 
 # Example using format.df as a pre-processor
 # > textplot(format.df(t(y), na.blanks=F,cdec=c(3,3,1)), row.valign="center", wrap.rownames=20, wrap.colnames=10, cex=1)
@@ -34,7 +34,7 @@ textplot.data.frame <- function(object,
 textplot.matrix <- function(object,
                             halign=c("center","left","right"),
                             valign=c("center","top","bottom"),
-                            cex, max.cex = 1.0, cmar=2, rmar=0.5,
+                            cex, max.cex = 1, cmar=2, rmar=0.5,
                             show.rownames=TRUE, show.colnames=TRUE,
                             hadj=1, vadj=NULL,
                             row.valign="center",
@@ -136,7 +136,6 @@ textplot.matrix <- function(object,
 
       rowheight = apply(object,1, function(X) max(strheight(X,cex=cex)) ) + strheight("(",cex=cex) * (1 + rmar )
       height=sum(rowheight)
-
       if(lastloop) break
 
       cex <- cex / max(width,height)
@@ -146,7 +145,16 @@ textplot.matrix <- function(object,
           lastloop <- TRUE
         }
     }
+    # reset to maximum size if "discovered" size is too large
+    if(cex>max.cex) {
+        cex = max.cex
 
+        colwidth = apply( object, 2, function(XX) max(strwidth(XX, cex=cex)) ) + strwidth("W",cex=cex) * cmar #(1 + cmar ) 
+        width = sum(colwidth)
+
+        rowheight = apply(object,1, function(X) max(strheight(X,cex=cex)) ) + strheight("(",cex=cex) * (1 + rmar )
+        height=sum(rowheight)
+    }
   # setup x alignment for the table
   if(halign=="left")
     xpos <- 0
