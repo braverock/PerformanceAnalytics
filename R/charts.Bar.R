d1704816fb1694f82e9333afccc00e126fd4b4df
Peter Carl <pcarl@gsb.uchicago.edu>
2009-08-18 21:24:00 +0000

    - multiple bar plots function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1014 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/charts.Bar.R
@@ -0,0 +1,85 @@
+`charts.Bar` <-
+function (R,  space = 0, main = "Returns", ...)
+{ # @author Peter Carl
+
+    # DESCRIPTION:
+    # A wrapper to create a small multiples of time series bar charts 
+
+    # FUNCTION:
+
+    # Transform input data to a data frame
+    R = checkData(R)
+
+    # Get dimensions and labels
+    columns = NCOL(R)
+    columnnames = colnames(R)
+
+# Create a page of bar chart panels (using chart.Bar) that are in a single column.
+# Option to add a "total" exposure chart, which shows the positive and negative "gross" exposures in bars
+# and the net exposure in an overlaid line
+# Option to add an "R.squared" panel that shows the R-squared value through time in a line chart
+
+# Establish common ylim across the bar charts
+# Title at the top of the page
+# x-axis in the bottom bar chart and the optional charts? or just in the bottom chart
+# each bar chart title labeled with the factor name, left justified
+# each panel may have a different color from colorset, but default set to the same color across all
+    ymax = max(R, na.rm=TRUE)
+    ymin = min(R, na.rm=TRUE)
+    # mar: a numerical vector of the form c(bottom, left, top, right) which
+    # gives the number of lines of margin to be specified on the four sides
+    # of the plot. The default is c(5, 4, 4, 2) + 0.1
+    op <- par(oma = c(2,0,4,0), mar=c(0,4,0,4))
+    layout(matrix(c(1:columns), nc = 1, byrow = TRUE), width=1)
+    xaxis=FALSE
+    yaxis=TRUE
+    for(i in 1:columns){
+         if(even(i))
+            yaxis.right=TRUE
+         else
+             yaxis.right=FALSE
+        positives = R[,i,drop=FALSE]
+        for(row in 1:length(R[,i,drop=FALSE])){ 
+            positives[row,]=max(0,R[row,i])
+        }
+        negatives = R[,i,drop=FALSE]
+        for(row in 1:length(R[,i,drop=FALSE])){ 
+            negatives[row,]=min(0,R[row,i])
+        }
+        if(i==columns)
+            xaxis = TRUE
+        chart.TimeSeries(positives, type = "h", lend="butt", xaxis=xaxis, main="", ylab=colnames(R)[i], ylim = c(ymin,ymax), yaxis=yaxis, yaxis.right=yaxis.right, col="darkgreen", lwd=2, ...)
+        lines(1:length(R[,1]), negatives, type="h", lend="butt", col="darkred", lwd=2)
+#         chart.Histogram(R[,i,drop=FALSE], xlim=c(ymin,ymax), main="", axes=FALSE)
+        if(i==1)
+            yaxis=FALSE
+    }
+
+    mtext(main,
+        side = 3, outer = TRUE, 
+        font = 2, cex = 1.2, line=1)
+    par(op)
+    
+
+}
+
+###############################################################################
+# R (http://r-project.org/) Econometrics for Performance and Risk Analysis
+#
+# Copyright (c) 2004-2007 Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id: charts.Bar.R,v 1.1 2009-08-18 21:24:00 peter Exp $
+#
+###############################################################################
+# $Log: not supported by cvs2svn $
+# Revision 1.2  2008-04-18 03:59:52  peter
+# - added na.omit to avoid problems with missing data
+#
+# Revision 1.1  2008/02/23 05:55:21  peter
+# - chart demonstrating fund exposures through time
+#
+#
+###############################################################################
