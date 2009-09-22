2a30041111d131b4502b58cf00f9b81358f34ea0
Peter Carl <pcarl@gsb.uchicago.edu>
2009-09-22 02:56:13 +0000

    - added reclass
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1061 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Drawdowns.R
+++ R/Drawdowns.R
@@ -29,7 +29,7 @@ function (R)
     }
 
     colnames(drawdown) = columnnames
-
+    drawdown = reclass(drawdown, x)
     return(drawdown)
 }
 
@@ -41,10 +41,14 @@ function (R)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: Drawdowns.R,v 1.4 2009-08-31 20:51:27 brian Exp $
+# $Id: Drawdowns.R,v 1.5 2009-09-22 02:56:13 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2009-08-31 20:51:27  brian
+# - add new function na.skip to deal with non-contiguous NA's in data, may eventually go to xts
+# - fix components of charts.PerformanceSummary to use na.skip
+#
 # Revision 1.3  2009-06-02 03:14:49  peter
 # - converted internal to xts, removed zoo coersion
 #
