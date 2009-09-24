f2a2a45ba57ca085ba48a6a82ef4d3e69d76c0d7
Brian G. Peterson <brian@braverock.com>
2009-09-24 16:01:58 +0000

    - remove unneeded function params - remove deprecated function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1072 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/VaR.R
+++ R/VaR.R
@@ -1,9 +1,9 @@
 ###############################################################################
-# $Id: VaR.R,v 1.12 2009-08-25 17:43:37 brian Exp $
+# $Id: VaR.R,v 1.13 2009-09-24 16:01:58 brian Exp $
 ###############################################################################
 
 VaR <-
-function (R , p=0.99, method=c("modified","gaussian","historical", "kernel"), clean=c("none","boudt"),  portfolio_method=c("single","component","marginal"), weights=NULL, mu=NULL, sigma=NULL, skew=NULL, exkurt=NULL, m1=NULL, m2=NULL, m3=NULL, m4=NULL, invert=TRUE, ...)
+function (R , p=0.99, method=c("modified","gaussian","historical", "kernel"), clean=c("none","boudt"),  portfolio_method=c("single","component","marginal"), weights=NULL, mu=NULL, sigma=NULL, m3=NULL, m4=NULL, invert=TRUE, ...)
 { # @author Brian G. Peterson
 
     # Descripion:
@@ -87,12 +87,12 @@ function (R , p=0.99, method=c("modified","gaussian","historical", "kernel"), cl
 	    names(weights)<-colnames(R)
             if (is.null(mu)) { mu =  apply(R,2,'mean' ) }
             if (is.null(sigma)) { sigma = cov(R) }
-            if (is.null(m1)) {m1 = multivariate_mean(weights, mu)}
-            if (is.null(m2)) {m2 = StdDev.MM(weights, sigma)}
+            # if (is.null(m1)) {m1 = multivariate_mean(weights, mu)}
+            # if (is.null(m2)) {m2 = StdDev.MM(weights, sigma)}
             if (is.null(m3)) {m3 = M3.MM(R)}
             if (is.null(m4)) {m4 = M4.MM(R)}
-            if (is.null(skew)) { skew = skewness.MM(weights,sigma,m3) }
-            if (is.null(exkurt)) { exkurt = kurtosis.MM(weights,sigma,m4) - 3 }
+            # if (is.null(skew)) { skew = skewness.MM(weights,sigma,m3) }
+            # if (is.null(exkurt)) { exkurt = kurtosis.MM(weights,sigma,m4) - 3 }
 
             switch(method,
                 modified = { return(VaR.CornishFisher.portfolio(p,weights,mu,sigma,m3,m4))},
@@ -111,144 +111,6 @@ function (R , p=0.99, method=c("modified","gaussian","historical", "kernel"), cl
 
 } # end VaR wrapper function
 
-# `VaR.CornishFisher` <-
-# function(R, p=0.99, modified = TRUE, clean=c("none", "boudt"))
-# {   # @author Brian G. Peterson (completed/debugged fn)
-#     # @author Diethelm Wuertz (prototype function)
-#
-#
-#     # Description:
-#
-#     # The limitations of mean Value-at-Risk are well covered in the literature.
-#     # Laurent Favre and Jose-Antonio Galeano published a paper in the
-#     # Fall 2002, volume 5 of the Journal of Alternative Investment,
-#     # "Mean-Modified Value-at-Risk optimization With Hedge Funds",
-#     # that proposed a modified VaR calculation that takes the higher moments
-#     # of non-normal distributions (skewness, kurtosis) into account, and
-#     # collapses to standard (traditional) mean-VaR if the return stream follows a
-#     # standard distribution.
-#     # This measure is now widely cited and used in the literature,
-#     # and is usually referred to as "Modified VaR" or "Modified Cornish-Fisher VaR"
-#
-#     # Diethelm Wuertz's original function was called monthlyVaR, but did not
-#     # contain the required modifications to get to a monthly or an annualized number.
-#     # I have converted it to VaR.CornishFisher, and made the assumption of p=0.99, with an option for p=0.95 and
-#     # a collapse to normal mean VaR.
-#
-#     # FUNCTION:
-#
-#     # compute zc for the probability we want
-#     if ( p >= 0.51 ) {
-#         # looks like p was a percent like .99
-#         p = 1-p
-#     }
-#     zc = qnorm(p)
-#
-#     R = checkData(R, method = "zoo")
-#     columns = ncol(R)
-#     columnnames=colnames(R)
-#     if(clean[1]!="none"){
-#         R = as.matrix(Return.clean(R, method=clean))
-#     }
-#
-#     # FUNCTION:
-#     for(column in 1:columns) {
-#         r = as.vector(na.omit(R[,column]))
-#         if (!is.numeric(r)) stop("The selected column is not numeric")
-#
-#         if (modified) {
-#             s = skewness(r) #skewness of the distribution
-#             k = kurtosis(r) #(excess) kurtosis
-#             Zcf = zc + (((zc^2-1)*s)/6) + (((zc^3-3*zc)*k)/24) - ((((2*zc^3)-5*zc)*s^2)/36)
-#             VaR = (-mean(r) - (Zcf * sd(r)))
-#         } else {
-#             VaR = (-mean(r) - (zc * sd(r)))
-#         }
-#         # check for unreasonable results
-#         if (eval(VaR<0)){ #eval added to get around Sweave bitching
-#             warning(c("VaR calculation produces unreliable result (inverse risk) for column: ",column," : ",VaR))
-#             # set VaR to 0, since inverse risk is unreasonable
-#             VaR=0
-#         }
-#         if (eval(VaR>1)){ #eval added to get around Sweave bitching
-#             warning(c("VaR calculation produces unreliable result (risk over 100%) for column: ",column," : ",VaR))
-#             # set VaR to 1, since greater than 100% is unreasonable
-#             VaR=1
-#         }
-#
-#         VaR=array(VaR)
-#         if (column==1) {
-#             #create data.frame
-#             result=data.frame(VaR=VaR)
-#         } else {
-#             VaR=data.frame(VaR=VaR)
-#             result=cbind(result,VaR)
-#         }
-#     } #end columns loop
-#
-#     if(ncol(result) == 1) {
-#         # some backflips to name the single column zoo object
-#         result = as.numeric(result)
-#     }
-#     else
-#         colnames(result) = columnnames
-#
-#     # Return Value:
-#     result
-# }
-#
-# ###############################################################################
-#
-# `modifiedVaR` <-
-# function(R, p=0.99)
-# {   # @author Brian G. Peterson
-#
-#     # Description:
-#
-#     # This is a wrapper function for VaR.CornishFisher,
-#     # because this measure is often referred to as modifiedVaR
-#
-#     # FUNCTION:
-#     VaR.CornishFisher(R = R, p = p, modified=TRUE)
-#
-# }
-#
-# ###############################################################################
-#
-# `VaR.mean` <-
-# function(R, p=0.95)
-# {   # @author Brian G. Peterson
-#
-#     # Description:
-#
-#     # This is a wrapper function for modified VaR which assumes a normal
-#     # distribution by discounting influence from skewness or kurtosis.
-#
-#     # Wrapper should be used with metrics related to VaR, such as Beyond VaR.
-#
-#     # FUNCTION:
-#     VaR.CornishFisher(R = R, p = p, modified=FALSE)
-#
-# }
-#
-# ###############################################################################
-#
-# `VaR.traditional` <-
-# function(R, p=0.95)
-# {   # @author Brian G. Peterson
-#
-#     # Description:
-#
-#     # This is a wrapper function for modified VaR which assumes a normal
-#     # distribution by discounting influence from skewness or kurtosis.
-#
-#     # Wrapper should be used with metrics related to VaR, such as Beyond VaR.
-#
-#     # FUNCTION:
-#     VaR.CornishFisher(R = R, p = p, modified=FALSE)
-#
-# }
-
 ###############################################################################
 # R (http://r-project.org/) Econometrics for Performance and Risk Analysis
 #
@@ -257,10 +119,14 @@ function (R , p=0.99, method=c("modified","gaussian","historical", "kernel"), cl
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: VaR.R,v 1.12 2009-08-25 17:43:37 brian Exp $
+# $Id: VaR.R,v 1.13 2009-09-24 16:01:58 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.12  2009-08-25 17:43:37  brian
+# - updates to support Marginal VaR
+# - use reclass() in Return.portfolio to return xts object
+#
 # Revision 1.11  2009-08-25 15:21:07  brian
 # - eliminate deprecated multi-argument return
 #
