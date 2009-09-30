75b132fd1969ceb30df8b7d183904a628c966003
Peter Carl <pcarl@gsb.uchicago.edu>
2009-09-30 03:00:04 +0000

    - added periodicity for setting scale
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1082 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/StdDev.annualized.R
+++ R/StdDev.annualized.R
@@ -1,26 +1,37 @@
 `sd.multiperiod` <-
-function (x, na.rm=TRUE, periods = 12, ...)
+function (x, scale = NA)
 {
     if (is.vector(x)) {
-        x = checkData (x,na.rm=na.rm, method="vector", ...=...)
         #scale standard deviation by multiplying by the square root of the number of periods to scale by
-        sqrt(periods)*sd(x, na.rm=na.rm)
+        sqrt(scale)*sd(x, na.rm=TRUE)
     } else {
-        x = checkData (x,na.rm=na.rm, method = "zoo", ...=...)
-        apply(x, 2, sd.multiperiod, na.rm = na.rm, periods=periods, ...=...)
+        x = checkData (x)
+        if(is.na(scale)) {
+            freq = periodicity(x)
+            switch(freq$scale,
+                minute = {stop("Data periodicity too high")},
+                hourly = {stop("Data periodicity too high")},
+                daily = {scale = 252},
+                weekly = {scale = 52},
+                monthly = {scale = 12},
+                quarterly = {scale = 4},
+                yearly = {scale = 1}
+            )
+        }
+        apply(x, 2, sd.multiperiod, scale=scale)
     }
 }
 
 `sd.annualized` <-
-function (x, na.rm=TRUE, periods = 12, ...)
+function (x, scale = NA)
 {   # wrapper function for backwards compatibility
-    sd.multiperiod(x, na.rm=na.rm, periods = periods, ...=...)
+    sd.multiperiod(x, scale = scale)
 }
 
 `StdDev.annualized` <-
-function (Ra, na.rm=TRUE, scale = 12, ...)
+function (Ra, scale = NA)
 {   # wrapper function for backwards compatibility
-    sd.multiperiod(Ra, na.rm=na.rm, periods = scale, ...=...)
+    sd.multiperiod(Ra, scale = scale)
 }
 
 ###############################################################################
@@ -45,10 +56,13 @@ function(Ra) {
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: StdDev.annualized.R,v 1.14 2008-10-14 14:37:29 brian Exp $
+# $Id: StdDev.annualized.R,v 1.15 2009-09-30 03:00:04 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.14  2008-10-14 14:37:29  brian
+# - convert from matrix or data.frame to zoo in checkData call
+#
 # Revision 1.13  2008-06-02 16:05:19  brian
 # - update copyright to 2004-2008
 #
