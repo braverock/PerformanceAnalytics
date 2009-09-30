014ab4750459d98247308f5dd60b4c72266d6dd2
Peter Carl <pcarl@gsb.uchicago.edu>
2009-09-30 02:22:59 +0000

    - added multi-column support
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1080 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/CAPM.utils.R
+++ R/CAPM.utils.R
@@ -2,51 +2,65 @@
 # CAPM.alpha and CAPM.beta could probably have gone in here too, but they're already in separate files
 
 `CAPM.CML.slope` <-
-function (Rb, rf = 0 )
+function (Rb, Rf = 0 )
 { #author Brian G. Peterson
 
   #the Capital Market Line slope is a wrapper for the Sharpe Ratio on the benchmark asset
   #
   # Rb = Return vector of the benchmark or market portfolio
-  return(SharpeRatio(Rb,rf))
+  return(SharpeRatio(Rb,Rf))
 }
 
 `CAPM.CML` <-
-function (Ra, Rb, rf = 0)
+function (Ra, Rb, Rf = 0)
 { #@author Brian G. Peterson
 
-    Ra = checkData(Ra, method="zoo")
-    Rb = checkData(Rb, method="zoo")
-    rf = checkData(rf, method="zoo")
+    Ra = checkData(Ra)
+    Rb = checkData(Rb)
+    if(!is.null(dim(Rf)))
+        Rf = checkData(Rf)
 
-    if (length(Ra) != length(Rb))
-        stop("Returns to be assessed have unequal time periods. Are there NAs in the data?")
+    Ra.ncols = NCOL(Ra) 
+    Rb.ncols = NCOL(Rb)
 
-    CML = mean(rf) + CAPM.CML.slope(Rb, rf)*mean(Ra)
+    pairs = expand.grid(1:Ra.ncols, 1:Rb.ncols)
 
-    return(CML)
+    cml <-function (Ra, Rb, Rf)
+    {
+        CML = mean(Rf, na.rm=TRUE) + CAPM.CML.slope(Rb, Rf)*mean(Ra, na.rm=TRUE)
+        return(CML)
+    }
+
+    result = apply(pairs, 1, FUN = function(n, Ra, Rb, Rf) cml(Ra[,n[1]], Rb[,n[2]], Rf), Ra = Ra, Rb = Rb, Rf = Rf)
+
+    if(length(result) ==1)
+        return(result)
+    else {
+        dim(result) = c(Ra.ncols, Rb.ncols)
+        colnames(result) = colnames(Rb)
+        rownames(result) = colnames(Ra)
+        return(t(result))
+    }
 }
 
 `CAPM.RiskPremium` <-
-function (Ra, rf = 0)
+function (Ra, Rf = 0)
 { #@author Brian G. Peterson
 
-    Ra = checkDataVector(Ra)
-    rf = checkDataVector(rf)
+    Ra = checkData(Ra)
+    if(!is.null(dim(Rf)))
+        Rf = checkData(Rf)
 
-    riskpremium = mean(Ra - rf)
-
-    return (riskpremium)
+    xRa = Return.excess(Ra, Rf)
+    result = apply(xRa, 2, mean, na.rm=TRUE)
+    return (result)
 }
 
 `CAPM.SML.slope` <-
-function (Rb, rf = 0)
+function (Rb, Rf = 0)
 { #@author Brian G. Peterson
 
-    Rb = checkDataVector(Rb)
-
-    SML.slope = 1/CAPM.RiskPremium(Rb, rf)
-
+    SML.slope = 1/CAPM.RiskPremium(Rb, Rf)
     return(SML.slope)
 }
 
@@ -58,10 +72,13 @@ function (Rb, rf = 0)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: CAPM.utils.R,v 1.6 2008-06-02 16:05:19 brian Exp $
+# $Id: CAPM.utils.R,v 1.7 2009-09-30 02:22:59 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.6  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.5  2007/08/15 20:08:46  brian
 # - fix warning for quote formatting
 #
--- R/SharpeRatio.R
+++ R/SharpeRatio.R
@@ -1,5 +1,5 @@
 `SharpeRatio` <-
-function (Ra, rf = 0)
+function (Ra, Rf = 0)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -20,11 +20,21 @@ function (Ra, rf = 0)
     # data being input (e.g., monthly data -> monthly SR)
 
     # FUNCTION:
+    Ra = checkData(Ra)
+    if(!is.null(dim(Rf)))
+        Rf = checkData(Rf)
 
-    Ra = checkData(Ra, method = "zoo")
-    Ra.excess = Return.excess(Ra, rf)
-    return(mean(Ra.excess)/sd(Ra.excess))
+    xRa = Return.excess(Ra, Rf)
+    colnames(xRa) = colnames(Ra)
+    sr <-function (xRa)
+    {
+        xRa = na.omit(xRa)
+        SR = mean(xRa)/sd(xRa)
+        SR
+    }
 
+    result = apply(xRa, 2, sr)
+    return (result)
 }
 
 ###############################################################################
@@ -35,10 +45,13 @@ function (Ra, rf = 0)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: SharpeRatio.R,v 1.7 2008-06-02 16:05:19 brian Exp $
+# $Id: SharpeRatio.R,v 1.8 2009-09-30 02:22:33 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.7  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.6  2007/07/12 21:45:09  brian
 # -calculate stddev on excess return to account for a rf series, per Sharpe paper
 #
