4b9aeb9fa0c97e251482128e502632d78acd0677
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-01 03:07:37 +0000

    - added multi-column support
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1091 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/UpDownRatios.R
+++ R/UpDownRatios.R
@@ -33,58 +33,66 @@ function(Ra, Rb, method = c("capture","number","percent"), side = c("up","down")
     method = method[1]
     side = side[1]
 
-    # Get dimensions and labels
-    columns.a = ncol(Ra)
-    columns.b = ncol(Rb)
-    columnnames.a = colnames(Ra)
-    columnnames.b = colnames(Rb)
+    Ra.ncols = NCOL(Ra) 
+    Rb.ncols = NCOL(Rb)
 
-    # Calculate
-#     for(column.a in 1:columns.a) { # for each asset passed in as R
-#         for(column.b in 1:columns.b) { # against each asset passed in as Rb
+    pairs = expand.grid(1:Ra.ncols, 1:Rb.ncols) 
+    # @todo: expand.grid(1:3,c("up","down"),1:2)
+    # deliver a list by benchmark with 'up' and 'down' statistics together
 
-            merged.assets = na.omit(merge(Ra[,1,drop=FALSE], Rb[,1,drop=FALSE]))
+    udr <-function (Ra, Rb, method, side)
+    {
+        merged.assets = na.omit(merge(as.xts(Ra), as.xts(Rb)))
 
-            if(method == "capture" & side == "up") {
-                UpRa = subset(merged.assets[,1], merged.assets[,2] > 0)
-                UpRb = subset(merged.assets[,2], merged.assets[,2] > 0)
-                cumRa = sum(UpRa)
-                cumRb = sum(UpRb)
-                result = cumRa/cumRb
-            }
-            if(method == "capture" & side == "down") {
-                DnRa = subset(merged.assets[,1], merged.assets[,2] <= 0)
-                DnRb = subset(merged.assets[,2], merged.assets[,2] <= 0)
-                cumRa = sum(DnRa)
-                cumRb = sum(DnRb)
-                result = cumRa/cumRb
-            }
+        if(method == "capture" & side == "up") {
+            UpRa = subset(merged.assets[,1], merged.assets[,2] > 0)
+            UpRb = subset(merged.assets[,2], merged.assets[,2] > 0)
+            cumRa = sum(UpRa)
+            cumRb = sum(UpRb)
+            result = cumRa/cumRb
+        }
+        if(method == "capture" & side == "down") {
+            DnRa = subset(merged.assets[,1], merged.assets[,2] <= 0)
+            DnRb = subset(merged.assets[,2], merged.assets[,2] <= 0)
+            cumRa = sum(DnRa)
+            cumRb = sum(DnRb)
+            result = cumRa/cumRb
+        }
 
-            if(method == "number" & side == "up") {
-                UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > 0) & (merged.assets[,2] > 0)))
-                UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
-                result = UpRi/UpRb
-            }
-            if(method == "number" & side == "down") {
-                DnRi = length(subset(merged.assets[,1], (merged.assets[,1] < 0) & (merged.assets[,2] < 0)))
-                DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
-                result = DnRi/DnRb
-            }
+        if(method == "number" & side == "up") {
+            UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > 0) & (merged.assets[,2] > 0)))
+            UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
+            result = UpRi/UpRb
+        }
+        if(method == "number" & side == "down") {
+            DnRi = length(subset(merged.assets[,1], (merged.assets[,1] < 0) & (merged.assets[,2] < 0)))
+            DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
+            result = DnRi/DnRb
+        }
 
-            if(method == "percent" & side == "up") {
-                UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] > 0)))
-                UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
-                result = UpRi/UpRb
-            }
-            if(method == "percent" & side == "down") {
-                DnRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] < 0)))
-                DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
-                result = DnRi/DnRb
-            }
+        if(method == "percent" & side == "up") {
+            UpRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] > 0)))
+            UpRb = length(subset(merged.assets[,2], merged.assets[,2] > 0))
+            result = UpRi/UpRb
+        }
+        if(method == "percent" & side == "down") {
+            DnRi = length(subset(merged.assets[,1], (merged.assets[,1] > merged.assets[,2]) & (merged.assets[,2] < 0)))
+            DnRb = length(subset(merged.assets[,2], merged.assets[,2] < 0))
+            result = DnRi/DnRb
+        }
+        return(result)
+    }
 
-#         }
-#     }
-    result
+    result = apply(pairs, 1, FUN = function(n, Ra, Rb, method, side) udr(Ra[,n[1]], Rb[,n[2]], method, side), Ra = Ra, Rb = Rb, method = method, side = side)
+
+    if(length(result) ==1)
+        return(result)
+    else {
+        dim(result) = c(Ra.ncols, Rb.ncols)
+        colnames(result) = colnames(Rb)
+        rownames(result) = colnames(Ra)
+        return(t(result))
+    }
 }
 
 ###############################################################################
@@ -95,10 +103,13 @@ function(Ra, Rb, method = c("capture","number","percent"), side = c("up","down")
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: UpDownRatios.R,v 1.7 2009-04-07 22:15:25 peter Exp $
+# $Id: UpDownRatios.R,v 1.8 2009-10-01 03:07:37 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.7  2009-04-07 22:15:25  peter
+# - removed unused dot dot dot
+#
 # Revision 1.6  2009-04-01 14:02:49  peter
 # - fixed number ratio and added percentage ratio
 #
