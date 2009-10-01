11a3dec3941527d2ac33ac5a87cb56cfa04c8c3b
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-01 01:45:17 +0000

    - added multi-column support - added periodicity check for scale
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1085 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/CalmarRatio.R
+++ R/CalmarRatio.R
@@ -1,5 +1,5 @@
 `CalmarRatio` <-
-function (Ra, scale=12)
+function (Ra, scale = NA)
 { # @author Brian G. Peterson
 
     # DESCRIPTION:
@@ -12,14 +12,26 @@ function (Ra, scale=12)
 
     # FUNCTION:
 
-    Ra = checkData(Ra, method = "zoo")
+    Ra = checkData(Ra)
+    if(is.na(scale)) {
+        freq = periodicity(Ra)
+        switch(freq$scale,
+            minute = {stop("Data periodicity too high")},
+            hourly = {stop("Data periodicity too high")},
+            daily = {scale = 252},
+            weekly = {scale = 52},
+            monthly = {scale = 12},
+            quarterly = {scale = 4},
+            yearly = {scale = 1}
+        )
+    }
     annualized_return = Return.annualized(Ra, scale=scale)
-    drawdown          = abs(maxDrawdown(Ra))
+    drawdown = abs(maxDrawdown(Ra))
     return(annualized_return/drawdown)
 }
 
 `SterlingRatio` <-
-function (Ra, scale=12, excess=.1)
+function (Ra, scale=NA, excess=.1)
 { # @author Brian G. Peterson
 
     # DESCRIPTION:
@@ -32,9 +44,21 @@ function (Ra, scale=12, excess=.1)
 
     # FUNCTION:
 
-    Ra = checkData(Ra, method = "zoo")
+    Ra = checkData(Ra)
+    if(is.na(scale)) {
+        freq = periodicity(Ra)
+        switch(freq$scale,
+            minute = {stop("Data periodicity too high")},
+            hourly = {stop("Data periodicity too high")},
+            daily = {scale = 252},
+            weekly = {scale = 52},
+            monthly = {scale = 12},
+            quarterly = {scale = 4},
+            yearly = {scale = 1}
+        )
+    }
     annualized_return = Return.annualized(Ra, scale=scale)
-    drawdown          = abs(maxDrawdown(Ra)-excess)
+    drawdown = abs(maxDrawdown(Ra)-excess)
     return(annualized_return/drawdown)
 }
 
@@ -46,10 +70,13 @@ function (Ra, scale=12, excess=.1)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: CalmarRatio.R,v 1.2 2008-06-02 16:05:19 brian Exp $
+# $Id: CalmarRatio.R,v 1.3 2009-10-01 01:45:17 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.1  2007/06/23 12:06:08  brian
 # - initial revision of Calmar and Sterling Ratio functions and docs
 #   originally requested by Khanh Nguyen <chaokhanh@yahoo.com>
