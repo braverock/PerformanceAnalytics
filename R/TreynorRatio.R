5f85e28ae56225b84f915548c1c670312aaae6f3
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-01 01:47:21 +0000

    - added multi-column support - added periodicity for scaling
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1086 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/TreynorRatio.R
+++ R/TreynorRatio.R
@@ -1,20 +1,54 @@
 `TreynorRatio` <-
-function (Ra, Rb, rf = 0, scale = 12, ...)
+function (Ra, Rb, Rf = 0, scale = NA)
 { # @author Peter Carl
 
     # DESCRIPTION:
     #
 
     # FUNCTION:
+    Ra = checkData(Ra)
+    Rb = checkData(Rb)
+    if(!is.null(dim(Rf)))
+        Rf = checkData(Rf)
 
-    Ra = checkData(Ra, method = "vector", ...=...)
-    Rb = checkData(Rb, method = "vector", ...=...)
+    Ra.ncols = NCOL(Ra) 
+    Rb.ncols = NCOL(Rb)
 
-    beta = CAPM.beta(Ra, Rb, rf = rf, ...=...)
-    Re = Return.excess(Ra, rf=rf, ...=...)
-    treynorRatio = (Return.annualized(Re, scale = scale, ...=...))/beta
-    treynorRatio
+    pairs = expand.grid(1:Ra.ncols, 1:Rb.ncols)
 
+    xRa = Return.excess(Ra, Rf)
+    xRb = Return.excess(Rb, Rf)
+
+    if(is.na(scale)) {
+        freq = periodicity(Ra)
+        switch(freq$scale,
+            minute = {stop("Data periodicity too high")},
+            hourly = {stop("Data periodicity too high")},
+            daily = {scale = 252},
+            weekly = {scale = 52},
+            monthly = {scale = 12},
+            quarterly = {scale = 4},
+            yearly = {scale = 1}
+        )
+    }
+
+    tr <-function (xRa, xRb, scale)
+    {
+        beta = CAPM.beta(xRa, xRb)
+        TR = (Return.annualized(xRa, scale = scale))/beta
+        TR
+    }
+
+    result = apply(pairs, 1, FUN = function(n, xRa, xRb, scale) tr(xRa[,n[1]], xRb[,n[2]], scale), xRa = xRa, xRb = xRb, scale = scale)
+
+    if(length(result) ==1)
+        return(result)
+    else {
+        dim(result) = c(Ra.ncols, Rb.ncols)
+        colnames(result) = colnames(Rb)
+        rownames(result) = colnames(Ra)
+        return(t(result))
+    }
 }
 
 ###############################################################################
@@ -25,10 +59,13 @@ function (Ra, Rb, rf = 0, scale = 12, ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: TreynorRatio.R,v 1.7 2008-06-02 16:05:19 brian Exp $
+# $Id: TreynorRatio.R,v 1.8 2009-10-01 01:47:21 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.7  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.6  2007/08/30 12:18:32  brian
 # - add passing of dots
 #
