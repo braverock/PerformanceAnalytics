994e14b26b947bedf2eb02e4c5be7f6d9b0354f7
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-02 19:11:00 +0000

    - added relative value table function - removed window, using xts:::last - revised periodicity labeling for changes in xts:::periodicity
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/table.RollingPeriods.R
+++ R/table.RollingPeriods.R
@@ -1,5 +1,5 @@
-`table.RollingPeriods` <-
-function (R,  periods = subset(c(3,6,9,12,18,24,36,48), c(3,6,9,12,18,24,36,48)
+`table.TrailingPeriods` <-
+function (R,  periods = subset(c(12,36,60), c(12,36,60)
 < length(as.matrix(R[,1]))), FUNCS=c("mean","sd"), funcs.names = c("Average", "Std Dev"), digits = 4, ...)
 {# @author Peter Carl
 
@@ -14,22 +14,13 @@ function (R,  periods = subset(c(3,6,9,12,18,24,36,48), c(3,6,9,12,18,24,36,48)
 
     # FUNCTION:
 
-    R = checkData(R, method="zoo") # must be zoo for now, until window works w xts
+    R = checkData(R)
 
     # Set up dimensions and labels
     columns = ncol(R)
     columnnames = colnames(R)
     freq = periodicity(R)
-
-    switch(freq$scale,
-        minute = {freq.lab = "minute"},
-        hourly = {freq.lab = "hour"},
-        daily = {freq.lab = "day"},
-        weekly = {freq.lab = "week"},
-        monthly = {freq.lab = "month"},
-        quarterly = {freq.lab = "quarter"},
-        yearly = {freq.lab = "year"}
-    )
+    freq.lab = freq$label
 
     if(length(FUNCS) != length(funcs.names)) {
         warning("The length of the names vector is unequal to the length of the functions vector, so using FUNCS for naming.")
@@ -44,20 +35,10 @@ function (R,  periods = subset(c(3,6,9,12,18,24,36,48), c(3,6,9,12,18,24,36,48)
         values = vector('numeric', 0)
         column.data = na.omit(R[,column,drop=FALSE])
 
-        # Find the last data point in the column
-        end.index = length(column.data)
-
         for(FUNC in FUNCS) {
             func.name = funcs.names[grep(FUNC, FUNCS)]
             for(period in periods) {
-                window.data = zoo(NA)
-
-                start.index = (length(column.data) - period + 1)
-
-                window.data = window(column.data, start = time(column.data)[start.index], end = time(column.data)[end.index])
-
-                values = c(values, apply(as.matrix(window.data), FUN = FUNC, ..., MARGIN = 2))
-
+                values = c(values, apply(as.matrix(last(column.data, period)), FUN = FUNC, ..., MARGIN = 2))
                 valueNames = c(valueNames,paste("Last", period, freq.lab, func.name, sep=" "))
             }
         }
@@ -75,45 +56,89 @@ function (R,  periods = subset(c(3,6,9,12,18,24,36,48), c(3,6,9,12,18,24,36,48)
     ans = base::round(resultingtable, digits)
     ans
 
-    # An example:
-    #     > table.RollingPeriods(monthlyReturns.ts,rf=.04/12,FUN=c("mean","sd","SharpeRatio"))
-    #                                 Actual    S&P500TR
-    #     Last 3 month mean         0.005833333 0.021533333
-    #     Last 6 month mean         0.005816667 0.007816667
-    #     Last 9 month mean         0.012533333 0.013033333
-    #     Last 12 month mean        0.009450000 0.008466667
-    #     Last 18 month mean        0.007516667 0.010100000
-    #     Last 24 month mean        0.006045833 0.006870833
-    #     Last 36 month mean        0.009169444 0.013061111
-    #     Last 3 month sd           0.013250786 0.019237030
-    #     Last 6 month sd           0.014797489 0.020937470
-    #     Last 9 month sd           0.017013230 0.020696376
-    #     Last 12 month sd          0.016328920 0.021764455
-    #     Last 18 month sd          0.017866704 0.021683526
-    #     Last 24 month sd          0.016994346 0.021892553
-    #     Last 36 month sd          0.026235013 0.025764242
-    #     Last 3 month SharpeRatio  1.574876283 4.371462171
-    #     Last 6 month SharpeRatio  1.406103795 1.350340592
-    #     Last 9 month SharpeRatio  2.735414338 2.344855957
-    #     Last 12 month SharpeRatio 2.112323000 1.412139876
-    #     Last 18 month SharpeRatio 1.519161292 1.706268724
-    #     Last 24 month SharpeRatio 1.274189604 1.129225354
-    #     Last 36 month SharpeRatio 1.273709661 1.887926464
+}
+
+`table.TrailingPeriodsRel` <-
+function (R, Rb, periods = subset(c(12,36,60), c(12,36,60)
+< length(as.matrix(R[,1]))), FUNCS=c("cor","CAPM.beta"), funcs.names = c("Correlation", "Beta"), digits = 4, ...)
+{# @author Peter Carl
+
+    # DESCRIPTION:
+    #  Rolling Periods Summary: Statistics and Stylized Facts
+
+    # Inputs:
+    # Assumes an input of monthly returns
+
+    # Output:
+    # A table of estimates of rolling period return measures
+
+    # FUNCTION:
+
+    R = checkData(R)
+Rb=checkData(Rb)
+
+    # Set up dimensions and labels
+    columns = ncol(R)
+    columns.b = ncol(Rb)
+    columnnames = colnames(R)
+    columnnames.b = colnames(Rb)
+    freq = periodicity(R)
+    freq.lab = freq$label
+
+    if(length(FUNCS) != length(funcs.names)) {
+        warning("The length of the names vector is unequal to the length of the functions vector, so using FUNCS for naming.")
+        funcs.names = NA
+    }
+    if(is.na(funcs.names[1]))
+        funcs.names = FUNCS
+
+    # for each column in the matrix, do the following:
+    for(column in 1:columns) {
+      for(column.b in 1:columns.b){
+        valueNames = vector('character', 0)
+        values = vector('numeric', 0)
+        merged.data = na.omit(merge(R[,column,drop=FALSE],Rb[,column.b,drop=FALSE]))
+
+        for(FUNC in FUNCS) {
+            func.name = funcs.names[grep(FUNC, FUNCS)]
+            for(period in periods) {
+                values = c(values, apply(last(merged.data[,1,drop=FALSE], period), FUN = FUNC, last(merged.data[,2,drop=FALSE], period), ..., MARGIN = 2))
+                valueNames = c(valueNames,paste("Last", period, freq.lab, func.name, "to", columnnames.b[column.b], sep=" "))
+            }
+        }
+    
+        if(column == 1) {
+            resultingtable = data.frame(Value = values, row.names = valueNames)
+        }
+
+        else {
+            nextcolumn = data.frame(Value = values, row.names = valueNames)
+            resultingtable = cbind(resultingtable, nextcolumn)
+        }
+      }
+    }
+
+    colnames(resultingtable) = columnnames
+    ans = base::round(resultingtable, digits)
+    ans
 
 }
 
 ###############################################################################
 # R (http://r-project.org/) Econometrics for Performance and Risk Analysis
 #
-# Copyright (c) 2004-2008 Peter Carl and Brian G. Peterson
+# Copyright (c) 2004-2009 Peter Carl and Brian G. Peterson
 #
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: table.RollingPeriods.R,v 1.8 2009-04-18 02:56:53 peter Exp $
+# $Id: table.RollingPeriods.R,v 1.9 2009-10-02 19:11:00 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.8  2009-04-18 02:56:53  peter
+# - argument cleanup and codoc issues
+#
 # Revision 1.7  2009-04-14 03:45:02  peter
 # - smarter labeling of frequency scale
 #
