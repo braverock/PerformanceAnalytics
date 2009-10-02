f0d54b07f2898896b419e44ba03e1e86adfa1f7e
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-02 18:44:10 +0000

    - revamped to provide xtsible windows
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1099 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/apply.rolling.R
+++ R/apply.rolling.R
@@ -1,49 +1,40 @@
 `apply.rolling` <-
-function (R, width = 12, FUN = "mean", na.pad = TRUE, ...)
+function (R, width, trim = TRUE, gap = 12, by = 1, FUN = "mean", ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
-    # A wrapper to create timeseries object of rolling performance metrics 
-
-    # Inputs:
-    # R: a matrix, data frame, or timeSeries of returns
-    # FUN: any function that can be evaluated using a single set of returns
-    #   (e.g., rolling beta won't work, but Return.annualizeds will)
-
-    # Outputs:
-    # A timeseries object of the calculated series
-
     # FUNCTION:
-
-    # Transform input data to a matrix
-    x = checkData(R, method = "zoo")
-
-    # Get dimensions and labels
-    columns = ncol(x)
-    columnnames = colnames(x)
-
-    # Calculate
+    R = checkData(R)
+    columns=NCOL(R)
+    result = xts(, order.by = time(R))
 
     for(column in 1:columns) {
-        # the drop=FALSE flag is essential for when the zoo object only has one column
-        column.Return.calc = rollapply(na.omit(x[,column,drop=FALSE]), width = width, FUN = FUN, ..., na.pad = na.pad, align = "right")
-        if(column == 1)
-            Return.calc = column.Return.calc
+        Ri = na.omit(R[,column,drop=FALSE])
+        rows = length(Ri)
+        dates=time(Ri)
+        steps = seq(from = rows, to = gap, by = -by)
+        calcs = matrix()
+
+        if(width == 0) { # from inception
+            gap = gap
+        }
         else
-            Return.calc = merge(Return.calc,column.Return.calc)
+            gap = width
+
+        for(row in steps[order(steps)]) {
+            if (width == 0)  # from inception
+                r = Ri[1:row]
+            else
+                r = Ri[(row-width+1):row]
+            calc = apply(r, MARGIN = 2, FUN = FUN, ...=...)
+            calcs = rbind(calcs, calc)
+        }
+        calcs = xts(calcs[-1],order.by=dates[steps[order(steps)]])
+        result = merge(result, calcs)
     }
-
-    if(ncol(Return.calc) == 1) {
-        # some backflips to name the single column zoo object
-        Return.calc = as.matrix(Return.calc)
-        colnames(Return.calc) = columnnames
-        Return.calc = zoo(Return.calc, order.by = rownames(Return.calc))
-    }
-    else
-        colnames(Return.calc) = columnnames
-
-    return(Return.calc)
-
+    colnames(result)=colnames(R)
+    result = reclass(result, R)
+    return (result)
 }
 
 ###############################################################################
@@ -54,10 +45,13 @@ function (R, width = 12, FUN = "mean", na.pad = TRUE, ...)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: apply.rolling.R,v 1.3 2008-06-02 16:05:19 brian Exp $
+# $Id: apply.rolling.R,v 1.4 2009-10-02 18:44:10 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2008-06-02 16:05:19  brian
+# - update copyright to 2004-2008
+#
 # Revision 1.2  2007/04/04 02:45:04  peter
 # - added some backflips to name the single column zoo object
 #
