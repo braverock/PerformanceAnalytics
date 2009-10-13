85bbb2d04710dbf3a3bca757fb74ce4606de498d
Peter Carl <pcarl@gsb.uchicago.edu>
2009-10-13 14:29:04 +0000

    - fixed ordering of results
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/InformationRatio.R
+++ R/InformationRatio.R
@@ -40,7 +40,7 @@ function (Ra, Rb, scale = NA)
     if(length(result) ==1)
         return(result)
     else {
-        result = matrix(result, ncol=Ra.ncols, nrow=Rb.ncols)
+        result = matrix(result, ncol=Ra.ncols, nrow=Rb.ncols, byrow=TRUE)
         rownames(result) = paste("Information Ratio:", colnames(Rb))
         colnames(result) = colnames(Ra)
         return(result)
@@ -55,10 +55,13 @@ function (Ra, Rb, scale = NA)
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: InformationRatio.R,v 1.11 2009-10-10 12:40:08 brian Exp $
+# $Id: InformationRatio.R,v 1.12 2009-10-13 14:29:04 peter Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.11  2009-10-10 12:40:08  brian
+# - update copyright to 2004-2009
+#
 # Revision 1.10  2009-10-06 15:14:44  peter
 # - fixed rownames
 # - fixed scale = 12 replacement errors
