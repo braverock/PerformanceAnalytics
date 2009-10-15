b031636e6c7e2c6143595503bea1671a362a6742
Brian G. Peterson <brian@braverock.com>
2009-10-15 21:50:19 +0000

    - updates to add automatic periodicity to chart labels, and support different frequency data
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/trunk@1191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/chart.RollingStyle.R
+++ R/chart.RollingStyle.R
@@ -1,5 +1,5 @@
 `chart.RollingStyle` <-
-function (R.fund, R.style, method = c("constrained","unconstrained","normalized"), leverage = FALSE, width = 12, main = paste(colnames(R.fund)[1]," Rolling ", width ,"-Month Style Weights", sep=""), space = 0, ...)
+function (R.fund, R.style, method = c("constrained","unconstrained","normalized"), leverage = FALSE, width = 12, main = NULL, space = 0, ...)
 { # @author Peter Carl
 
     # DESCRIPTION:
@@ -26,6 +26,21 @@ function (R.fund, R.style, method = c("constrained","unconstrained","normalized"
     merged.assets = na.omit(merge(R.fund, R.style))
     result = rollapply(data = merged.assets, FUN= function(x) {t(style.fit(R.fund = x[,1,drop=FALSE], R.style = x[,-1,drop=FALSE], method = method, leverage = leverage)$weights)}, width = width, by = 1, by.column = FALSE, na.pad = FALSE, align = "right")
 
+    if (is.null(main)){
+        freq = periodicity(R.fund)
+
+        switch(freq$scale,
+            minute = {freq.lab = "minute"},
+            hourly = {freq.lab = "hour"},
+            daily = {freq.lab = "day"},
+            weekly = {freq.lab = "week"},
+            monthly = {freq.lab = "month"},
+            quarterly = {freq.lab = "quarter"},
+            yearly = {freq.lab = "year"}
+        )
+
+        main = paste(colnames(R.fund)[1]," Rolling ", width ,"-",freq.lab," Style Weights", sep="")
+    }
     colnames(result) = columnnames.style
     chart.StackedBar(result, main = main, space = space, ...)
 
@@ -39,10 +54,13 @@ function (R.fund, R.style, method = c("constrained","unconstrained","normalized"
 # This library is distributed under the terms of the GNU Public License (GPL)
 # for full details see the file COPYING
 #
-# $Id: chart.RollingStyle.R,v 1.3 2008-07-11 03:22:01 peter Exp $
+# $Id: chart.RollingStyle.R,v 1.4 2009-10-15 21:50:19 brian Exp $
 #
 ###############################################################################
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2008-07-11 03:22:01  peter
+# - removed unnecessary function attributes
+#
 # Revision 1.2  2008-04-18 03:59:52  peter
 # - added na.omit to avoid problems with missing data
 #
