f0a45168891909a422a625c270113359bf424088
Peter Carl <pcarl@gsb.uchicago.edu>
2009-12-18 20:22:43 +0000

    - moved from PortfolioAnalytics package
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@1465 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/zerofill.R
@@ -0,0 +1,22 @@
+###############################################################################
+# R (http://r-project.org/) Numeric Methods for Optimization of Portfolios
+#
+# Copyright (c) 2004-2009 Kris Boudt, Peter Carl and Brian G. Peterson
+#
+# This library is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
+
+zerofill <- function (x) {
+  mat<-checkData(x,"matrix")
+  for(column in 1:ncol(mat)){
+    for (row in 1:nrow(mat)){
+      if(is.na(mat[row,column])) mat[row,column] <- 0
+    }
+  }
+  x<-reclass(mat,x)
+  return(x)
+}
