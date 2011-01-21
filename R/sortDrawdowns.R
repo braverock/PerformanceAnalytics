766168455114b4aa3459bc5efdecd07e23aba79c
Brian G. Peterson <brian@braverock.com>
2011-01-21 12:52:53 +0000

    - replace body of function with lapply based version provided by H. Felix Wittman < hfwittmann <at> googlemail <dot> com >
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@1798 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/sortDrawdowns.R
+++ R/sortDrawdowns.R
@@ -1,43 +1,16 @@
-sortDrawdowns <-
-function (runs) {
-    # sortDrawdowns(findDrawdowns(caduceus.ts@Data[,1]))
+sortDrawdowns <- function (runs) {
+    # sortDrawdowns(findDrawdowns(returns))
     # gives the drawdowns in order of worst to best
 
-    # modified with permission from function by Sankalp Upadhyay
+    # original function modified with permission from function by Sankalp Upadhyay
     # <sankalp.upadhyay [at] gmail [dot] com>
     
-    if (length(runs$return) == 1) return(runs) 
-
-    for (i in 1:length(runs$return)) {
-        for (j in 1:(length(runs$return)-1)) {
-            if (runs$return[j] > runs$return[j+1]) {
-                tempRet = runs$return[j]
-                tempFor = runs$from[j]
-                tempTo = runs$to[j]
-                tempLen = runs$length[j]
-                tempPtt = runs$peaktotrough[j]
-                tempTro = runs$trough[j]
-                tempRec = runs$recovery[j]
-
-                runs$return[j] = runs$return[j+1]
-                runs$from[j] = runs$from[j+1]
-                runs$to[j] = runs$to[j+1]
-                runs$length[j] = runs$length[j+1]
-                runs$peaktotrough[j] = runs$peaktotrough[j+1]
-                runs$trough[j] = runs$trough[j+1]
-                runs$recovery[j] = runs$recovery[j+1]
-
-                runs$return[j+1] = tempRet
-                runs$from[j+1] = tempFor
-                runs$to[j+1] = tempTo
-                runs$length[j+1] = tempLen
-                runs$peaktotrough[j+1] = tempPtt
-                runs$trough[j+1] = tempTro
-                runs$recovery[j+1] = tempRec
-            }
-        }
-    }
-    runs
+    # this version provided by H. Felix Wittman < hfwittmann <at> googlemail <dot> com >
+    
+    index.sorted <- sort(runs$return, index=T)$ix # das kleinste zu erst
+    runs.sorted <- lapply(runs, function(x) x <- x[index.sorted])
+    
+    return(runs.sorted)
 }
 
 ###############################################################################
