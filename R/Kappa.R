346d31987cf6b1580e1330f5ba6dff9445ae3f78
M. Matthieu Lestel <matthieu.lestel@gmail.com>
2012-06-21 16:38:02 +0000

    addition of kappa with examples and documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2046 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/Kappa.R
@@ -0,0 +1,74 @@
+#' Kappa of the return distribution
+#'
+#' Introduced by Kaplan and Knowles (2004), Kappa is a generalized
+#' downside risk-adjusted performance measure.
+#'
+#' To calculate it, we take the difference of the mean of the distribution
+#' to the target and we divide it by the l-root of the lth lower partial
+#' moment. To calculate the lth lower partial moment we take the subset of
+#' returns below the target and we sum the differences of the target to
+#' these returns. We then return return this sum divided by the length of
+#' the whole distribution.
+#'
+#' \deqn{Kappa(R, MAR, l) = \frac{r_{p}-MAR}{\sqrt[l]{\frac{1}{n}*\sum^n_{t=1}
+#' max(MAR-R_{t}, 0)^l}}}{Kappa(R, MAR, l) = (rp - MAR)/(\sqrt[l](1/n*sum(t=1..n)
+#' (max(MAR-r(t),0)^l)))}
+#'
+#' For l=1 kappa is the Sharpe-omega ratio and for l=2 kappa
+#' is the sortino ratio.
+#'
+#' Kappa should only be used to rank portfolios as it is difficult to
+#' interpret the absolute differences between kappas. The higher the
+#' kappa is, the better.
+#'
+#'
+#' @aliases Kappa
+#' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
+#' asset returns
+#' @param MAR Minimum Acceptable Return, in the same periodicity as your
+#' returns
+#' @param l the coefficient of the Kappa
+#' @param \dots any other passthru parameters
+#' @author Matthieu Lestel
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' and attribution}, second edition 2008 p.96
+#' 
+#' @keywords ts multivariate distribution models
+#'
+#' @examples
+#' l = 2
+#'
+#' data(portfolio_bacon)
+#' MAR = 0.5
+#' print(Kappa(portfolio_bacon, MAR, l)) #expected 0.157
+#'
+#' data(managers)
+#' MAR = 0
+#' print(Kappa(managers['1996'], MAR, l))
+#' print(Kappa(managers['1996',1], MAR, l)) #expected 1.493
+#'
+#' @export 
+
+Kappa <- function (R, MAR, l, ...)
+{
+    R0 <- R
+    R = checkData(R, method="matrix")
+
+    if (ncol(R)==1 || is.null(R) || is.vector(R)) {
+       R = na.omit(R)
+       r = subset(R, R<MAR)
+       n = length(R)
+       m = mean(R)
+       result = (m-MAR)/(((1/n)*sum((MAR - r)^l))^(1/l))
+       reclass(result, R0)
+       return(result)
+    }
+    else {
+        R = checkData(R)
+        result = apply(R, MARGIN = 2, Kappa, MAR=MAR, l=l, ...)
+        result<-t(result)
+        colnames(result) = colnames(R)
+        rownames(result) = paste("kappa (MAR = ",MAR,"%)", sep="")
+        return(result)
+    }
+}
