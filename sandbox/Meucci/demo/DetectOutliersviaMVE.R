e0e42c15ef5559201893abe2c066625d201c58fa
Manan Shah <mkshah@tepper.cmu.edu>
2012-06-24 17:20:00 +0000

    Cleaned file and checked against Meucci's Matlab Code.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2057 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/Meucci/demo/DetectOutliersviaMVE.R
+++ sandbox/Meucci/demo/DetectOutliersviaMVE.R
@@ -1,9 +1,8 @@
-
 # Example 1: Two-asset example with -.9 correlation
 # generate covariance matrix to sample "good returns"
 sig = matrix( c( 1 , 1 ) , nrow = 1 ); r = -.9
 correlationMatrix = rbind( cbind( 1 , r ) , cbind( r , 1 ) ) # generate 2x2 correlation matrix
-covarianceMatrix = diag(sig) * correlationMatrix * diag(sig) ; rm( correlationMatrix ) ; rm( sig ) ; rm( r ) # covert to covariance matrix
+covarianceMatrix = diag(sig) * correlationMatrix * diag(sig) ; rm( correlationMatrix ) ; rm( sig ) ; rm( r ) # convert to covariance matrix
 
 # co-mingle sample from good covariance matrix with bad outliers and shuffle rows
 corruptSample = NoisyObservations( 50 , 7 , covarianceMatrix , shuffle = TRUE ) # returns 50 good samples and 7 bad samples
