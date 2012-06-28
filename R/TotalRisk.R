e4deb7873e8f276b9b98a6a990fe6a087bfad478
M. Matthieu Lestel <matthieu.lestel@gmail.com>
2012-06-28 15:16:44 +0000

    Total risk with examples and documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2088 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/TotalRisk.R
@@ -0,0 +1,67 @@
+#' Total risk of the return distribution
+#'
+#' The square of total risk is the sum of the square of systematic risk and the square
+#' of specific risk. Specific risk is the standard deviation of the error term in the
+#' regression equation. Both terms are annualized to calculate total risk.
+#'
+#' \deqn{Total Risk = \sqrt{Systematic Risk^2 + Specific Risk^2}} 
+#' {Total Risk^2 = Systematic Risk^2 + Specific Risk^2}
+#'
+#' @aliases total risk
+#' @param Ra an xts, vector, matrix, data frame, timeSeries or zoo object of
+#' asset returns
+#' @param Rb return vector of the benchmark asset
+#' @param Rf risk free rate, in same period as your returns
+#' @param Period the number of return in a year in the asset return 
+#' (i.e. 12 for monthly returns, 4 for quarterly returns)
+#' @param \dots any other passthru parameters
+#' @author Matthieu Lestel
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' and attribution}, second edition 2008 p.75
+#' 
+#' @keywords ts multivariate distribution models
+#' @examples
+#'
+#' data(portfolio_bacon)
+#' print(TotalRisk(portfolio_bacon[,1], portfolio_bacon[,2])) #expected 13.4
+#'
+#' data(managers)
+#' print(TotalRisk(managers['1996',1], managers['1996',8]))
+#' print(TotalRisk(managers['1996',1:5], managers['1996',8]))
+#'
+#' @export 
+
+TotalRisk <-
+function (Ra, Rb, Rf = 0, Period = 12,  ...)
+{
+    calcul = FALSE
+    Ra = checkData(Ra, method="matrix")
+    Rb = checkData(Rb, method="matrix")
+
+    if (ncol(Ra)==1 || is.null(Ra) || is.vector(Ra)) {
+    
+     for (i in (1:length(Ra))) {
+     	 if (!is.na(Ra[i])) {
+     	    calcul = TRUE
+	 }
+      }
+
+     if (calcul) {
+     	epsilon = Ra - Rb * CAPM.beta(Ra,Rb,Rf) - CAPM.alpha(Ra,Rb,Rf)
+	specifikRisk = sqrt(sum((epsilon - mean(epsilon))^2)/length(epsilon))*sqrt(Period)
+        result = sqrt((SystematicRisk(Ra,Rb,Rf)*sqrt(Period))^2 + specifikRisk^2)
+     }    
+     else {
+        result = NA
+     }
+      return(result)
+    }
+    else {
+        Ra = checkData(Ra)
+        result = apply(Ra, MARGIN = 2, TotalRisk, Rb = Rb, Rf = Rf, ...)
+        result<-t(result)
+        colnames(result) = colnames(Ra)
+        rownames(result) = paste("Total Risk = ", sep="")
+        return(result)
+    }
+}
