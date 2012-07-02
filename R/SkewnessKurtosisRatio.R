5f41d8fdd3f90cecbc696ca28df037e09d56f595
M. Matthieu Lestel <matthieu.lestel@gmail.com>
2012-07-02 15:07:25 +0000

    SkewnessKurtosisRatio with examples and documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2098 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/SkewnessKurtosisRatio.R
@@ -0,0 +1,53 @@
+#' Skewness-Kurtosis ratio of the return distribution
+#'
+#' Skewness-Kurtosis ratio is the division of Skewness by Kurtosis.
+#' 
+#' It is used in conjunction with the Sharpe ratio to rank portfolios.
+#' The higher the rate the better.
+#'
+#' \deqn{ SkewnessKurtosisRatio(R , MAR) = \frac{S}{K}}
+#' {SkewnessKurtosisRatio(R, MAR) = S/K}
+#'
+#' where \eqn{S} is the skewness and \eqn{K} is the Kurtosis
+#'
+#' @aliases Skewness-Kurtosis ratio
+#' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
+#' asset returns
+#' @param \dots any other passthru parameters
+#' @author Matthieu Lestel
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' and attribution}, second edition 2008 p.100
+#' 
+#' @keywords ts multivariate distribution models
+#' @examples
+#'
+#' data(portfolio_bacon)
+#' print(SkewnessKurtosisRatio(portfolio_bacon)) #expected 
+#'
+#' data(managers)
+#' print(SkewnessKurtosisRatio(managers['1996']))
+#' print(SkewnessKurtosisRatio(managers['1996',1])
+#'
+#' @export 
+
+SkewnessKurtosisRatio <-
+function (R, ...)
+{
+    R0 <- R
+    R = checkData(R, method="matrix")
+
+    if (ncol(R)==1 || is.null(R) || is.vector(R)) {
+       R = na.omit(R)
+	result = skewness(R) / kurtosis(R, method = "moment")
+	reclass(result, R0)
+        return(result)
+    }
+    else {
+        R = checkData(R)
+        result = apply(R, MARGIN = 2, SkewnessKurtosisRatio, ...)
+        result<-t(result)
+        colnames(result) = colnames(R)
+        rownames(result) = paste("SkewnessKurtosisRatio", sep="")
+        return(result)
+    }
+}
