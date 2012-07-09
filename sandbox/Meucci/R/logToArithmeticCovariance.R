5fe187dc269a860f55aff5b58ccfb1b9c37081f0
Manan Shah <mkshah@tepper.cmu.edu>
2012-07-09 02:12:17 +0000

    Updating equations and references
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/Meucci/R/logToArithmeticCovariance.R
+++ sandbox/Meucci/R/logToArithmeticCovariance.R
@@ -6,12 +6,15 @@
 #' @return                        a list containing two elements:
 #' @return  arithmeticMean          a numeric containing the mean arithmetic returns
 #' @return  arithmeticCovariance    a variance-covariance matrix in simple arithmetic return terms
-#'
+#' \deqn { M_{ \tau }^{i}  =  e^{  \mu ^{\tau} _{i} +  \frac{1}{2} \Sigma^{ii} _{\tau} },
+#' \\  S^{ij} = e^{  \mu ^{\tau} _{i} + \mu ^{\tau} _{j} + \frac{1}{2} \big(\Sigma^{ii} _{\tau} + \Sigma^{jj} _{\tau}\big)  }  \big(e^{\Sigma^{ij} _{\tau}} - 1\big) }
 #' @author Ram Ahluwalia \email{ram@@wingedfootcapital.com}
 #' @export
+#' @references
+#' # formula (7) and (8) on page 5 of Appendix to "Meucci - A Common Pitfall in Mean-Variance Estimation"
+#' \url{http://www.wilmott.com/pdfs/011119_meucci.pdf}
 linreturn <- function( mu , sigma )
-{
-    # formula (7) on page 5 of Appendix to "Meucci - A Common Pitfall in Mean-Variance Estimation"
+{   
     # each element of M represents the linear returns for the corresponding log-returns element in mu
     M <- exp( mu + ( diag( sigma ) / 2 ) ) - 1
     
