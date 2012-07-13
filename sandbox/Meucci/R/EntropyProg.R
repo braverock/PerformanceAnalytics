85a25b8a68fdfb8f869ba406a1c6bfd202792fef
Manan Shah <mkshah@tepper.cmu.edu>
2012-07-13 10:23:33 +0000

    Correcting documentation mistakes
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/Meucci/R/EntropyProg.R
+++ sandbox/Meucci/R/EntropyProg.R
@@ -180,22 +180,22 @@ EntropyProg = function( p , A , b , Aeq , beq )
 
 #' Calculate the full-confidence posterior distributions of Mu and Sigma
 #'
+#' \deqn{ \tilde{ \mu }  \equiv \mu +  \Sigma  Q'    {\big(Q \Sigma  Q' \big)}^{-1}   \big( \tilde{\mu}_{Q} - Q \mu \big),
+#' \\ \tilde{ \Sigma } \equiv \Sigma + \Sigma G' \big({\big(G \Sigma  G' \big)}^{-1} \tilde{ \Sigma }_G {\big(G \Sigma  G' \big)}^{-1} - {\big(G \Sigma  G' \big)}^{-1} \big) G \Sigma }
 #' @param M     a numeric vector with the Mu of the normal reference model
-#' @param Q     a numeric vector used to construct a view on expectation of the linear combination Q %*% X
+#' @param Q     a numeric vector used to construct a view on expectation of the linear combination QX
 #' @param M_Q   a numeric vector with the view of the expectations of QX
 #' @param S     a covariance matrix for the normal reference model
-#' @param G     a numeric vector used to construct a view on covariance of the linear combination G %*% X
+#' @param G     a numeric vector used to construct a view on covariance of the linear combination GX
 #' @param S_G   a numeric with the expectation associated with the covariance of the linear combination GX
 #'
 #' @return a list with 
-#'             M_   a numeric vector with the full-confidence posterior distribution of \Mu
-#'             S_   a covariance matrix with the full-confidence posterior distribution of \Sigma
+#' @return M_   a numeric vector with the full-confidence posterior distribution of Mu
+#' @return S_   a covariance matrix with the full-confidence posterior distribution of Sigma
 #'
 #' @references 
 #' \url{http://www.symmys.com}
 #' \url{http://ssrn.com/abstract=1213325}
-#' \deqn{ \tilde{ \mu }  \equiv \mu +  \Sigma  Q'    {\big(Q \Sigma  Q' \big)}^{-1}   \big( \tilde{\mu}_{Q} - Q \mu \big),
-#' \\ \tilde{ \Sigma } \equiv \Sigma + \Sigma G' \big({\big(G \Sigma  G' \big)}^{-1} \tilde{ \Sigma }_G {\big(G \Sigma  G' \big)}^{-1} - {\big(G \Sigma  G' \big)}^{-1} \big) G \Sigma }
 #' A. Meucci - "Fully Flexible Views: Theory and Practice". See formula (21) and (22) on page 7
 #' See Meucci script Prior2Posterior.m attached to Entropy Pooling Paper
 #' @author Ram Ahluwalia \email{ram@@wingedfootcapital.com}
