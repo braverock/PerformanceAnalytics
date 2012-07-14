11d92c0006acea794d806dbebc2b5dddc9a922a4
M. Matthieu Lestel <matthieu.lestel@gmail.com>
2012-07-14 15:19:37 +0000

    Mean absolute deviation with examples and documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2158 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/MeanAbsoluteDeviation.R
@@ -0,0 +1,49 @@
+#' Mean absolute deviation of the return distribution
+#'
+#' To calculate Mean absolute deviation we take the sum of the absolute value of the difference between the returns and the mean of the returns and we divide it by the number of returns.
+#'
+#' \deqn{MeanAbsoluteDeviation = \frac{\sum^{n}_{i=1}\mid r_i - \overline{r}\mid}{n}}
+#' {MeanAbsoluteDeviation = sum(r-mean(r))/n }
+#'
+#' where \eqn{n} is the number of observations of the entire series, \eqn{r_i} is the
+#' return in month i and \eqn{\overline{r}} is the mean return
+#' 
+#' @aliases MeanAbsoluteDeviation
+#' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
+#' asset returns
+#' @param \dots any other passthru parameters
+#' @author Matthieu Lestel
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' and attribution}, second edition 2008 p.62
+#' 
+#' @keywords ts multivariate distribution models
+#' @examples
+#' data(portfolio_bacon[,1])
+#' print(MeanAbsoluteDeviation(portfolio_bacon)) #expected 1.78
+#'
+#' data(managers)
+#' print(MeanAbsoluteDeviation(managers['1996']))
+#' print(MeanAbsoluteDeviation(managers['1996',1])) #expected 4.598
+#'
+#' @export 
+
+MeanAbsoluteDeviation <- function (R, ...)
+{
+    R0 <- R
+    R = checkData(R, method="matrix")
+
+    if (ncol(R)==1 || is.null(R) || is.vector(R)) {
+       R = na.omit(R)
+       result = sum(abs(R - mean(R)))/length(R)
+       reclass(result, R0)
+       return(result)
+    }  
+    else {
+        R = checkData(R)
+        result = apply(R, MARGIN = 2, MeanAbsoluteDeviation, ...)
+        result<-t(result)
+        colnames(result) = colnames(R)
+        rownames(result) = paste("Mean absolute deviation", sep="")
+        return(result)
+    }
+}
