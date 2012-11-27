20b84393688e90a336ac56be005b28dadd47b076
Brian G. Peterson <brian@braverock.com>
2012-11-27 18:11:12 +0000

    - refactor to remove need for sd.xts
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2301 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/StdDev.annualized.R
+++ R/StdDev.annualized.R
@@ -51,24 +51,26 @@ function (x, scale = NA, ...)
 {
     if (is.vector(x)) {
         #scale standard deviation by multiplying by the square root of the number of periods to scale by
+        sqrt(scale)*sd(x, na.rm=TRUE)
+    } else { 
         if(!xtsible(x) & is.na(scale))
             stop("'x' needs to be timeBased or xtsible, or scale must be specified." )
         x = checkData (x)
         if(is.na(scale)) {
             freq = periodicity(x)
             switch(freq$scale,
-                minute = {stop("Data periodicity too high")},
-                hourly = {stop("Data periodicity too high")},
-                daily = {scale = 252},
-                weekly = {scale = 52},
-                monthly = {scale = 12},
-                quarterly = {scale = 4},
-                yearly = {scale = 1}
+                    minute = {stop("Data periodicity too high")},
+                    hourly = {stop("Data periodicity too high")},
+                    daily = {scale = 252},
+                    weekly = {scale = 52},
+                    monthly = {scale = 12},
+                    quarterly = {scale = 4},
+                    yearly = {scale = 1}
             )
-        }
-        sqrt(scale)*xts:::sd.xts(x, na.rm=TRUE)
-    } else { 
+        }        
+        
         result = apply(x, 2, sd.multiperiod, scale=scale)
+        
         dim(result) = c(1,NCOL(x))
         colnames(result) = colnames(x)
         rownames(result) = "Annualized Standard Deviation"
