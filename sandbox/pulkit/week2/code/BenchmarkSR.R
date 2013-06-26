925f85f03c3aa1f5e7a5ae563bb6f0dd22e779a2
Pulkit Mehrotra <mehrotra.pulkit@gmail.com>
2013-06-26 14:54:05 +0000

    Added vignette for Benchmark SR
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2443 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/pulkit/week2/code/BenchmarkSR.R
+++ sandbox/pulkit/week2/code/BenchmarkSR.R
@@ -13,21 +13,30 @@
 #'@param R a vector, matrix, data frame,timeseries or zoo object of asset returns
 #'
 #'@references
+#'Bailey, David H. and Lopez de Prado, Marcos, The Strategy Approval Decision: 
+#'A Sharpe Ratio Indifference Curve Approach (January 2013). Algorithmic Finance, 
+#'Vol. 2, No. 1 (2013).
+#'
+#'@examples
+#'
+#'data(edhec)
+#'BenchmarkSR(edhec) #expected 0.2019308
 #'
 #'@export
+#'
 BenchmanrkSR<-function(R){
-  
   x = checkData(R)
   columns = ncol(x)
   SR = SharpeRatio(x)
   sr_avg = mean(SR)
   corr = table.Correlation(edhec,edhec)
   corr_avg = 0
-  for(i in 1:columns){
-    for(j in i:columns){
+  for(i in 1:(columns-1)){
+    for(j in (i+1):columns){
       corr_avg = corr_avg + corr[(i-1)*columns+j,]
     }
   }
+  corr_avg = corr_avg*2/(columns*(columns-1))
   SR_Benchmark = sr_avg*sqrt(columns/(1+(columns-1))*corr_avg[1,1])
   return(SR_Benchmark)
 }
