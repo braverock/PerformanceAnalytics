5bd92f17c34ede786a2313801a50fc3d8b4af1fa
Shubhankit Mohan <shubhankit1@gmail.com>
2013-07-06 22:31:01 +0000

    Week 3: Stack Bar Plot of Autocorrelation Lag Factors
    Status : Backtested 
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2512 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sandbox/Shubhankit/chart.Autocorrelation.R
@@ -0,0 +1,47 @@
+#' Stacked Bar Plot of Autocorrelation Lag Coefficients
+#' 
+#' A wrapper to create box and whiskers plot of comparitive inputs
+#' 
+#' We have also provided controls for all the symbols and lines in the chart.
+#' One default, set by \code{as.Tufte=TRUE}, will strip chartjunk and draw a
+#' Boxplot per recommendations by Burghardt, Duncan and Liu(2013)
+#' 
+#' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
+#' asset returns
+#' @return Stack Bar plot of lagged return  coefficients
+#' @author R 
+#' @seealso \code{\link[graphics]{boxplot}}
+#' @references Burghardt, Duncan and Liu(2013)  \emph{It's the autocorrelation, stupid}. AlternativeEdge Note  November, 2012 }
+#' @keywords Autocorrelation lag factors
+#' @examples
+#' 
+#' data(edhec)
+#' chart.Autocorrelation(edhec)
+#' 
+#' 
+#' @export 
+chart.Autocorrelation <-
+  function (R, ...)
+  { # @author R
+    
+    # DESCRIPTION:
+    # A wrapper to create box and whiskers plot, of autocorrelation lag coeffiecients
+    # of the First six factors
+    
+    R = checkData(R, method="xts")
+    
+# Graph autos with adjacent bars using rainbow colors
+ 
+aa= table.Autocorrelation(R)
+barplot(as.matrix(aa), main="Auto Correlation Lag", ylab= "Value of Coefficient",
+                   , xlab = "Fund Type",beside=TRUE, col=rainbow(6))
+
+   # Place the legend at the top-left corner with no frame  
+   # using rainbow colors
+   legend("topright", c("1","2","3","4","5","6"), cex=0.6, 
+                   bty="n", fill=rainbow(6));
+
+
+
+
+}
