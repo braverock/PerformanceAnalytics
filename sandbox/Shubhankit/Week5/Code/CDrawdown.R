24c43cfec8659b75fd44b454c4f2ccd78e5425c6
Shubhankit Mohan <shubhankit1@gmail.com>
2013-07-24 11:10:58 +0000

    Added : Okunev Model for Unsmoothed Return (Tested and Implemented)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@2638 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/Shubhankit/Week5/Code/CDrawdown.R
+++ sandbox/Shubhankit/Week5/Code/CDrawdown.R
@@ -1,5 +1,36 @@
+#' Active Premium or Active Return
+#' 
+#' The return on an investment's annualized return minus the benchmark's
+#' annualized return.
+#' 
+#' Active Premium = Investment's annualized return - Benchmark's annualized
+#' return
+#' 
+#' Also commonly referred to as 'active return'.
+#' 
+#' @param Ra return vector of the portfolio
+#' @param Rb return vector of the benchmark asset
+#' @param scale number of periods in a year (daily scale = 252, monthly scale =
+#' 12, quarterly scale = 4)
+#' @author Peter Carl
+#' @seealso \code{\link{InformationRatio}} \code{\link{TrackingError}}
+#' \code{\link{Return.annualized}}
+#' @references Sharpe, W.F. The Sharpe Ratio,\emph{Journal of Portfolio
+#' Management},Fall 1994, 49-58.
+#' @keywords ts multivariate distribution models
+#' @examples
+#' 
+#'     data(managers)
+#'     ActivePremium(managers[, "HAM1", drop=FALSE], managers[, "SP500 TR", drop=FALSE])
+#'     ActivePremium(managers[,1,drop=FALSE], managers[,8,drop=FALSE]) 
+#'     ActivePremium(managers[,1:6], managers[,8,drop=FALSE]) 
+#'     ActivePremium(managers[,1:6], managers[,8:7,drop=FALSE])
+#' @rdname ActivePremium
+#' @aliases ActivePremium, ActiveReturn
+#' @export 
+
 CDrawdown <-
-  function (R,p=0.95, ...)
+  function (R,p=0.90, ...)
   {
     y = checkData(R, method = "xts")
     columns = ncol(y)
