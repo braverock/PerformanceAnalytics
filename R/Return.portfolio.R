e94e13332e3d8de0355b610bad5afe63cd608be9
Brian G. Peterson <brian@braverock.com>
2014-06-09 16:06:30 +0000

    - add back copyright/license block, lost in the prototyping
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@3414 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Return.portfolio.R
+++ R/Return.portfolio.R
@@ -275,3 +275,14 @@ Return.portfolio <- Return.rebalancing <- function(R,
   return(out)
 }
 
+###############################################################################
+# R (http://r-project.org/) Econometrics for Performance and Risk Analysis
+#
+# Copyright (c) 2004-2014 Peter Carl and Brian G. Peterson
+#
+# This R package is distributed under the terms of the GNU Public License (GPL)
+# for full details see the file COPYING
+#
+# $Id$
+#
+###############################################################################
