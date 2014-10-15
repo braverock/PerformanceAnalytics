b1fced4b4a2fb9fafbfcab9d68fe332ed17dd5fa
Peter Carl <pcarl@gsb.uchicago.edu>
2014-10-15 13:10:23 +0000

    - added roxygen documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/returnanalytics/pkg/PerformanceAnalytics@3544 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- sandbox/to.period.contributions.R
+++ sandbox/to.period.contributions.R
@@ -1,3 +1,34 @@
+#' aggregate contributions through time
+#' 
+#' Higher frequency contributions provided as a time series are converted to a lower 
+#' frequency for a specified calendar period.
+#' 
+#' From the portfolio contributions of individual assets, such as those of a particular asset class 
+#' or manager, the multiperiod contribution is neither summable from nor the geometric compounding of
+#' single-period contributions.  Because the weights of the individual assets change through time as
+#' transactions occur, the capital base for the asset changes.
+#' 
+#' Instead, the asset's multiperiod contribution is the sum of the asset's dollar contributions from
+#' each period, as calculated from the wealth index of the total portfolio. Once contributions are
+#' expressed in cumulative terms, asset contributions then sum to the returns of the total portfolio for 
+#' the period.
+#' 
+#' Valid period character strings for period include: "weeks", "months", "quarters", and "years". 
+#' These are calculated internally via \code{\link{endpoints}}. See that function's help page for further details.
+#' 
+#' @param Contributions a time series of the per period contribution to portfolio return of each asset 
+#' @param period period to convert to. See details."years", "quarters", "months", "weeks"
+#' @param \dots any other passthru parameters
+#' @author Peter Carl, with thanks to Paolo Cavatore
+#' @seealso \code{\link{Return.portfolio}} \cr \code{\link{endpoints}} \cr
+#' @references Morningstar \emph{Total Portfolio Performance Attribution Methodology} p.36. Available at http://corporate.morningstar.com/US/documents/MethodologyDocuments/MethodologyPapers/TotalPortfolioPerformanceAttributionMethodology.pdf \cr
+#' @aliases to.monthly.contributions to.weekly.contributions to.quarterly.contributions to.yearly.contributions
+#' @examples
+#' data(managers)
+#' res_qtr_rebal = Return.portfolio(managers["2002::",1:5], weights=c(.05,.1,.3,.4,.15), rebalance_on = "quarters", verbose=TRUE)
+#' to.period.contributions(res_qtr_rebal$contribution, period="years")
+#' to.yearly.contributions(res_qtr_rebal$contribution)
+#' @export
 to.period.contributions <- function(Contributions, period = c("years", "quarters", "months", "weeks"), ...){
   C = checkData(Contributions)
   period = period[1] 
@@ -50,16 +81,19 @@ to.period.contributions <- function(Contributions, period = c("years", "quarters
   return(period.contrib)
   
 }
-
+#' @export
 to.weekly.contributions <- function(contributions) {
   to.period.contributions(contributions = contributions, period = "weeks")
 }
+#' @export
 to.monthly.contributions <- function(contributions) {
   to.period.contributions(contributions = contributions, period = "months")
 }
+#' @export
 to.quarterly.contributions <- function(contributions) {
   to.period.contributions(contributions = contributions, period = "quarters")
 }
+#' @export
 to.yearly.contributions <- function(contributions) {
   to.period.contributions(contributions = contributions, period = "years")
 }
