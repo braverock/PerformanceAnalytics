73f6df0234b5a8ee0c03c80cd82d86452cb4f3af
Eric Hung <naturalsmen@gmail.com>
2016-05-29 18:10:02 +0800

    - refactor to use plot.xts

--- R/charts.PerformanceSummary.R
+++ R/charts.PerformanceSummary.R
@@ -50,13 +50,13 @@
 #' 
 #' @export
 charts.PerformanceSummary <-
-function (R, Rf = 0, main = NULL, geometric=TRUE, methods = "none", width = 0, event.labels = NULL, ylog = FALSE, wealth.index = FALSE, gap = 12, begin=c("first","axis"), legend.loc="topleft", p=0.95,...)
-{ # @author Peter Carl
-
+  function (R, Rf = 0, main = NULL, geometric=TRUE, methods = "none", width = 0, event.labels = NULL, ylog = FALSE, wealth.index = FALSE, gap = 12, begin=c("first","axis"), legend.loc="topleft", p=0.95,...)
+  { # @author Peter Carl
+    
     # DESCRIPTION:
     # A wrapper to create a wealth index chart, bars for monthly peRformance,
     # and underwater chart for drawdown.
-
+    
     # Inputs:
     # R: a matrix, data frame, or timeSeries, usually a set of monthly returns.
     #   The first column is assumed to be the returns of interest, the next
@@ -69,83 +69,77 @@ function (R, Rf = 0, main = NULL, geometric=TRUE, methods = "none", width = 0, e
     #     VaR - uses traditional Value at Risk
     #     StdDev - monthly standard deviation of trailing 12 month returns
     #
-
+    
     # Outputs:
     # A stack of three related timeseries line charts
-
+    
     # FUNCTION:
     begin = begin[1]
     x = checkData(R)
     colnames = colnames(x)
     ncols = ncol(x)
-
-# This repeats a bit of code from chart.CumReturns, but it's intended
-# to align the start dates of all three charts.  Basically, it assumes
-# that the first column in the list is the column of interest, and 
-# starts everything from that start date
-
+    
+    # This repeats a bit of code from chart.CumReturns, but it's intended
+    # to align the start dates of all three charts.  Basically, it assumes
+    # that the first column in the list is the column of interest, and 
+    # starts everything from that start date
+    
     length.column.one = length(x[,1])
-# find the row number of the last NA in the first column
+    # find the row number of the last NA in the first column
     start.row = 1
     start.index = 0
     while(is.na(x[start.row,1])){
-        start.row = start.row + 1
+      start.row = start.row + 1
     }
     x = x[start.row:length.column.one,]
-
+    
     if(ncols > 1)
-        legend.loc = legend.loc
+      legend.loc = legend.loc
     else
-        legend.loc = NULL
-
+      legend.loc = NULL
+    
     if(is.null(main))
-        main = paste(colnames[1],"Performance", sep=" ")
-
+      main = paste(colnames[1],"Performance", sep=" ")
+    
     if(ylog)
-        wealth.index = TRUE
-
-    op <- par(no.readonly=TRUE)
-
-    # First, we lay out the graphic as a three row, one column format
-#    plot.new()
-    layout(matrix(c(1,2,3)),heights=c(2,1,1.3),widths=1)
-    # to see the resulting layout, use layout.show(3)
-
+      wealth.index = TRUE
+    
     # mar: a numerical vector of the form c(bottom, left, top, right) which
     # gives the number of lines of margin to be specified on the four sides
     # of the plot. The default is c(5, 4, 4, 2) + 0.1
-
+    
     # The first row is the cumulative returns line plot
+    p <- list()
+    par(mfrow = c(3, 1))
     par(mar=c(1,4,4,2))
-    chart.CumReturns(x, main = main, xaxis = FALSE, legend.loc = legend.loc, event.labels = event.labels, ylog = ylog, wealth.index = wealth.index, begin = begin, geometric = geometric, ylab="Cumulative Return",...)
-
-    # The second row is the monthly returns bar plot
-    par(mar=c(1,4,0,2))
-
+    p[[1]] <- chart.CumReturns(x, main = main, xaxis = FALSE, legend.loc = legend.loc, event.labels = event.labels, ylog = ylog, wealth.index = wealth.index, begin = begin, geometric = geometric, ylab="Cumulative Return",...)
+    
     freq = periodicity(x)
-
+    
     switch(freq$scale,
-	seconds = { date.label = "Second"},
-	minute = { date.label = "Minute"},
-	hourly = {date.label = "Hourly"},
-	daily = {date.label = "Daily"},
-	weekly = {date.label = "Weekly"},
-	monthly = {date.label = "Monthly"},
-	quarterly = {date.label = "Quarterly"},
-	yearly = {date.label = "Annual"}
+           seconds = { date.label = "Second"},
+           minute = { date.label = "Minute"},
+           hourly = {date.label = "Hourly"},
+           daily = {date.label = "Daily"},
+           weekly = {date.label = "Weekly"},
+           monthly = {date.label = "Monthly"},
+           quarterly = {date.label = "Quarterly"},
+           yearly = {date.label = "Annual"}
     )
-
-    chart.BarVaR(x, main = "", xaxis = FALSE, width = width, ylab = paste(date.label,"Return"), methods = methods, event.labels = NULL, ylog=FALSE, gap = gap, p=p, ...)
-
+    
+    par(mar=c(1,4,0,2))
+    p[[2]] <- chart.BarVaR(x, main = "", xaxis = FALSE, width = width, ylab = paste(date.label,"Return"), methods = methods, event.labels = NULL, ylog=FALSE, gap = gap, p=p, ...)
+    
     # The third row is the underwater plot
     par(mar=c(5,4,0,2))
-    chart.Drawdown(x, geometric = geometric, main = "", ylab = "Drawdown", event.labels = NULL, ylog=FALSE, ...)
-
+    p[[3]] <- chart.Drawdown(x, geometric = geometric, main = "", ylab = "Drawdown", event.labels = NULL, ylog=FALSE, ...)
+    
+    return(invisible(capture.output(print(p))))
     # If we wanted to add a fourth row with the table of monthly returns
     #par(mar=c(0,0,0,0))
     #textplot(table.Returns(as.matrix(R)),cex=.7,cmar=1.5,rmar=0.5,halign="center", valign="center")
-    par(op)
-}
+    par(mfrow = c(1, 1))
+  }
 
 ###############################################################################
 # R (http://r-project.org/) Econometrics for Performance and Risk Analysis
