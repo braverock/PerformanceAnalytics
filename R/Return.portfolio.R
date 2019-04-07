0375c14f433864bda498a022c610dd80ed532232
cortinah <hch@alum.mit.edu>
2019-04-07 06:59:29 -0400

    Allow single-period return calc by manually specifying periodicity (#92)
    * Allow single-period return calc by manually specifying periodicity
    
    Currently Return.portfolio() fails when calculation has a single-period.  Reason is that xts::periodicity fails for a single period.  This patch allows user to manually specify freq="xxx".  This allows Return.portfolio() to work for a single period.  Default behavior is unchanged.
    
    * Changed ... to dots from extras

--- R/Return.portfolio.R
+++ R/Return.portfolio.R
@@ -156,7 +156,8 @@ Return.portfolio <- Return.rebalancing <- function(R,
   rebalance_on = rebalance_on[1]
   
   # find the right unit to subtract from the first return date to create a start date
-  freq = periodicity(R)
+  dots <- list(...)
+  if (is.null(dots$freq)) freq = periodicity(R) else freq = list(scale=dots$freq)
   switch(freq$scale, 
          seconds = { stop("Use a returns series of daily frequency or higher.") },
          minute = { stop("Use a returns series of daily frequency or higher.") },
