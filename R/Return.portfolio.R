f6cbd8f47ff80efea3c577df422a2382ecd65b3b
Divyansh Chawla <45530045+divyansh997@users.noreply.github.com>
2019-04-07 03:23:52 -0700

    Fixed subsetting by names, issue #110 (#116)
    * Fixed subsetting by names
    
    * Fixed subsetting by names for numeric vectors.
    
    * Fixed space after else

--- R/Return.portfolio.R
+++ R/Return.portfolio.R
@@ -191,13 +191,17 @@ Return.portfolio <- Return.rebalancing <- function(R,
     # make sure that frequency(weights)<frequency(R) ?
     
     # make sure the number of assets in R matches the number of assets in weights
-    # Should we also check the names of R and names of weights?
+    # We also check the names of R and names of weights
     if(NCOL(R) != NCOL(weights)){
-      if(NCOL(R) > NCOL(weights)){
+      if(NCOL(weights)>NCOL(R)){
+        stop("number of assets is greater than number of columns in returns object")
+      } else if(is.matrix(weights)&&length(intersect(names(R), colnames(weights)))!=0){
+        R = R[ , intersect(names(R), colnames(weights))]
+      } else if(!is.matrix(weights)&&length(intersect(names(R), names(weights)))!=0){
+        R = R[ , intersect(names(R), names(weights))]
+      } else {
         R = R[, 1:NCOL(weights)]
         warning("number of assets in beginning_weights is less than number of columns in returns, so subsetting returns.")
-      } else {
-        stop("number of assets is greater than number of columns in returns object")
       }
     }
   } # we should have good weights objects at this point
