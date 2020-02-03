96703b3ae1862a31d5ef37c3bc14cda803dc7456
Peter Carl <pcarl@williamblair.com>
2020-02-03 11:28:33 -0600

    specifies the calls for xlab and ylab into ggplot2 namespace

--- R/chart.TimeSeries.ggplot2.R
+++ R/chart.TimeSeries.ggplot2.R
@@ -1,63 +1,63 @@
 #' @rdname chart.TimeSeries
-#' 
+#'
 
 chart.TimeSeries.ggplot2<-
    function(R,
-            auto.grid, 
-            xaxis, yaxis, 
-            yaxis.right, 
-            type, 
-            lty, 
-            lwd, 
+            auto.grid,
+            xaxis, yaxis,
+            yaxis.right,
+            type,
+            lty,
+            lwd,
             las,
-            main, 
-            ylab, 
-            xlab, 
-            date.format.in, 
-            date.format, 
-            xlim, 
-            ylim, 
-            element.color, 
-            event.lines, 
-            event.labels, 
-            period.areas, 
-            event.color, 
-            period.color, 
-            colorset, 
-            pch, 
-            legend.loc, 
-            ylog, 
-            cex.axis, 
-            cex.legend, 
-            cex.lab, 
-            cex.labels, 
-            cex.main, 
-            major.ticks, 
-            minor.ticks, 
-            grid.color, 
-            grid.lty, 
+            main,
+            ylab,
+            xlab,
+            date.format.in,
+            date.format,
+            xlim,
+            ylim,
+            element.color,
+            event.lines,
+            event.labels,
+            period.areas,
+            event.color,
+            period.color,
+            colorset,
+            pch,
+            legend.loc,
+            ylog,
+            cex.axis,
+            cex.legend,
+            cex.lab,
+            cex.labels,
+            cex.main,
+            major.ticks,
+            minor.ticks,
+            grid.color,
+            grid.lty,
             xaxis.labels,
             plot.engine,
             yaxis.pct)
 
 {
-    
+
     R <- checkData(R, method='xts')
-  
+
     # Stack columns as rows and add security role
     R <- data.frame(date=index(R), coredata(R))
     R <- data.frame(R[1], utils::stack(R[2:ncol(R)]))
-    
+
     col_names = colnames(R)
     col_names[2] = "returns"
     col_names[3] = "security"
-    
+
     colnames(R) = col_names
 
     returns <- R[,"returns"]
     date <- R[,"date"]
     security <- R[,"security"]
-    
+
     # adjust of yaxis if in percentage
     if(yaxis.pct)
       returns = returns * 100
@@ -71,7 +71,7 @@ chart.TimeSeries.ggplot2<-
     if(mode(colorset) == "character"){
       p <- p + ggplot2::scale_color_brewer(palette=colorset)
     }
-    
+
     # format xlim and ylim
     if(!is.null(xlim[1])) # is.na or is.null?
       p <- p+xlim(xlim)
@@ -88,7 +88,7 @@ chart.TimeSeries.ggplot2<-
 
     # set legend position
     p <-p + ggplot2::theme(legend.position = legend.loc)
-    p <- p + xlab(xlab) +ylab(ylab)
+    p <- p + ggplot2::xlab(xlab) + ggplot2::ylab(ylab)
 
 
     return(p)
