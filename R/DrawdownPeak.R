f0297e239d4c29710d3297fa7e5f0096de6e200e
Brian G. Peterson <brian@braverock.com>
2026-03-30 14:31:16 -0500

    Fix DrawdownPeak scaling to handle decimal returns (fixes #102, fixes #132)

--- R/DrawdownPeak.R
+++ R/DrawdownPeak.R
@@ -7,51 +7,50 @@
 #' asset returns
 #' @param \dots any other passthru parameters
 #' @author Matthieu Lestel
-#' 
+#'
 #' @export
 
 
-DrawdownPeak <- function (R, ...)
-{
+DrawdownPeak <- function(R, ...) {
   R0 <- R
-  R = checkData(R, method="matrix")
-  
-  if (ncol(R)==1 || is.null(R) || is.vector(R)) {
-    calcul = FALSE
+  R <- checkData(R, method = "matrix")
+
+  if (ncol(R) == 1 || is.null(R) || is.vector(R)) {
+    calcul <- FALSE
     for (i in (1:length(R))) {
       if (any(!is.na(R))) {
-        calcul = TRUE
+        calcul <- TRUE
       }
-    }		      
+    }
     if (!calcul) {
-      result = NaN
+      result <- NaN
     } else {
-      R = na.omit(R)
-      drawdownpeak = rep(NA,length(R))
-      peak = 0
-      for(i in (1:length(R))) {
-        val = 1
-        borne = peak+1
-        for(j in (borne:i)) {
-          val = val*(1+R[j]/100)
+      R <- na.omit(R)
+      drawdownpeak <- rep(NA, length(R))
+      peak <- 0
+      for (i in (1:length(R))) {
+        val <- 1
+        borne <- peak + 1
+        for (j in (borne:i)) {
+          val <- val * (1 + R[j])
         }
         if (val > 1) {
-          peak = i
-          drawdownpeak[i] = 0
+          peak <- i
+          drawdownpeak[i] <- 0
         } else {
-          drawdownpeak[i] = (val-1)*100
+          drawdownpeak[i] <- (val - 1)
         }
       }
-      result = drawdownpeak
+      result <- drawdownpeak
     }
-    reclass(result, R0)	
+    reclass(result, R0)
     return(result)
   } else {
-    R = checkData(R)
-    result = apply(R, MARGIN = 2, DrawdownPeak, ...)
-    result<-t(result)
-    colnames(result) = colnames(R)
-    rownames(result) = paste("DrawdownPeak", sep="")
+    R <- checkData(R)
+    result <- apply(R, MARGIN = 2, DrawdownPeak, ...)
+    result <- t(result)
+    colnames(result) <- colnames(R)
+    rownames(result) <- paste("DrawdownPeak", sep = "")
     return(result)
   }
 }
