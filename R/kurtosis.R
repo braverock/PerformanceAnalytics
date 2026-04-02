98a9fa5b6c30f4ba255923f39082314c3f001b8a
Brian G. Peterson <brian@braverock.com>
2026-04-02 13:17:39 -0500

    fixes #125: Update Fisher skewness and kurtosis mathematical formulations
    - Removed mathematically incorrect usage of raw uncentered moments in method='fisher' estimators for both skewness and kurtosis, resolving highly divergent results.
    - Aligned Fisher's equations properly against central moments, verifying exact algebraic correspondence to standard SAS/SPSS algorithms (specifically e1071::type=2 equivalents for G1 statistics).
    - Modified Roxygen documentation equations to accurately reflect these sample bias offsets.
    - Added comprehensive regression tests enforcing precision limits against the robust e1071 implementation.

--- R/kurtosis.R
+++ R/kurtosis.R
@@ -1,7 +1,7 @@
 #' Kurtosis
-#' 
+#'
 #' compute kurtosis of a univariate distribution
-#' 
+#'
 #' This function was ported from the RMetrics package fUtilities to eliminate a
 #' dependency on fUtilties being loaded every time.  This function is identical
 #' except for the addition of \code{\link{checkData}} and additional labeling.
@@ -9,14 +9,14 @@
 #' \deqn{Kurtosis(moment) = \frac{1}{n}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_P})^4}{kurtosis(moment) = sum((x-mean(x))^4/var(x)^2)/length(x)}
 #' \deqn{Kurtosis(excess) = \frac{1}{n}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_P})^4 - 3}{kurtosis(excess) = sum((x-mean(x))^4/var(x)^2)/length(x) - 3}
 #' \deqn{Kurtosis(sample) =  \frac{n*(n+1)}{(n-1)*(n-2)*(n-3)}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_{S_P}})^4 }{kurtosis(sample) = sum(((x-mean(x))/var(x))^4)*n*(n+1)/((n-1)*(n-2)*(n-3))}
-#' \deqn{Kurtosis(fisher) = \frac{(n+1)*(n-1)}{(n-2)*(n-3)}*(\frac{\sum^{n}_{i=1}\frac{(r_i)^4}{n}}{(\sum^{n}_{i=1}(\frac{(r_i)^2}{n})^2} - \frac{3*(n-1)}{n+1})}{kurtosis (fisher) = ((n+1)*(n-1)*((sum(x^4)/n)/(sum(x^2)/n)^2 - (3*(n-1))/(n+1)))/((n-2)*(n-3))}
+#' \deqn{Kurtosis(fisher) = \frac{(n+1)*(n-1)}{(n-2)*(n-3)}*(\frac{\sum^{n}_{i=1}(r_i - \overline{r})^4/n}{(\sum^{n}_{i=1}(r_i - \overline{r})^2/n)^2} - \frac{3*(n-1)}{n+1})}{kurtosis (fisher) = ((n+1)*(n-1)*((sum(y^4)/n)/(sum(y^2)/n)^2 - (3*(n-1))/(n+1)))/((n-2)*(n-3))}
 #' \deqn{Kurtosis(sample excess) =  \frac{n*(n+1)}{(n-1)*(n-2)*(n-3)}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_{S_P}})^4  - \frac{3*(n-1)^2}{(n-2)*(n-3)}}{kurtosis(sample excess) = sum(((x-mean(x))/var(x))^4)*n*(n+1)/((n-1)*(n-2)*(n-3)) - 3*(n-1)^2/((n-2)*(n-3))}
 #'
 #'
 #' where \eqn{n} is the number of return, \eqn{\overline{r}} is the mean of the return
 #' distribution, \eqn{\sigma_P} is its standard deviation and \eqn{\sigma_{S_P}} is its
 #' sample standard deviation
-#' 
+#'
 #' @param na.rm a logical. Should missing values be removed?
 #' @param method a character string which specifies the method of computation.
 #' These are either \code{"moment"}, \code{"fisher"}, or \code{"excess"}.  If
@@ -32,34 +32,33 @@
 #' @param \dots arguments to be passed.
 #' @author Diethelm Wuertz, Matthieu Lestel
 #' @seealso \code{\link{skewness}}.
-#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement
 #' and attribution}, second edition 2008 p.84-85
-###keywords univar
+### keywords univar
 #' @examples
-#' 
+#'
 #' ## mean -
 #' ## var -
-#'    # Mean, Variance:
-#'    r = rnorm(100)
-#'    mean(r)
-#'    var(r)
-#' 
+#' # Mean, Variance:
+#' r <- rnorm(100)
+#' mean(r)
+#' var(r)
+#'
 #' ## kurtosis -
-#'    kurtosis(r)
-#' 
+#' kurtosis(r)
+#'
 #' data(managers)
-#' kurtosis(managers[,1:8])
+#' kurtosis(managers[, 1:8])
 #'
 #' data(portfolio_bacon)
-#' print(kurtosis(portfolio_bacon[,1], method="sample")) #expected 3.03
-#' print(kurtosis(portfolio_bacon[,1], method="sample_excess")) #expected -0.41
-#' print(kurtosis(managers['1996'], method="sample"))
-#' print(kurtosis(managers['1996',1], method="sample"))
-#' 
+#' print(kurtosis(portfolio_bacon[, 1], method = "sample")) # expected 3.03
+#' print(kurtosis(portfolio_bacon[, 1], method = "sample_excess")) # expected -0.41
+#' print(kurtosis(managers["1996"], method = "sample"))
+#' print(kurtosis(managers["1996", 1], method = "sample"))
+#'
 #' @export
 kurtosis <-
-    function (x, na.rm = FALSE, method = c("excess", "moment", "fisher", "sample", "sample_excess"), ...)
-{
+  function(x, na.rm = FALSE, method = c("excess", "moment", "fisher", "sample", "sample_excess"), ...) {
     # @author Diethelm Wuertz
     # @author Brian Peterson   (modify for PerformanceAnalytics)
 
@@ -72,69 +71,69 @@ kurtosis <-
     # FUNCTION:
 
     # Method:
-    method = match.arg(method)
+    method <- match.arg(method)
 
-    R=checkData(x,method="matrix")
+    R <- checkData(x, method = "matrix")
 
-    columns = ncol(R)
-    columnnames=colnames(R)
+    columns <- ncol(R)
+    columnnames <- colnames(R)
     # FUNCTION:
-    for(column in 1:columns) {
-        x = as.vector(na.omit(R[,column]))
-        #x = R[,column]
+    for (column in 1:columns) {
+      x <- as.vector(na.omit(R[, column]))
+      # x = R[,column]
 
-        if (!is.numeric(x)) stop("The selected column is not numeric")
+      if (!is.numeric(x)) stop("The selected column is not numeric")
 
-        # Remove NAs:
-        if (na.rm) x = x[!is.na(x)]
+      # Remove NAs:
+      if (na.rm) x <- x[!is.na(x)]
 
-        # Warnings:
-        if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
-            warning("argument is not numeric or logical: returning NA")
-            return(as.numeric(NA))}
+      # Warnings:
+      if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
+        warning("argument is not numeric or logical: returning NA")
+        return(as.numeric(NA))
+      }
 
 
-        # Kurtosis:
-        n = length(x)
-        if (is.integer(x)) x = as.numeric(x)
-        if (method == "excess") {
-            kurtosis = sum((x-mean(x))^4/(var(x)*(n-1)/n)^2)/length(x) - 3
-        }
-        if (method == "moment") {
-            kurtosis = sum((x-mean(x))^4/(var(x)*(n-1)/n)^2)/length(x)
-        }
-        if (method == "fisher") {
-            kurtosis = ((n+1)*(n-1)*((sum(x^4)/n)/(sum(x^2)/n)^2 -
-                (3*(n-1))/(n+1)))/((n-2)*(n-3))
-        }
-	if (method == "sample") {
-	   kurtosis = sum((x-mean(x))^4/var(x)^2)*n*(n+1)/((n-1)*(n-2)*(n-3))
-	}
-	if (method == "sample_excess") {
-	    kurtosis = sum((x-mean(x))^4/var(x)^2)*n*(n+1)/((n-1)*(n-2)*(n-3)) - 3*(n-1)^2/((n-2)*(n-3))
-	}
-        kurtosis=array(kurtosis)
-        if (column==1) {
-            #create data.frame
-            result=data.frame(kurtosis=kurtosis)
-        } else {
-            kurtosis=data.frame(kurtosis=kurtosis)
-            result=cbind(result,kurtosis)
-        }
-    } #end columns loop
+      # Kurtosis:
+      n <- length(x)
+      if (is.integer(x)) x <- as.numeric(x)
+      if (method == "excess") {
+        kurtosis <- sum((x - mean(x))^4 / (var(x) * (n - 1) / n)^2) / length(x) - 3
+      }
+      if (method == "moment") {
+        kurtosis <- sum((x - mean(x))^4 / (var(x) * (n - 1) / n)^2) / length(x)
+      }
+      if (method == "fisher") {
+        y <- x - mean(x)
+        kurtosis <- ((n + 1) * (n - 1) * ((sum(y^4) / n) / (sum(y^2) / n)^2 -
+          (3 * (n - 1)) / (n + 1))) / ((n - 2) * (n - 3))
+      }
+      if (method == "sample") {
+        kurtosis <- sum((x - mean(x))^4 / var(x)^2) * n * (n + 1) / ((n - 1) * (n - 2) * (n - 3))
+      }
+      if (method == "sample_excess") {
+        kurtosis <- sum((x - mean(x))^4 / var(x)^2) * n * (n + 1) / ((n - 1) * (n - 2) * (n - 3)) - 3 * (n - 1)^2 / ((n - 2) * (n - 3))
+      }
+      kurtosis <- array(kurtosis)
+      if (column == 1) {
+        # create data.frame
+        result <- data.frame(kurtosis = kurtosis)
+      } else {
+        kurtosis <- data.frame(kurtosis = kurtosis)
+        result <- cbind(result, kurtosis)
+      }
+    } # end columns loop
 
-    if(ncol(result) == 1) {
-        # some backflips to name the single column zoo object
-        result = as.numeric(result)
-    }
-    else{
-        colnames(result) = columnnames
-        rownames(result) = "Excess Kurtosis"
+    if (ncol(result) == 1) {
+      # some backflips to name the single column zoo object
+      result <- as.numeric(result)
+    } else {
+      colnames(result) <- columnnames
+      rownames(result) <- "Excess Kurtosis"
     }
     # Return Value:
     result
-
-}
+  }
 
 ###############################################################################
 # R (https://r-project.org/) Econometrics for Performance and Risk Analysis
--- R/skewness.R
+++ R/skewness.R
@@ -1,54 +1,53 @@
 #' Skewness
-#' 
+#'
 #' compute skewness of a univariate distribution.
-#' 
+#'
 #' This function was ported from the RMetrics package fUtilities to eliminate a
 #' dependency on fUtiltiies being loaded every time.  The function is identical
 #' except for the addition of \code{\link{checkData} and column support.}
 #'
 #' \deqn{Skewness(moment) = \frac{1}{n}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_P})^3}{Skewness(moment) = sum((x-mean(x))^3/var(x)^(3/2))/length(x)}
 #' \deqn{Skewness(sample) =  \frac{n}{(n-1)*(n-2)}*\sum^{n}_{i=1}(\frac{r_i - \overline{r}}{\sigma_{S_P}})^3 }{skewness(sample) = sum(((x-mean(x))/var(x))^3)*n/((n-1)*(n-2))}
-#' \deqn{Skewness(fisher) = \frac{\frac{\sqrt{n*(n-1)}}{n-2}*\sum^{n}_{i=1}\frac{x^3}{n}}{\sum^{n}_{i=1}(\frac{x^2}{n})^{3/2}}}{Skewness(fisher)((sqrt(n*(n-1))/(n-2))*(sum(x^3)/n))/((sum(x^2)/n)^(3/2))}
+#' \deqn{Skewness(fisher) = \frac{\sqrt{n*(n-1)}}{n-2}*\frac{\sum^{n}_{i=1}(x_i - \overline{x})^3/n}{(\sum^{n}_{i=1}(x_i - \overline{x})^2/n)^{3/2}}}{Skewness(fisher)((sqrt(n*(n-1))/(n-2))*(sum((x-mean(x))^3)/n))/((sum((x-mean(x))^2)/n)^(3/2))}
 #'
 #' where \eqn{n} is the number of return, \eqn{\overline{r}} is the mean of the return
 #' distribution, \eqn{\sigma_P} is its standard deviation and \eqn{\sigma_{S_P}} is its
 #' sample standard deviation
-#' 
+#'
 #' @param na.rm a logical. Should missing values be removed?
 #' @param method a character string which specifies the method of computation.
 #' These are either \code{"moment"} or \code{"fisher"} The \code{"moment"}
 #' method is based on the definitions of skewnessfor distributions; these forms
 #' should be used when resampling (bootstrap or jackknife). The \code{"fisher"}
 #' method correspond to the usual "unbiased" definition of sample variance,
-#' although in the case of skewness exact unbiasedness is not possible. The 
+#' although in the case of skewness exact unbiasedness is not possible. The
 #' \code{"sample"} method gives the sample skewness of the distribution.
 #' @param x a numeric vector or object.
 #' @param \dots arguments to be passed.
 #' @author Diethelm Wuertz, Matthieu Lestel
 #' @seealso
-#' 
+#'
 #' \code{\link{kurtosis}}
-#' @references Carl Bacon, \emph{Practical portfolio performance measurement 
+#' @references Carl Bacon, \emph{Practical portfolio performance measurement
 #' and attribution}, second edition 2008 p.83-84
-###keywords univar
+### keywords univar
 #' @examples
-#' 
+#'
 #' ## mean -
 #' ## var -
-#'    # Mean, Variance:
-#'    r = rnorm(100)
-#'    mean(r)
-#'    var(r)
-#' 
+#' # Mean, Variance:
+#' r <- rnorm(100)
+#' mean(r)
+#' var(r)
+#'
 #' ## skewness -
-#'    skewness(r)
+#' skewness(r)
 #' data(managers)
 #' skewness(managers)
-#' 
+#'
 #' @export
 skewness <-
-    function (x, na.rm = FALSE, method = c("moment", "fisher", "sample"), ...)
-{
+  function(x, na.rm = FALSE, method = c("moment", "fisher", "sample"), ...) {
     # @author Diethelm Wuertz
     # @author Brian Peterson   (modify for PerformanceAnalytics)
 
@@ -61,68 +60,67 @@ skewness <-
     # FUNCTION:
 
     # Method:
-    method = match.arg(method)
+    method <- match.arg(method)
 
-    R=checkData(x,method="matrix")
+    R <- checkData(x, method = "matrix")
 
-    columns = ncol(R)
-    columnnames=colnames(R)
+    columns <- ncol(R)
+    columnnames <- colnames(R)
     # FUNCTION:
-    for(column in 1:columns) {
-        x = as.vector(na.omit(R[,column]))
-        #x = R[,column]
+    for (column in 1:columns) {
+      x <- as.vector(na.omit(R[, column]))
+      # x = R[,column]
 
-        if (!is.numeric(x)) stop("The selected column is not numeric")
+      if (!is.numeric(x)) stop("The selected column is not numeric")
 
-        # Remove NAs:
-        if (na.rm) x = x[!is.na(x)]
+      # Remove NAs:
+      if (na.rm) x <- x[!is.na(x)]
 
-        # Warnings:
-        if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
-            warning("argument is not numeric or logical: returning NA")
-            return(as.numeric(NA))}
+      # Warnings:
+      if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
+        warning("argument is not numeric or logical: returning NA")
+        return(as.numeric(NA))
+      }
 
-        # Skewness:
-        n = length(x)
-        if (is.integer(x)) x = as.numeric(x)
+      # Skewness:
+      n <- length(x)
+      if (is.integer(x)) x <- as.numeric(x)
 
-        # Selected Method:
-        if (method == "moment") {
-            skewness = sum((x-mean(x))^3/sqrt(var(x)*(n-1)/n)^3)/length(x)
-        }
-        if (method == "fisher") {
-            if (n < 3)
-                skewness = NA
-            else
-                skewness = ((sqrt(n*(n-1))/(n-2))*(sum(x^3)/n))/((sum(x^2)/n)^(3/2))
-        }
-	if (method == "sample") {
-            skewness = sum((x-mean(x))^3/sqrt(var(x)*(n-1)/n)^3)*n/((n-1)*(n-2))
-	}
-
-        skewness=array(skewness)
-        if (column==1) {
-            #create data.frame
-            result=data.frame(skewness=skewness)
+      # Selected Method:
+      if (method == "moment") {
+        skewness <- sum((x - mean(x))^3 / sqrt(var(x) * (n - 1) / n)^3) / length(x)
+      }
+      if (method == "fisher") {
+        if (n < 3) {
+          skewness <- NA
         } else {
-            skewness=data.frame(skewness=skewness)
-            result=cbind(result,skewness)
+          skewness <- ((sqrt(n * (n - 1)) / (n - 2)) * (sum((x - mean(x))^3) / n)) / ((sum((x - mean(x))^2) / n)^(3 / 2))
         }
+      }
+      if (method == "sample") {
+        skewness <- sum((x - mean(x))^3 / sqrt(var(x) * (n - 1) / n)^3) * n / ((n - 1) * (n - 2))
+      }
 
-    } #end columns loop
+      skewness <- array(skewness)
+      if (column == 1) {
+        # create data.frame
+        result <- data.frame(skewness = skewness)
+      } else {
+        skewness <- data.frame(skewness = skewness)
+        result <- cbind(result, skewness)
+      }
+    } # end columns loop
 
-    if(ncol(result) == 1) {
-        # some backflips to name the single column zoo object
-        result = as.numeric(result)
-    }
-    else{
-        colnames(result) = columnnames
-        rownames(result) = "Skewness"
+    if (ncol(result) == 1) {
+      # some backflips to name the single column zoo object
+      result <- as.numeric(result)
+    } else {
+      colnames(result) <- columnnames
+      rownames(result) <- "Skewness"
     }
     # Return Value:
     result
-
-}
+  }
 
 ###############################################################################
 # R (https://r-project.org/) Econometrics for Performance and Risk Analysis
