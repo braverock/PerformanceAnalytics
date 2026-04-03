e197cf53024775c5fab424eec82a17fe1027f2e0
Brian G. Peterson <brian@braverock.com>
2026-04-02 19:20:19 -0500

    fixes #86: SmoothingIndex doesn't de-mean returns
    - Corrected a critical mathematical flaw where 'include.mean=FALSE' allowed the raw dataset mean to drift into the moving-average (MA) parameters.
    - Explicitly demeaned the returns prior to the arima() execution, structurally aligning the calculation with the formal Getmansky (2004) algorithm.
    - Updated the Roxygen documentation explicitly noting the manual demean step.
    - Built regression tests reproducing the reporter's synthetic ARMA series confirming the thetas securely match their theoretical baselines.

--- R/SmoothingIndex.R
+++ R/SmoothingIndex.R
@@ -1,12 +1,12 @@
 #' calculate Normalized Getmansky Smoothing Index
-#' 
+#'
 #' Proposed by Getmansky et al to provide a normalized measure of "liquidity
 #' risk."
-#' 
+#'
 #' To measure the effects of smoothing, Getmansky, Lo, et al (2004) define a
 #' "smoothing profile" as a vector of coefficients for an MLE fit on returns
 #' using a two-period moving-average process.
-#' 
+#'
 #' The moving-average process of order \eqn{k=2} (specified using
 #' \code{MAorder}) gives \eqn{R_t = \theta_{0} R_{t} + \theta_1 R_{t -1} +
 #' \theta_2 R_{t-2}}, under the constraint that the sum of the coefficients is
@@ -19,26 +19,27 @@
 #' The \code{method} specifies how to fit the model, in this case using maximum
 #' likelihood estimation (MLE) in a fashion similar to the estimation of
 #' standard moving-average time series models, using:
-#' 
+#'
 #' \code{arima(ra, order=c(0,0,2), method="ML", transform.pars=TRUE,
 #' include.mean=FALSE)}
-#' 
+#'
 #' \code{include.mean}: Getmansky, et al. (2004) p 555 "By applying the above
-#' procedure to observed de-meaned returns...", so we set that parameter to
-#' 'FALSE'.
-#' 
+#' procedure to observed de-meaned returns...". To comply with this, we explicitly
+#' subtract the mean from the returns prior to the \code{arima} fit and set
+#' \code{include.mean=FALSE} to avoid intercept absorption into the MA terms.
+#'
 #' \code{transform.pars}: ibid, "we impose the additional restriction that the
 #' estimated MA(k) process be invertible," so we set the parameter to 'TRUE'.
-#' 
+#'
 #' The coefficients, \eqn{\theta_{j}}, are then normalized to sum to
 #' interpreted as a "weighted average of the fund's true returns over the most
 #' recent \eqn{k + 1} periods, including the current period."
-#' 
+#'
 #' If these weights are disproportionately centered on a small number of lags,
 #' relatively little serial correlation will be induced. However, if the
 #' weights are evenly distributed among many lags, this would show higher
 #' serial correlation.
-#' 
+#'
 #' The paper notes that because \eqn{\theta_j \in [0, 1]}, \eqn{\xi} is also
 #' confined to the unit interval, and is minimized when all the
 #' \eqn{\theta_j}'s are identical.  That implies a value of \eqn{1/(k + 1)} for
@@ -46,12 +47,12 @@
 #' and the rest are 0.  In the context of smoothed returns, a lower value of
 #' \eqn{\xi} implies more smoothing, and the upper bound of 1 implies no
 #' smoothing.
-#' 
+#'
 #' The "smoothing index", represented as \eqn{\xi}, is calculated the same way
 #' the Herfindahl index.  The Herfindal measure is well known in the industrial
 #' organization literature as a measure of the concentration of firms in a
 #' given industry where \eqn{y_j} represents the market share of firm \eqn{j}.
-#' 
+#'
 #' This method (as well as the implementation described in the paper), does not
 #' enforce \eqn{\theta_j \in [0, 1]}, so \eqn{\xi} is not limited to that range
 #' either.  All we can say is that lower values are "less liquid" and higher
@@ -60,12 +61,12 @@
 #' negative autocorrelation coefficients from the calculation (the papers below
 #' do not make an economic case for eliminating negative autocorrelation,
 #' however).
-#' 
+#'
 #' Interpretation of the resulting value is difficult.  All we can say is that
 #' lower values appear to have autocorrelation structure like we might expect
 #' of "less liquid" instruments.  Higher values appear "more liquid" or are
 #' poorly fit or mis-specified.
-#' 
+#'
 #' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
 #' asset returns
 #' @param neg.thetas if FALSE, function removes negative coefficients (thetas)
@@ -83,19 +84,18 @@
 #' Getmansky, Mila, Andrew W. Lo, and Igor Makarov. 2004. An Econometric Model
 #' of Serial Correlation and Illiquidity in Hedge Fund Returns. Journal of
 #' Financial Economics (74): 529-609.
-###keywords ts multivariate distribution models
+### keywords ts multivariate distribution models
 #' @examples
-#' 
+#'
 #' data(managers)
 #' data(edhec)
-#' SmoothingIndex(managers[,1,drop=FALSE])
-#' SmoothingIndex(managers[,1:8])
+#' SmoothingIndex(managers[, 1, drop = FALSE])
+#' SmoothingIndex(managers[, 1:8])
 #' SmoothingIndex(edhec)
-#' 
+#'
 #' @export
 SmoothingIndex <-
-function (R, neg.thetas = FALSE, MAorder=2, verbose = FALSE, ...)
-{ # @author Peter Carl
+  function(R, neg.thetas = FALSE, MAorder = 2, verbose = FALSE, ...) { # @author Peter Carl
 
     # Description:
     # SmoothingIndex
@@ -104,87 +104,90 @@ function (R, neg.thetas = FALSE, MAorder=2, verbose = FALSE, ...)
 
     # Function:
     if (is.vector(R)) {
-        x = na.omit(R)
+      x <- na.omit(R)
 
-        MA2 = NULL
-        thetas = 0
-        smoothing.index = 0
+      MA2 <- NULL
+      thetas <- 0
+      smoothing.index <- 0
 
-        # First, create a a maximum likelihood estimation fit for an MA process.
+      # First, create a a maximum likelihood estimation fit for an MA process.
 
-        # include.mean: Getmansky, et al. JFE 2004 p 555 "By applying the above procedure
-        # to observed de-meaned returns...", so set parameter to FALSE
-        # transform.pars: ibid, "we impose the additional restriction that the estimated MA(k)
-        # process be invertible." so set the parameter to TRUE
-        MA2 = arima(R, order=c(0,0,MAorder), method="ML", transform.pars=TRUE, include.mean=FALSE)
+      # include.mean: Getmansky, et al. JFE 2004 p 555 "By applying the above procedure
+      # to observed de-meaned returns...", so we explicitly demean the returns
+      # and set include.mean to FALSE.
+      # transform.pars: ibid, "we impose the additional restriction that the estimated MA(k)
+      # process be invertible." so set the parameter to TRUE
+      R_demean <- R - mean(R, na.rm = TRUE)
+      MA2 <- arima(R_demean, order = c(0, 0, MAorder), method = "ML", transform.pars = TRUE, include.mean = FALSE)
 
-        # Page 555:
-        #
-        # "Because of the scaling property Eq. (52) of the MA(k) likelihood function, a
-        # simple procedure for obtaining estimates of our smoothing model with the
-        # normalization Eq. (49) is to transform estimates (\theta; sigma) from standard
-        # MA(k) estimation packages such as SAS or RATS by dividing each \theta_i by 1 + \theta_1 +
-        # \theta_2 ... \theta_k and multiplying sigma by the same factor. The likelihood function
-        # remains unchanged but the transformed smoothing coefficients will now satisfy
-        # Eq. (49)."
+      # Page 555:
+      #
+      # "Because of the scaling property Eq. (52) of the MA(k) likelihood function, a
+      # simple procedure for obtaining estimates of our smoothing model with the
+      # normalization Eq. (49) is to transform estimates (\theta; sigma) from standard
+      # MA(k) estimation packages such as SAS or RATS by dividing each \theta_i by 1 + \theta_1 +
+      # \theta_2 ... \theta_k and multiplying sigma by the same factor. The likelihood function
+      # remains unchanged but the transformed smoothing coefficients will now satisfy
+      # Eq. (49)."
 
-        # From the arima function above, MA2$coef contains two coefficients, and no intercept value.
-        # The calculation below adjusts for that.
-        coefMA2=0
-        if(neg.thetas == FALSE)
-            for (i in 1:length(coef(MA2)))
-                coefMA2[i] = max(0,coef(MA2)[i])
-    #         coefMA2 = max(0,coef(MA2)) # enforces no negative thetas
-        else
-            coefMA2 = coef(MA2) # allows negative thetas
-
-        # Dr. Stefan Albrecht, CFA points out, "I assume that you have to take:"
-        thetas = c(1, coefMA2) / (1 + sum(coefMA2))
-    #
-    #
-    #     thetas = as.numeric((MA2$coef)/sum(MA2$coef))
-
-        # This measure is well known in the industrial organization literature as the HeRfindahl
-        # index, a measure of the concentration of firms in a given industry where yj represents the
-        # market share of firm j: Because theta_j; 1; x is also confined to the unit interval,
-        # and is minimized when all the theta_j 's are identical, which implies a value of 1=\delta_k \phi 1 \phi
-        # for x; and is maximized when one coefficient is 1 and the rest are 0, in which case x 1/4 1:
-        # In the context of smoothed returns, a lower value of x implies more smoothing, and the upper bound
-        # of 1 implies no smoothing, hence we shall refer to x as a "smoothing index".
-
-        smoothing.index = sum(thetas^2) # Calc'd as Herfindahl index would be, referred to as \xi, below
-
-        # The interpretation of this is tricky:
-
-        # "Because \theta_j \varepsilon [0, 1], \xi is also confined to the unit interval, and is minimized when all
-        # the \theta_j 's are identical, which implies a value of 1/(k + 1) for \xi, and is maximized when
-        # one coefficient is 1 and the rest are 0, in which case \xi = 1. In the context of smoothed
-        # returns, a lower value of \xi implies more smoothing, and the upper bound of 1 implies no
-        # smoothing, hence we shall refer to \xi as a "smoothing index"."
-
-        # That's fine, except that this method (as described in the paper), does not enforce
-        # \thetaj \varepsilon [0, 1], so \xi is not limited to that range either.  All we can say is that lower values
-        # are "less liquid" and higher values are "more liquid" or mis-specified.
-
-        if(verbose)
-            return(list(SmoothingIndex = smoothing.index, Thetas = thetas))
-        else
-            return(smoothing.index)
-
-    }
-    else {
-        R = checkData(R, method = "matrix", ... = ...)
-        result = apply(R, 2, SmoothingIndex, neg.thetas = neg.thetas, MAorder = MAorder, verbose = verbose, ... = ...)
-        if(length(result) ==1)
-            return(result)
-        else {
-            dim(result) = c(1,NCOL(R))
-            colnames(result) = colnames(R)
-            rownames(result) = "Smoothing Index"
-            return(result)
+      # From the arima function above, MA2$coef contains two coefficients, and no intercept value.
+      # The calculation below adjusts for that.
+      coefMA2 <- 0
+      if (neg.thetas == FALSE) {
+        for (i in 1:length(coef(MA2))) {
+          coefMA2[i] <- max(0, coef(MA2)[i])
         }
+      } #         coefMA2 = max(0,coef(MA2)) # enforces no negative thetas
+      else {
+        coefMA2 <- coef(MA2)
+      } # allows negative thetas
+
+      # Dr. Stefan Albrecht, CFA points out, "I assume that you have to take:"
+      thetas <- c(1, coefMA2) / (1 + sum(coefMA2))
+      #
+      #
+      #     thetas = as.numeric((MA2$coef)/sum(MA2$coef))
+
+      # This measure is well known in the industrial organization literature as the HeRfindahl
+      # index, a measure of the concentration of firms in a given industry where yj represents the
+      # market share of firm j: Because theta_j; 1; x is also confined to the unit interval,
+      # and is minimized when all the theta_j 's are identical, which implies a value of 1=\delta_k \phi 1 \phi
+      # for x; and is maximized when one coefficient is 1 and the rest are 0, in which case x 1/4 1:
+      # In the context of smoothed returns, a lower value of x implies more smoothing, and the upper bound
+      # of 1 implies no smoothing, hence we shall refer to x as a "smoothing index".
+
+      smoothing.index <- sum(thetas^2) # Calc'd as Herfindahl index would be, referred to as \xi, below
+
+      # The interpretation of this is tricky:
+
+      # "Because \theta_j \varepsilon [0, 1], \xi is also confined to the unit interval, and is minimized when all
+      # the \theta_j 's are identical, which implies a value of 1/(k + 1) for \xi, and is maximized when
+      # one coefficient is 1 and the rest are 0, in which case \xi = 1. In the context of smoothed
+      # returns, a lower value of \xi implies more smoothing, and the upper bound of 1 implies no
+      # smoothing, hence we shall refer to \xi as a "smoothing index"."
+
+      # That's fine, except that this method (as described in the paper), does not enforce
+      # \thetaj \varepsilon [0, 1], so \xi is not limited to that range either.  All we can say is that lower values
+      # are "less liquid" and higher values are "more liquid" or mis-specified.
+
+      if (verbose) {
+        return(list(SmoothingIndex = smoothing.index, Thetas = thetas))
+      } else {
+        return(smoothing.index)
+      }
+    } else {
+      R <- checkData(R, method = "matrix", ... = ...)
+      result <- apply(R, 2, SmoothingIndex, neg.thetas = neg.thetas, MAorder = MAorder, verbose = verbose, ... = ...)
+      if (length(result) == 1) {
+        return(result)
+      } else {
+        dim(result) <- c(1, NCOL(R))
+        colnames(result) <- colnames(R)
+        rownames(result) <- "Smoothing Index"
+        return(result)
+      }
     }
-}
+  }
 
 ###############################################################################
 # R (https://r-project.org/) Econometrics for Performance and Risk Analysis
