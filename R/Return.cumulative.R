03aab12ef3147c586abda77d60b5cf8641010d20
Brian G. Peterson <brian@braverock.com>
2026-04-03 21:40:18 -0500

    docs: clarify Return.cumulative output and document alternatives (fixes #159)
    The user in #159 requested clarification on why `Return.cumulative` output
    a single scalar rather than a cumulative time series.
    
    This commit updates the roxygen documentation to explicitly note that
    `Return.cumulative` evaluates the total compounded return over the entire period.
    It now officially points users towards `cumprod(1+R)` and `chart.CumReturns`
    if they require a time series wealth index.

--- R/Return.cumulative.R
+++ R/Return.cumulative.R
@@ -1,12 +1,16 @@
 #' calculate a compounded (geometric) cumulative return
-#' 
+#'
 #' This is a useful function for calculating cumulative return over a period of
 #' time, say a calendar year.  Can produce simple or geometric return.
-#' 
+#'
 #' product of all the individual period returns
-#' 
+#'
 #' \deqn{(1+r_{1})(1+r_{2})(1+r_{3})\ldots(1+r_{n})-1=prod(1+R)-1}{prod(1+R)-1}
-#' 
+#'
+#' Note that this function calculates the total return over the entire period as a
+#' single scalar value. If you need the time series of cumulative wealth (i.e. a
+#' wealth index), you should use \code{cumprod(1+R)} or \code{cumprod(1+R)-1}.
+#'
 #' @param R an xts, vector, matrix, data frame, timeSeries or zoo object of
 #' asset returns
 #' @param geometric utilize geometric chaining (TRUE) or simple/arithmetic chaining (FALSE) to aggregate returns,
@@ -15,39 +19,41 @@
 #' @seealso \code{\link{Return.annualized}}
 #' @references Bacon, Carl. \emph{Practical Portfolio Performance Measurement
 #' and Attribution}. Wiley. 2004. p. 6
-###keywords ts multivariate distribution models
+#'
+#' For time series of cumulative wealth, see:
+#' \code{\link[base]{cumprod}}
+#' \code{\link{chart.CumReturns}}
+### keywords ts multivariate distribution models
 #' @examples
-#' 
+#'
 #' data(managers)
-#' Return.cumulative(managers[,1,drop=FALSE])
-#' Return.cumulative(managers[,1:8])
-#' Return.cumulative(managers[,1:8],geometric=FALSE)
-#' 
+#' Return.cumulative(managers[, 1, drop = FALSE])
+#' Return.cumulative(managers[, 1:8])
+#' Return.cumulative(managers[, 1:8], geometric = FALSE)
+#'
 #' @export
 Return.cumulative <-
-function (R, geometric = TRUE)
-{ # @author Peter Carl
+  function(R, geometric = TRUE) { # @author Peter Carl
 
     # This is a useful function for calculating cumulative return over a period
     # of time, say a calendar year.  Can produce simple or geometric return.
 
     if (is.vector(R)) {
-        R = na.omit(R)
-        if (!geometric)
-            return(sum(R))
-        else {
-            return(prod(1+R)-1)
-        }
+      R <- na.omit(R)
+      if (!geometric) {
+        return(sum(R))
+      } else {
+        return(prod(1 + R) - 1)
+      }
+    } else {
+      R <- checkData(R, method = "matrix")
+      result <- apply(R, 2, Return.cumulative, geometric = geometric)
+      dim(result) <- c(1, NCOL(R))
+      colnames(result) <- colnames(R)
+      rownames(result) <- "Cumulative Return"
+      return(result)
     }
-    else {
-        R = checkData(R, method = "matrix")
-        result = apply(R, 2, Return.cumulative, geometric = geometric)
-        dim(result) = c(1,NCOL(R))
-        colnames(result) = colnames(R)
-        rownames(result) = "Cumulative Return"
-        return(result)
-    }
-}
+  }
 
 ###############################################################################
 # R (https://r-project.org/) Econometrics for Performance and Risk Analysis
